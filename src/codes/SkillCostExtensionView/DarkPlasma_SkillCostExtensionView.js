c55c9e2b34f76fa32044a904c81654e4315fe8d3
plasma <librarian256@gmail.com>
2020-10-03 22:00:49 +0900

    1.0.0 拡張スキルコスト表示プラグイン

---
+++ src/codes/SkillCostExtensionView/DarkPlasma_SkillCostExtensionView.js
@@ -0,0 +1,40 @@
+import { settings } from './_build/DarkPlasma_SkillCostExtensionView_parameters';
+
+ColorManager.hpCostColor = function () {
+  return settings.hpCostColor.startsWith('#') ? settings.hpCostColor : this.textColor(settings.hpCostColor);
+};
+
+ColorManager.itemCostColor = function () {
+  return settings.itemCostColor.startsWith('#') ? settings.itemCostColor : this.textColor(settings.itemCostColor);
+};
+
+ColorManager.goldCostColor = function () {
+  return settings.goldCostColor.startsWith('#') ? settings.goldCostColor : this.textColor(settings.goldCostColor);
+};
+
+const _Window_SkillList_drawSkillCost = Window_SkillList.prototype.drawSkillCost;
+Window_SkillList.prototype.drawSkillCost = function (skill, x, y, width) {
+  if (this._actor.skillGoldCost(skill) > 0) {
+    this.changeTextColor(ColorManager.goldCostColor());
+    this.drawText(this._actor.skillGoldCost(skill), x, y, width, 'right');
+  } else if (this._actor.skillItemCosts(skill).length > 0) {
+    this.changeTextColor(ColorManager.itemCostColor());
+    this.drawItemCost(skill, x, y, width);
+  } else if (this._actor.skillHpCost(skill) > 0) {
+    this.changeTextColor(ColorManager.hpCostColor());
+    this.drawText(this._actor.skillHpCost(skill), x, y, width, 'right');
+  } else {
+    _Window_SkillList_drawSkillCost.call(this, skill, x, y, width);
+  }
+};
+
+Window_SkillList.prototype.drawItemCost = function (skill, x, y, width) {
+  const firstItemCost = this._actor.skillItemCosts(skill)[0];
+  this.drawText(
+    `${firstItemCost.num}/${$gameParty.numItemsForDisplay($dataItems[firstItemCost.item])}`,
+    x,
+    y,
+    width,
+    'right'
+  );
+};
