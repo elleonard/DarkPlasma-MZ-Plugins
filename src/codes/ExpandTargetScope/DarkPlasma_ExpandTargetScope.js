0c26349117f3ea0961884bca4f971d472af01c40
plasma <librarian256@gmail.com>
2020-10-17 19:29:47 +0900

    1.0.4 カーソル描画処理修正 味方対象スキル全体化

--- src/codes/ExpandTargetScope/DarkPlasma_ExpandTargetScope.js
+++ src/codes/ExpandTargetScope/DarkPlasma_ExpandTargetScope.js
@@ -29,6 +29,14 @@ Game_Action.prototype.isForAll = function () {
   return _Game_Action_isForAll.call(this) || this._isExpandedScope;
 };
 
+/**
+ * 元々から全体対象であるかどうか
+ * @return {boolean}
+ */
+Game_Action.prototype.isForAllByDefault = function () {
+  return _Game_Action_isForAll.call(this);
+};
+
 const _Game_Action_needsSelection = Game_Action.prototype.needsSelection;
 Game_Action.prototype.needsSelection = function () {
   return _Game_Action_needsSelection.call(this) || this.isForAll();
@@ -81,26 +89,28 @@ const _Scene_Battle_startActorSelection = Scene_Battle.prototype.startActorSelec
 Scene_Battle.prototype.startActorSelection = function () {
   _Scene_Battle_startActorSelection.call(this);
   const action = BattleManager.inputtingAction();
-  if (action.isForAll()) {
+  if (action.isForAllByDefault()) {
     this._actorWindow.setCursorAll(true);
     this._actorWindow.setCursorFixed(true);
   } else {
     this._actorWindow.setCursorAll(false);
     this._actorWindow.setCursorFixed(false);
   }
+  this._actorWindow.refreshCursor();
 };
 
 const _Scene_Battle_startEnemySelection = Scene_Battle.prototype.startEnemySelection;
 Scene_Battle.prototype.startEnemySelection = function () {
   _Scene_Battle_startEnemySelection.call(this);
   const action = BattleManager.inputtingAction();
-  if (action.isForAll()) {
+  if (action.isForAllByDefault()) {
     this._enemyWindow.setCursorAll(true);
     this._enemyWindow.setCursorFixed(true);
   } else {
     this._enemyWindow.setCursorAll(false);
     this._enemyWindow.setCursorFixed(false);
   }
+  this._enemyWindow.refreshCursor();
 };
 
 const _Scene_Battle_onActorOk = Scene_Battle.prototype.onActorOk;
@@ -132,20 +142,58 @@ Sprite_Battler.prototype.updateSelectionEffect = function () {
   _Sprite_Battler_updateSelectionEffect.call(this);
 };
 
+const _Window_BattleActor_processHandling = Window_BattleActor.prototype.processHandling;
+Window_BattleActor.prototype.processHandling = function () {
+  if (this.isOpenAndActive()) {
+    const action = BattleManager.inputtingAction();
+    if (action.canExpandScope() && Input.isTriggered(settings.switchScopeButton) && !this.cursorFixed()) {
+      this.setCursorAll(!this._cursorAll);
+    }
+  }
+  return _Window_BattleActor_processHandling.call(this);
+};
+
+const _Window_BattleActor_setCursorAll = Window_BattleActor.prototype.setCursorAll;
+Window_BattleActor.prototype.setCursorAll = function (cursorAll) {
+  _Window_BattleActor_setCursorAll.call(this, cursorAll);
+  if (cursorAll) {
+    $gameParty.selectAll();
+  } else {
+    $gameParty.select(this.actor(this.index()));
+  }
+};
+
 const _Window_BattleEnemy_processHandling = Window_BattleEnemy.prototype.processHandling;
 Window_BattleEnemy.prototype.processHandling = function () {
   if (this.isOpenAndActive()) {
     const action = BattleManager.inputtingAction();
     if (action.canExpandScope() && Input.isTriggered(settings.switchScopeButton) && !this.cursorFixed()) {
       this.setCursorAll(!this._cursorAll);
-      if (this.cursorAll()) {
-        $gameTroop.selectAll();
-      } else {
-        $gameTroop.select(this.enemy());
-      }
     }
   }
   return _Window_BattleEnemy_processHandling.call(this);
+};
+
+const _Window_BattleEnemy_setCursorAll = Window_BattleEnemy.prototype.setCursorAll;
+Window_BattleEnemy.prototype.setCursorAll = function (cursorAll) {
+  _Window_BattleEnemy_setCursorAll.call(this, cursorAll);
+  if (cursorAll) {
+    $gameTroop.selectAll();
+  } else {
+    $gameTroop.select(this.enemy());
+  }
+};
+
+const _Window_BattleEnemy_refreshCursorForAll = Window_BattleEnemy.prototype.refreshCursorForAll;
+Window_BattleEnemy.prototype.refreshCursorForAll = function () {
+  const maxItems = this.maxItems();
+  if (maxItems > 0) {
+    const rect = this.itemRect(0);
+    [...Array(maxItems).keys()].forEach((index) => rect.enlarge(this.itemRect(index)));
+    this.setCursorRect(rect.x, rect.y, rect.width, rect.height);
+  } else {
+    _Window_BattleEnemy_refreshCursorForAll.call(this);
+  }
 };
 
 Window_BattleLog.prototype.startAction = function (subject, action, targets) {
