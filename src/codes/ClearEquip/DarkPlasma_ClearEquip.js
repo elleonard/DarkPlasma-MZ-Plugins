8bb6fe6ebf5b48e05220bf603243c36d1a75553c
plasma <librarian256@gmail.com>
2020-10-30 01:13:13 +0900

    2.1.0 プラグインコマンドを追加

--- src/codes/ClearEquip/DarkPlasma_ClearEquip.js
+++ src/codes/ClearEquip/DarkPlasma_ClearEquip.js
@@ -1,18 +1,54 @@
 import { pluginName } from './../../common/pluginName';
 import { settings } from './_build/DarkPlasma_ClearEquip_parameters';
 
+const PLUGIN_COMMAND_NAME = {
+  CLEAR_EQUIP: 'clearEquip',
+  CLEAR_ALL_MEMBER_EQUIP: 'clearAllMemberEquip',
+  CLEAR_ALL_EQUIP: 'clearAllEquip',
+  CLEAR_ALL_MEMBER_ALL_EQUIP: 'clearAllMemberAllEquip',
+};
+
 const _Game_Party_removeActor = Game_Party.prototype.removeActor;
 Game_Party.prototype.removeActor = function (actorId) {
   // パーティメンバーがはずれたときに装備をすべてはずす
-  if (settings.clearEquipWhenMemberIsOut && this._actors.contains(actorId)) {
+  if (settings.clearEquipWhenMemberIsOut && this._actors.includes(actorId)) {
     $gameActors.actor(actorId).clearEquipments();
   }
   _Game_Party_removeActor.call(this, actorId);
 };
 
-PluginManager.registerCommand(pluginName, 'clearEquip', function (args) {
+/**
+ * 指定した装備タイプIDの装備を可能であればはずす
+ * @param {number[]} equipTypes 装備タイプIDリスト
+ */
+Game_Actor.prototype.clearEquipByEquipTypes = function (equipTypes) {
+  [...Array(this.equipSlots().length).keys()]
+    .filter((slotId) => {
+      return equipTypes.includes(this.equipSlots()[slotId]) && this.isEquipChangeOk(slotId);
+    })
+    .forEach((slotId) => this.changeEquip(slotId, null));
+};
+
+PluginManager.registerCommand(pluginName, PLUGIN_COMMAND_NAME.CLEAR_EQUIP, function (args) {
+  const actor = $gameParty.members().find((actor) => actor.actorId() === Number(args.actorId));
+  if (actor) {
+    const equipTypes = JSON.parse(args.equipTypes).map((equipType) => Number(equipType));
+    actor.clearEquipByEquipTypes(equipTypes);
+  }
+});
+
+PluginManager.registerCommand(pluginName, PLUGIN_COMMAND_NAME.CLEAR_ALL_EQUIP, function (args) {
   const actor = $gameParty.members().find((actor) => actor.actorId() === Number(args.actorId));
   if (actor) {
     actor.clearEquipments();
   }
+});
+
+PluginManager.registerCommand(pluginName, PLUGIN_COMMAND_NAME.CLEAR_ALL_MEMBER_EQUIP, function (args) {
+  const equipTypes = JSON.parse(args.equipTypes).map((equipType) => Number(equipType));
+  $gameParty.members().forEach((actor) => actor.clearEquipByEquipTypes(equipTypes));
+});
+
+PluginManager.registerCommand(pluginName, PLUGIN_COMMAND_NAME.CLEAR_ALL_MEMBER_ALL_EQUIP, function () {
+  $gameParty.members().forEach((actor) => actor.clearEquipments());
 });
--- src/common/registerTestCase.js
+++ src/common/registerTestCase.js
@@ -9,3 +9,20 @@ import { targetPluginName } from './targetPluginName';
 export function registerTestCase(targetPluginVersion, testCaseName, isAuto) {
   $testTargetPlugins.addTestCase(targetPluginName, targetPluginVersion, testCaseName, isAuto);
 }
+
+/**
+ * 自動テストケースを登録する
+ * @param {string} targetPluginVersion 対象プラグインのバージョン
+ * @param {string[]} testCaseNames テストケース名一覧
+ */
+export function registerAutoTestCases(targetPluginVersion, testCaseNames) {
+  testCaseNames.forEach((testCaseName) => {
+    registerTestCase(targetPluginVersion, testCaseName, true);
+  });
+}
+
+export function registerManualTestCases(targetPluginVersion, testCaseNames) {
+  testCaseNames.forEach((testCaseName) => {
+    registerTestCase(targetPluginVersion, testCaseName, false);
+  });
+}
