51418ef5d31eb078e69d477bb6e118334da76800
elleonard <librarian256@gmail.com>
2021-01-04 17:04:19 +0900

    abc83e28a7cf9896fd8ffc548f84cf85d543efef

--- DarkPlasma_ForceFormation.js
+++ DarkPlasma_ForceFormation.js
@@ -1,10 +1,11 @@
-// DarkPlasma_ForceFormation 2.2.3
+// DarkPlasma_ForceFormation 2.2.4
 // Copyright (c) 2020 DarkPlasma
 // This software is released under the MIT license.
 // http://opensource.org/licenses/mit-license.php
 
 /**
- * 2021/01/04 2.2.3 null合体演算子が動作しないブラウザに対応
+ * 2021/01/04 2.2.4 正しく動作しない不具合を修正
+ *            2.2.3 null合体演算子が動作しないブラウザに対応
  *            2.2.2 全滅判定を飛ばした場合にも入れ替え無効スイッチを判定するよう修正
  * 2021/01/01 2.2.1 戦闘外の全滅判定に影響していた不具合を修正
  * 2020/12/31 2.2.0 強制入れ替え無効スイッチ設定を追加
@@ -47,7 +48,7 @@
  * @default 0
  *
  * @help
- * version: 2.2.3
+ * version: 2.2.4
  * 戦闘時 前衛が全滅したら強制的に後衛と入れ替えます。
  */
 
@@ -87,7 +88,7 @@
     }
     if (this._phase) {
       // 前衛が全滅していたら後衛と交代して戦闘続行
-      if ($gameParty.forwardMembersAreAllDead() && this.isForceFormationEnabled()) {
+      if ($gameParty.forwardMembersAreAllDead() && $gameParty.isForceFormationEnabled()) {
         $gameParty.forceFormation();
         this._logWindow.displayForceChangedFormation();
         if (settings.forceFormationCommonEvent > 0) {
