0c1b01ccfc1281bc313dda6353d00032c5cf5e56
elleonard <librarian256@gmail.com>
2021-04-16 23:46:09 +0900

    98fc887f84909858bb2cbfaead3ac7d6c1d24d70

---
+++ DarkPlasma_SeverImmortality.js
@@ -0,0 +1,96 @@
+// DarkPlasma_SeverImmortality 1.0.0
+// Copyright (c) 2021 DarkPlasma
+// This software is released under the MIT license.
+// http://opensource.org/licenses/mit-license.php
+
+/**
+ * 2021/04/16 1.0.0 公開
+ */
+
+/*:ja
+ * @plugindesc 倒した相手を復活不可にする行動
+ * @author DarkPlasma
+ * @license MIT
+ *
+ * @target MZ
+ * @url https://github.com/elleonard/DarkPlasma-MZ-Plugins/tree/release
+ *
+ * @help
+ * version: 1.0.0
+ * 下記のメモタグが設定されたスキルやアイテムで敵を倒すと、対象が復活不能になります。
+ *
+ * <severImmortality>
+ */
+
+(() => {
+  'use strict';
+
+  const _Game_Enemy_initMembers = Game_Enemy.prototype.initMembers;
+  Game_Enemy.prototype.initMembers = function () {
+    _Game_Enemy_initMembers.call(this);
+    this._cannotRevive = false;
+  };
+
+  Game_Battler.prototype.cannotRevive = function () {
+    return false;
+  };
+
+  Game_Enemy.prototype.cannotRevive = function () {
+    return this._cannotRevive;
+  };
+
+  Game_Enemy.prototype.setCannotRevive = function (cannotRevive) {
+    this._cannotRevive = cannotRevive;
+  };
+
+  const _Game_Enemy_recoverAll = Game_Enemy.prototype.recoverAll;
+  Game_Enemy.prototype.recoverAll = function () {
+    if (this.isDead() && this.cannotRevive()) {
+      return;
+    }
+    _Game_Enemy_recoverAll.call(this);
+  };
+
+  const _Game_Enemy_removeState = Game_Enemy.prototype.removeState;
+  Game_Enemy.prototype.removeState = function (stateId) {
+    if (stateId === this.deathStateId() && this.cannotRevive()) {
+      return;
+    }
+    _Game_Enemy_removeState.call(this, stateId);
+  };
+
+  const _Game_Enemy_gainHp = Game_Enemy.prototype.gainHp;
+  Game_Enemy.prototype.gainHp = function (value) {
+    if (this.hp === 0 && this.cannotRevive()) {
+      return;
+    }
+    _Game_Enemy_gainHp.call(this, value);
+  };
+
+  const _Game_Action_testItemEffect = Game_Action.prototype.testItemEffect;
+  Game_Action.prototype.testItemEffect = function (target, effect) {
+    const result = _Game_Action_testItemEffect.call(this, target, effect);
+    if (effect.code === Game_Action.prototype.EFFECT_REMOVE_STATE && effect.dataId === target.deathStateId()) {
+      return result && !target.cannotRevive();
+    }
+    return result;
+  };
+
+  const _Game_Action_apply = Game_Action.prototype.apply;
+  Game_Action.prototype.apply = function (target) {
+    _Game_Action_apply.call(this, target);
+    if (
+      target.result().isHit() &&
+      this.item().damage.type > 0 &&
+      target.isEnemy() &&
+      target.isDead() &&
+      this.isSealingRevive()
+    ) {
+      target.setCannotRevive(true);
+    }
+  };
+
+  Game_Action.prototype.isSealingRevive = function () {
+    return !!this.item().meta.severImmortality;
+  };
+})();
