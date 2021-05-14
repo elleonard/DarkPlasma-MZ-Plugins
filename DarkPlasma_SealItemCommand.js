095282ab846cf347bd32d450f5c05f1e45bb7901
elleonard <librarian256@gmail.com>
2021-05-14 23:40:02 +0900

    4e83a08913db0e567c8dbdee7603b4b6d3b321b3

---
+++ DarkPlasma_SealItemCommand.js
@@ -0,0 +1,51 @@
+// DarkPlasma_SealItemCommand 1.0.0
+// Copyright (c) 2021 DarkPlasma
+// This software is released under the MIT license.
+// http://opensource.org/licenses/mit-license.php
+
+/**
+ * 2021/05/14 1.0.0 公開
+ */
+
+/*:ja
+ * @plugindesc アイテムコマンドを封印する
+ * @author DarkPlasma
+ * @license MIT
+ *
+ * @target MZ
+ * @url https://github.com/elleonard/DarkPlasma-MZ-Plugins/tree/release
+ *
+ * @help
+ * version: 1.0.0
+ * メモ欄に<sealItemCommand>と入力したマップにおいて、
+ * アイテムコマンドを戦闘/メニュー共に使用不可にします。
+ */
+
+(() => {
+  'use strict';
+
+  Game_Map.prototype.isItemCommandEnabled = function () {
+    return !$dataMap || !$dataMap.meta.sealItemCommand;
+  };
+
+  const _Window_MenuCommand_makeCommandList = Window_MenuCommand.prototype.makeCommandList;
+  Window_MenuCommand.prototype.makeCommandList = function () {
+    _Window_MenuCommand_makeCommandList.call(this);
+    const itemCommand = this.itemCommand();
+    if (itemCommand && !$gameMap.isItemCommandEnabled()) {
+      itemCommand.enabled = false;
+    }
+  };
+
+  const _Window_ActorCommand_addItemCommand = Window_ActorCommand.prototype.addItemCommand;
+  Window_ActorCommand.prototype.addItemCommand = function () {
+    _Window_ActorCommand_addItemCommand.call(this);
+    if (!$gameMap.isItemCommandEnabled()) {
+      this._list.find((command) => command.symbol === 'item').enabled = false;
+    }
+  };
+
+  Window_Command.prototype.itemCommand = function () {
+    return this._list.find((command) => command.symbol === 'item');
+  };
+})();
