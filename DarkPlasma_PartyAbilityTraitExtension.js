cd9b4c1d1da362bd32a96c595ec175a4c88622fe
elleonard <librarian256@gmail.com>
2021-06-27 19:02:13 +0900

    1b94cd132be14acb02cf44a313f6d3e147212a28

---
+++ DarkPlasma_PartyAbilityTraitExtension.js
@@ -0,0 +1,180 @@
+// DarkPlasma_PartyAbilityTraitExtension 1.0.0
+// Copyright (c) 2021 DarkPlasma
+// This software is released under the MIT license.
+// http://opensource.org/licenses/mit-license.php
+
+/**
+ * 2021/06/27 1.0.0 公開
+ */
+
+/*:ja
+ * @plugindesc パーティ能力特徴を追加する
+ * @author DarkPlasma
+ * @license MIT
+ *
+ * @target MZ
+ * @url https://github.com/elleonard/DarkPlasma-MZ-Plugins/tree/release
+ *
+ * @help
+ * version: 1.0.0
+ * パーティ能力特徴を追加します。
+ * アクター/職業/装備/ステートのメモ欄に指定の記述を行うことで、
+ * パーティ全体に効果を及ぼす特徴を付与できます。
+ *
+ * 基本構文:
+ * <partyAbility:[effect]:[value]>
+ * パーティ全体に[effect]で指定した効果を、効果量[value]で付与します。
+ *
+ * [effect]:
+ *   mhp: 最大HP加算
+ *   mmp: 最大MP加算
+ *   atk: 攻撃力加算
+ *   def: 防御力加算
+ *   mat: 魔法攻撃力加算
+ *   mdf: 魔法防御力加算
+ *   agi: 敏捷性加算
+ *   luk: 運加算
+ *   tgr: 狙われ率乗算
+ *   grd: 防御効果率乗算
+ *   rec: 回復効果率乗算
+ *   pha: 薬の知識乗算
+ *   mcr: MP消費率乗算
+ *   tcr: TPチャージ率乗算
+ *   phr: 物理ダメージ率乗算
+ *   mdr: 魔法ダメージ率乗算
+ *   fdr: 床ダメージ率乗算
+ *   exr: 経験値獲得率乗算
+ *
+ * 設定例
+ * 最大HP+10:
+ * <partyAbility:mhp:10>
+ *
+ * 床ダメージ率*0:
+ * <partyAbility:fdr:0>
+ */
+
+(() => {
+  'use strict';
+
+  const _Game_Actor_paramBasePlus = Game_Actor.prototype.paramBasePlus;
+  Game_Actor.prototype.paramBasePlus = function (paramId) {
+    return Math.max(0, _Game_Actor_paramBasePlus.call(this, paramId) + this.paramPlusByPartyAbility(paramId));
+  };
+
+  /**
+   * ステータス計算用の一時パーティをセットする
+   * 一時アクターにのみセットされるため、この値はセーブデータには含まれない
+   * @param {Game_Party} tempParty ステータス計算用一時パーティ
+   */
+  Game_Actor.prototype.setTempParty = function (tempParty) {
+    this._tempParty = tempParty;
+  };
+
+  Game_Actor.prototype.paramPlusByPartyAbility = function (paramId) {
+    const party = this._tempParty ? this._tempParty : $gameParty;
+    return party.paramPlusByPartyAbility(paramId);
+  };
+
+  const _Game_Actor_sparam = Game_Actor.prototype.sparam;
+  Game_Actor.prototype.sparam = function (paramId) {
+    return _Game_Actor_sparam.call(this, paramId) * this.sparamRateByPartyAbility(paramId);
+  };
+
+  Game_Actor.prototype.sparamRateByPartyAbility = function (paramId) {
+    return $gameParty.sparamRateByPartyAbility(paramId);
+  };
+
+  /**
+   * 対象の特徴にある加算式のパーティ能力の合計値
+   * @param {string} key パラメータ名を表すキー
+   * @return {number}
+   */
+  Game_Actor.prototype.partyAbilityTraitsSum = function (key) {
+    return this.traitObjects().reduce((result, object) => result + partyAbilityTraitAdd(object, key), 0);
+  };
+
+  /**
+   * 対象の特徴にある乗算式のパーティ能力を全てかけた値
+   * @param {string} key パラメータ名を表すキー
+   * @return {number}
+   */
+  Game_Actor.prototype.partyAbilityTraitsPi = function (key) {
+    return this.traitObjects().reduce((result, object) => result * partyAbilityTraitMulti(object, key), 1);
+  };
+
+  /**
+   * 指定した通常能力値を加算するパーティ能力のパーティ全体の合計
+   * @param {number} paramId パラメータID
+   * @return {number}
+   */
+  Game_Party.prototype.paramPlusByPartyAbility = function (paramId) {
+    return this.allMembers().reduce((result, actor) => result + actor.partyAbilityTraitsSum(paramKey(paramId)), 0);
+  };
+
+  /**
+   * 指定した特殊能力地を乗算するパーティ能力のパーティ全体の倍率
+   * @param {number} paramId パラメータID
+   * @return {number}
+   */
+  Game_Party.prototype.sparamRateByPartyAbility = function (paramId) {
+    return this.allMembers().reduce((result, actor) => result * actor.partyAbilityTraitsPi(sparamKey(paramId)), 1);
+  };
+
+  const _Window_EquipStatus_setTempActor = Window_EquipStatus.prototype.setTempActor;
+  Window_EquipStatus.prototype.setTempActor = function (tempActor) {
+    if (this._tempActor !== tempActor && tempActor) {
+      /**
+       * 表示ステータス計算用の一時パーティ生成
+       */
+      const party = new Game_Party();
+      $gameParty.allMembers().forEach((actor) => party.addActor(actor.actorId()));
+      party.allMembers = function () {
+        return this._actors.map((id) => (id === tempActor.actorId() ? tempActor : $gameActors.actor(id)));
+      };
+      tempActor.setTempParty(party);
+    }
+    _Window_EquipStatus_setTempActor.call(this, tempActor);
+  };
+
+  /**
+   * 対象に設定された、加算方式のパーティ能力値を取得する
+   * @param {MZ.Actor|MZ.Class|MZ.Weapon|MZ.Armor|MZ.State} object 対象
+   * @param {string} key キー
+   * @return {number}
+   */
+  function partyAbilityTraitAdd(object, key) {
+    const match = new RegExp(`${key}:([0-9]+)`).exec(object.meta.partyAbility);
+    return match && match.length > 1 ? Number(match[1]) : 0;
+  }
+
+  /**
+   * 対象に設定された、乗算方式のパーティ能力値を取得する
+   * @param {MZ.Actor|MZ.Class|MZ.Weapon|MZ.Armor|MZ.State} object 対象
+   * @param {string} key キー
+   * @return {number}
+   */
+  function partyAbilityTraitMulti(object, key) {
+    const match = new RegExp(`${key}:([0-9]+)`).exec(object.meta.partyAbility);
+    return match && match.length > 1 ? Number(match[1]) : 1;
+  }
+
+  const PARAM_KEYS = ['mhp', 'mmp', 'atk', 'def', 'mat', 'mdf', 'agi', 'luk'];
+
+  const SPARAM_KEYS = ['tgr', 'grd', 'rec', 'pha', 'mcr', 'tcr', 'pdr', 'mdr', 'fdr', 'exr'];
+
+  /**
+   * @param {number} paramId パラメータID
+   * @return {string}
+   */
+  function paramKey(paramId) {
+    return PARAM_KEYS[paramId];
+  }
+
+  /**
+   * @param {number} paramId パラメータID
+   * @return {string}
+   */
+  function sparamKey(paramId) {
+    return SPARAM_KEYS[paramId];
+  }
+})();
---
+++ DarkPlasma_TitleCommand.js
@@ -0,0 +1,139 @@
+// DarkPlasma_TitleCommand 1.0.0
+// Copyright (c) 2021 DarkPlasma
+// This software is released under the MIT license.
+// http://opensource.org/licenses/mit-license.php
+
+/**
+ * 2021/06/27 1.0.0 公開
+ */
+
+/*:ja
+ * @plugindesc タイトルコマンドを設定する
+ * @author DarkPlasma
+ * @license MIT
+ *
+ * @target MZ
+ * @url https://github.com/elleonard/DarkPlasma-MZ-Plugins/tree/release
+ *
+ * @param additionalCommands
+ * @desc 追加のタイトルコマンド一覧を設定します。
+ * @text 追加コマンド
+ * @type struct<TitleCommand>[]
+ * @default []
+ *
+ * @help
+ * version: 1.0.0
+ * タイトルコマンドを設定します。
+ */
+/*~struct~TitleCommand:
+ * @param commandType
+ * @desc コマンドの種別を設定します。
+ * @text コマンド種別
+ * @type select
+ * @option シーン遷移
+ * @value 1
+ * @option シャットダウン
+ * @value 2
+ *
+ * @param position
+ * @desc 追加位置を数値インデックスで指定します。0で最上部（ニューゲームの上）、3でオプションの下に追加されます。
+ * @text 追加位置
+ * @type number
+ * @default 3
+ *
+ * @param text
+ * @desc タイトルコマンドのテキストを指定します。
+ * @text テキスト
+ * @type string
+ *
+ * @param symbol
+ * @desc コマンドを一意に識別する文字列を指定します。
+ * @text 識別子
+ * @type string
+ *
+ * @param scene
+ * @desc 遷移先のシーン名を指定します。コマンド種別がシーン遷移の場合のみ有効です。
+ * @text シーン名
+ * @type string
+ */
+(() => {
+  'use strict';
+
+  const pluginName = decodeURIComponent(document.currentScript.src.match(/^.*\/plugins\/(.*)\.js$/)[1]);
+
+  const pluginParameters = PluginManager.parameters(pluginName);
+
+  const settings = {
+    additionalCommands: JSON.parse(pluginParameters.additionalCommands || '[]').map((e) => {
+      return ((parameter) => {
+        const parsed = JSON.parse(parameter);
+        return {
+          commandType: Number(parsed.commandType || 0),
+          position: Number(parsed.position || 3),
+          text: String(parsed.text || ''),
+          symbol: String(parsed.symbol || ''),
+          scene: String(parsed.scene || ''),
+        };
+      })(e || '{}');
+    }),
+  };
+
+  function selectedScene(symbol) {
+    const command = settings.additionalCommands.find((command) => command.symbol === symbol);
+    return window[command.scene] || null;
+  }
+
+  const _Scene_Title_createCommandWindow = Scene_Title.prototype.createCommandWindow;
+  Scene_Title.prototype.createCommandWindow = function () {
+    _Scene_Title_createCommandWindow.call(this);
+    settings.additionalCommands.forEach((command) => {
+      const handler = command.commandType === 1 ? this.commandSceneChange.bind(this) : this.commandShutdown.bind(this);
+      this._commandWindow.setHandler(command.symbol, handler);
+    });
+  };
+
+  const _Scene_Title_commandWindowRect = Scene_Title.prototype.commandWindowRect;
+  Scene_Title.prototype.commandWindowRect = function () {
+    const rect = _Scene_Title_commandWindowRect.call(this);
+    return new Rectangle(
+      rect.x,
+      rect.y,
+      rect.width,
+      this.calcWindowHeight(3 + settings.additionalCommands.length, true)
+    );
+  };
+
+  Scene_Title.prototype.commandSceneChange = function () {
+    this._commandWindow.close();
+    const scene = selectedScene(this._commandWindow.currentSymbol());
+    if (!scene) {
+      throw `シンボル ${this._commandWindow.currentSymbol()} に無効なシーンが指定されています。`;
+    }
+    SceneManager.push(scene);
+  };
+
+  Scene_Title.prototype.commandShutdown = function () {
+    if (StorageManager.isLocalMode()) {
+      window.close();
+    } else {
+      window.open('about:blank', '_self').close();
+    }
+  };
+
+  const _Window_TitleCommand_makeCommandList = Window_TitleCommand.prototype.makeCommandList;
+  Window_TitleCommand.prototype.makeCommandList = function () {
+    _Window_TitleCommand_makeCommandList.call(this);
+    settings.additionalCommands.forEach((command) => {
+      this.addCommandAt(command.position, command.text, command.symbol);
+    });
+  };
+
+  Window_TitleCommand.prototype.addCommandAt = function (index, name, symbol, enabled = true, ext = null) {
+    this._list.splice(index, 0, {
+      name: name,
+      symbol: symbol,
+      enabled: enabled,
+      ext: ext,
+    });
+  };
+})();
