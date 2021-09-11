20a117b3eb90a172c5e5239522c7502e8567cdf2
plasma <librarian256@gmail.com>
2021-09-11 23:36:36 +0900

    1.0.1 クラスを上書きしない

--- src/codes/FilterEquip_RecentlyGained/DarkPlasma_FilterEquip_RecentlyGained.js
+++ src/codes/FilterEquip_RecentlyGained/DarkPlasma_FilterEquip_RecentlyGained.js
@@ -1,9 +1,10 @@
 import { pluginName } from '../../common/pluginName';
 import { settings } from './_build/DarkPlasma_FilterEquip_RecentlyGained_parameters';
 
-Game_Party = class extends Game_Party {
-  gainItem(item, amount, includeEquip) {
-    super.gainItem(item, amount, includeEquip);
+function Game_Party_FilterEquipRecentlyGainedMixIn(gameParty) {
+  const _gainItem = gameParty.gainItem;
+  gameParty.gainItem = function (item, amount, includeEquip) {
+    _gainItem.call(this, item, amount, includeEquip);
     if (amount > 0) {
       if (DataManager.isWeapon(item)) {
         this.pushGainWeaponHistory(item);
@@ -11,12 +12,12 @@ Game_Party = class extends Game_Party {
         this.pushGainArmorHistory(item);
       }
     }
-  }
+  };
 
   /**
    * @param {MZ.Weapon} weapon 武器データ
    */
-  pushGainWeaponHistory(weapon) {
+  gameParty.pushGainWeaponHistory = function (weapon) {
     if (!this._gainWeaponHistory) {
       this._gainWeaponHistory = [];
     }
@@ -24,12 +25,12 @@ Game_Party = class extends Game_Party {
     if (this._gainWeaponHistory.length > settings.threshold) {
       this._gainWeaponHistory.shift();
     }
-  }
+  };
 
   /**
    * @param {MZ.Armor} armor 防具データ
    */
-  pushGainArmorHistory(armor) {
+  gameParty.pushGainArmorHistory = function (armor) {
     if (!this._gainArmorHistory) {
       this._gainArmorHistory = [];
     }
@@ -37,60 +38,61 @@ Game_Party = class extends Game_Party {
     if (this._gainArmorHistory.length > settings.threshold) {
       this._gainArmorHistory.shift();
     }
-  }
+  };
 
   /**
    * 最近入手した武器ID一覧
    * @return {number[]}
    */
-  gainWeaponHistory() {
+  gameParty.gainWeaponHistory = function () {
     return this._gainWeaponHistory || [];
-  }
+  };
 
   /**
    * 最近入手した防具ID一覧
    * @return {number[]}
    */
-  gainArmorHistory() {
+  gameParty.gainArmorHistory = function () {
     return this._gainArmorHistory || [];
-  }
+  };
 
   /**
    * 最近入手したアイテムかどうか
    * @param {MZ.Item|MZ.Weapon|MZ.Armor} item アイテムデータ
    * @return {boolean}
    */
-  isRecentlyGainded(item) {
+  gameParty.isRecentlyGainded = function (item) {
     if (DataManager.isWeapon(item)) {
       return this.gainWeaponHistory().includes(item.id);
     } else if (DataManager.isArmor(item)) {
       return this.gainArmorHistory().includes(item.id);
     }
     return false;
-  }
-};
+  };
+}
+
+Game_Party_FilterEquipRecentlyGainedMixIn(Game_Party.prototype);
 
 const traitIds = [];
 
-Scene_Equip = class extends Scene_Equip {
-  equipFilterBuilder(equips) {
-    const ALLOCATION_TRAIT_ID_RECENTLY_GAINED = 0;
-    traitIds[ALLOCATION_TRAIT_ID_RECENTLY_GAINED] = EquipFilterBuilder.allocateUniqueTraitId(
-      pluginName,
-      settings.traitName,
-      ALLOCATION_TRAIT_ID_RECENTLY_GAINED
-    );
-    const builder = super.equipFilterBuilder(equips);
-    return builder.withTrait(traitIds[ALLOCATION_TRAIT_ID_RECENTLY_GAINED]).withEquipToTraitsRule((equip) => {
-      return $gameParty.isRecentlyGainded(equip)
-        ? [
-            {
-              code: traitIds[ALLOCATION_TRAIT_ID_RECENTLY_GAINED],
-              dataId: 1, // dummy
-              value: 1, // dummy
-            },
-          ]
-        : [];
-    });
-  }
+const _Scene_Equip_equipFilterBuilder = Scene_Equip.prototype.equipFilterBuilder;
+Scene_Equip.prototype.equipFilterBuilder = function (equips) {
+  const ALLOCATION_TRAIT_ID_RECENTLY_GAINED = 0;
+  traitIds[ALLOCATION_TRAIT_ID_RECENTLY_GAINED] = EquipFilterBuilder.allocateUniqueTraitId(
+    pluginName,
+    settings.traitName,
+    ALLOCATION_TRAIT_ID_RECENTLY_GAINED
+  );
+  const builder = _Scene_Equip_equipFilterBuilder(equips);
+  return builder.withTrait(traitIds[ALLOCATION_TRAIT_ID_RECENTLY_GAINED]).withEquipToTraitsRule((equip) => {
+    return $gameParty.isRecentlyGainded(equip)
+      ? [
+          {
+            code: traitIds[ALLOCATION_TRAIT_ID_RECENTLY_GAINED],
+            dataId: 1, // dummy
+            value: 1, // dummy
+          },
+        ]
+      : [];
+  });
 };
