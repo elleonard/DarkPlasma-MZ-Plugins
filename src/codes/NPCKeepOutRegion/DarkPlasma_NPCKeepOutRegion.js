5462588edda7e2750fbb028709ad1bfc043bebea
plasma <librarian256@gmail.com>
2021-11-09 10:09:59 +0900

    1.1.0 移動制限無視をメモ欄で制御

--- src/codes/NPCKeepOutRegion/DarkPlasma_NPCKeepOutRegion.js
+++ src/codes/NPCKeepOutRegion/DarkPlasma_NPCKeepOutRegion.js
@@ -1,21 +1,30 @@
 import { settings } from './_build/DarkPlasma_NPCKeepOutRegion_parameters';
 
-const MOVE_TYPE_CUSTOM = 3;
-
 Game_Map.prototype.isEventKeepOutRegion = function (x, y) {
   return settings.regions.includes(this.regionId(x, y));
 };
 
-const _Game_Event_isMapPassable = Game_Event.prototype.isMapPassable;
-Game_Event.prototype.isMapPassable = function (x, y, d) {
-  const x2 = $gameMap.roundXWithDirection(x, d);
-  const y2 = $gameMap.roundYWithDirection(y, d);
-  if (this.isMoveRangeLimited() && $gameMap.isEventKeepOutRegion(x2, y2)) {
-    return false;
-  }
-  return _Game_Event_isMapPassable.call(this, x, y, d);
-};
+/**
+ * @param {Game_Event.prototype} gameEvent
+ */
+function Game_Event_KeepOutRegionMixIn(gameEvent) {
+  const _isMapPassable = gameEvent.isMapPassable;
+  gameEvent.isMapPassable = function (x, y, d) {
+    const x2 = $gameMap.roundXWithDirection(x, d);
+    const y2 = $gameMap.roundYWithDirection(y, d);
+    if (!this.ignoreKeepOut() && $gameMap.isEventKeepOutRegion(x2, y2)) {
+      return false;
+    }
+    return _isMapPassable.call(this, x, y, d);
+  };
 
-Game_Event.prototype.isMoveRangeLimited = function () {
-  return this._moveType !== MOVE_TYPE_CUSTOM && !this.isMoveRouteForcing();
-};
+  gameEvent.ignoreKeepOut = function () {
+    if (!this.event()) {
+      return false;
+    }
+    const selfSwitchCh = this.event().meta.ignoreKeepOut;
+    return selfSwitchCh === true || $gameSelfSwitches.value([this._mapId, this._eventId, selfSwitchCh]);
+  };
+}
+
+Game_Event_KeepOutRegionMixIn(Game_Event.prototype);
