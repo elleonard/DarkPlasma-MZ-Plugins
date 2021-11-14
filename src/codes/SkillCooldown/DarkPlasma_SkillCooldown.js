d3ba1a97105e7d53c4aff0fc5da7afa884177abf
plasma <librarian256@gmail.com>
2021-11-14 20:40:10 +0900

    2.0.4 戦闘中にパーティメンバーを追加するとエラー発生する不具合を修正

--- src/codes/SkillCooldown/DarkPlasma_SkillCooldown.js
+++ src/codes/SkillCooldown/DarkPlasma_SkillCooldown.js
@@ -1,108 +1,5 @@
 import { settings } from './_build/DarkPlasma_SkillCooldown_parameters';
 
-class SkillCooldownSettings {
-  constructor(cooldownSettings) {
-    this._cooldownSettings = cooldownSettings;
-  }
-
-  static fromJson(json) {
-    const parsed = JsonEx.parse(json);
-    return new SkillCooldownSettings(parsed.map((setting) => SkillCooldownSetting.fromJson(setting)));
-  }
-
-  /**
-   * @param {object} object オブジェクト
-   * @return {SkillCooldownSettings}
-   */
-  static fromObject(object) {
-    return new SkillCooldownSettings(object.map((cooldownSetting) => SkillCooldownSetting.fromObject(cooldownSetting)));
-  }
-
-  /**
-   * トリガースキルIDからクールタイム設定を取得する
-   * @param {number} triggerSkillId トリガースキルID
-   * @return {SkillCooldownSetting}
-   */
-  getSkillCooldownSetting(triggerSkillId) {
-    return this._cooldownSettings.find((setting) => setting.triggerSkillId === triggerSkillId);
-  }
-}
-
-class SkillCooldownSetting {
-  constructor(triggerSkillId, targets) {
-    this._triggerSkillId = triggerSkillId;
-    this._targets = targets;
-  }
-
-  static fromJson(json) {
-    const parsed = JsonEx.parse(json);
-    return new SkillCooldownSetting(
-      Number(parsed['Trigger SKill Id'] || 0),
-      JsonEx.parse(parsed['Target Skills'] || '[]').map((target) => {
-        return SkillCooldownTargetSetting.fromJson(target);
-      })
-    );
-  }
-
-  /**
-   * @param {object} object オブジェクト
-   * @return {SkillCooldownSetting}
-   */
-  static fromObject(object) {
-    return new SkillCooldownSetting(
-      object.triggerSkillId,
-      object.targetSkills.map((targetSkill) => SkillCooldownTargetSetting.fromObject(targetSkill))
-    );
-  }
-
-  get triggerSkillId() {
-    return this._triggerSkillId;
-  }
-
-  get targets() {
-    return this._targets;
-  }
-}
-
-class SkillCooldownTargetSetting {
-  /**
-   * @param {number} skillId スキルID
-   * @param {number} turnCount ターン数
-   */
-  constructor(skillId, turnCount) {
-    this._skillId = skillId;
-    this._turnCount = turnCount;
-  }
-
-  /**
-   * @param {string} json JSON文字列
-   * @return {SkillCooldownTargetSetting}
-   */
-  static fromJson(json) {
-    const parsed = JsonEx.parse(json);
-    return new SkillCooldownTargetSetting(
-      Number(parsed['Target Skill Id'] || 0),
-      Number(parsed['Cooldown Turn Count'] || 3)
-    );
-  }
-
-  /**
-   * @param {object} object オブジェクト
-   * @return {SkillCooldownTargetSetting}
-   */
-  static fromObject(object) {
-    return new SkillCooldownTargetSetting(object.targetSkillId, object.cooldownTurnCount);
-  }
-
-  get skillId() {
-    return this._skillId;
-  }
-
-  get turnCount() {
-    return this._turnCount;
-  }
-}
-
 class SkillCooldown {
   /**
    * @param {number} skillId スキルID
@@ -118,9 +15,13 @@ class SkillCooldown {
    * @return {SkillCooldown[]}
    */
   static setup(triggerSkillId) {
-    const cooldownSetting = skillCooldownSettings.getSkillCooldownSetting(triggerSkillId);
+    const cooldownSetting = settings.skillCooldownSettings.find(
+      (cooldown) => cooldown.triggerSkillId === triggerSkillId
+    );
     return cooldownSetting
-      ? cooldownSetting.targets.map((target) => new SkillCooldown(target.skillId, target.turnCount + 1))
+      ? cooldownSetting.targetSkills.map(
+          (target) => new SkillCooldown(target.targetSkillId, target.cooldownTurnCount + 1)
+        )
       : [];
   }
 
@@ -185,11 +86,35 @@ class SkillCooldownManager {
   }
 
   /**
-   * クールダウン開始
-   * @param {SkillCooldown[]} targetCooldowns クールダウンオブジェクト
-   * @param {MZ.Skill} skill スキルデータ
+   * @param {number} actorId
+   * @return {SkillCooldown[]}
    */
-  setupCooldownTurn(targetCooldowns, skill) {
+  actorsCooldowns(actorId) {
+    if (!this._actorsSkillCooldowns[actorId]) {
+      this._actorsSkillCooldowns[actorId] = [];
+    }
+    return this._actorsSkillCooldowns[actorId];
+  }
+
+  /**
+   * @param {number} index
+   * @return {SkillCooldown[]}
+   */
+  enemysCooldowns(index) {
+    if (!this._enemysSkillCooldowns[index]) {
+      this._enemysSkillCooldowns[index] = [];
+    }
+    return this._enemysSkillCooldowns[index];
+  }
+
+  /**
+   * クールダウン開始
+   * @param {number} id
+   * @param {MZ.Skill} skill スキルデータ
+   * @param {boolean} isActor
+   */
+  setupCooldownTurn(id, skill, isActor) {
+    const targetCooldowns = isActor ? this.actorsCooldowns(id) : this.enemysCooldowns(id);
     const cooldowns = SkillCooldown.setup(skill.id);
     cooldowns.forEach((cooldown) => {
       targetCooldowns[cooldown.skillId] = cooldown;
@@ -197,74 +122,29 @@ class SkillCooldownManager {
   }
 
   /**
-   * アクターのクールダウン開始
-   * @param {number} actorId アクターID
-   * @param {MZ.Skill} skill スキルデータ
-   */
-  setupActorsCooldownTurn(actorId, skill) {
-    this.setupCooldownTurn(this._actorsSkillCooldowns[actorId], skill);
-  }
-
-  /**
-   * エネミーのクールダウン開始
-   * @param {number} index 編成の中での敵番号
-   * @param {MZ.Skill} skill スキルデータ
-   */
-  setupEnemysCooldownTurn(index, skill) {
-    this.setupCooldownTurn(this._enemysSkillCooldowns[index], skill);
-  }
-
-  /**
    * クールダウン中かどうか
-   * @param {SkillCooldown[]} targetCooldowns クールダウンオブジェクト
+   * @param {number} id
    * @param {MZ.Skill} skill スキルデータ
+   * @param {boolean} isActor
    * @return {boolean}
    */
-  isDuringCooldown(targetCooldowns, skill) {
+  isDuringCooldown(id, skill, isActor) {
+    const targetCooldowns = isActor ? this.actorsCooldowns(id) : this.enemysCooldowns(id);
     const cooldown = targetCooldowns[skill.id];
     return cooldown ? !cooldown.isFinished() : false;
   }
 
   /**
-   * アクターのスキルがクールダウン中かどうか
-   * @param {number} actorId アクターID
-   * @param {MZ.Skill} skill スキルデータ
-   */
-  isActorDuringCooldown(actorId, skill) {
-    if (!$gameParty.inBattle()) {
-      return false;
-    }
-    return this.isDuringCooldown(this._actorsSkillCooldowns[actorId], skill);
-  }
-
-  /**
-   * エネミーのスキルがクールダウン中かどうか
-   * @param {number} index 編成の中での敵番号
-   * @param {MZ.Skill} skill スキルデータ
-   */
-  isEnemyDuringCooldown(index, skill) {
-    return this.isDuringCooldown(this._enemysSkillCooldowns[index], skill);
-  }
-
-  /**
    * 残りクールダウンターン数を返す
-   * @param {SkillCooldown[]} targetCooldowns クールダウンオブジェクト
+   * @param {number} id
    * @param {MZ.Skill} skill スキルデータ
+   * @param {boolean} isActor
    * @return {number}
    */
-  cooldownTurn(targetCooldowns, skill) {
+  cooldownTurn(id, skill, isActor) {
+    const targetCooldowns = isActor ? this.actorsCooldowns(id) : this.enemysCooldowns(id);
     const cooldown = targetCooldowns[skill.id];
     return cooldown ? cooldown.turnCount : 0;
-  }
-
-  /**
-   * 残りクールダウンターン数を返す
-   * @param {number} actorId アクターID
-   * @param {MZ.Skill} skill スキルデータ
-   * @return {number}
-   */
-  actorsCooldownTurn(actorId, skill) {
-    return this.cooldownTurn(this._actorsSkillCooldowns[actorId], skill);
   }
 
   /**
@@ -279,80 +159,7 @@ class SkillCooldownManager {
   }
 }
 
-const skillCooldownSettings = SkillCooldownSettings.fromObject(settings.skillCooldownSettings);
-
 const skillCooldownManager = new SkillCooldownManager();
-
-const _Game_Battler_useItem = Game_Battler.prototype.useItem;
-Game_Battler.prototype.useItem = function (item) {
-  _Game_Battler_useItem.call(this, item);
-  if (DataManager.isSkill(item) && $gameParty.inBattle()) {
-    this.setupCooldownTurn(item);
-  }
-};
-
-/**
- * スキルクールタイムを開始する
- * @param {MZ.Skill} skill スキルデータ
- */
-Game_BattlerBase.prototype.setupCooldownTurn = function (skill) {};
-
-/**
- * スキルクールタイムを開始する
- * @param {MZ.Skill} skill スキルデータ
- */
-Game_Actor.prototype.setupCooldownTurn = function (skill) {
-  skillCooldownManager.setupActorsCooldownTurn(this.actorId(), skill);
-};
-
-/**
- * スキルクールタイムを開始する
- * @param {MZ.Skill} skill スキルデータ
- */
-Game_Enemy.prototype.setupCooldownTurn = function (skill) {
-  skillCooldownManager.setupEnemysCooldownTurn(this.index(), skill);
-};
-
-const _Game_BattlerBase_meetsSkillConditions = Game_BattlerBase.prototype.meetsSkillConditions;
-Game_BattlerBase.prototype.meetsSkillConditions = function (skill) {
-  return _Game_BattlerBase_meetsSkillConditions.call(this, skill) && !this.isDuringCooldown(skill);
-};
-
-/**
- * 指定したスキルのクールタイム中であるかどうか
- * @param {MZ.Skill} skill スキルデータ
- * @return {boolean}
- */
-Game_BattlerBase.prototype.isDuringCooldown = function (skill) {
-  return false;
-};
-
-/**
- * 指定したスキルのクールタイム中であるかどうか
- * @param {MZ.Skill} skill スキルデータ
- * @return {boolean}
- */
-Game_Actor.prototype.isDuringCooldown = function (skill) {
-  return skillCooldownManager.isActorDuringCooldown(this.actorId(), skill);
-};
-
-/**
- * 指定したスキルのクールタイム中であるかどうか
- * @param {MZ.Skill} skill スキルデータ
- * @return {boolean}
- */
-Game_Enemy.prototype.isDuringCooldown = function (skill) {
-  return skillCooldownManager.isEnemyDuringCooldown(this.index(), skill);
-};
-
-/**
- * 指定したスキルの残りクールタイムを返す
- * @param {MZ.Skill} skill スキルデータ
- * @return {number}
- */
-Game_Actor.prototype.cooldownTurn = function (skill) {
-  return skillCooldownManager.actorsCooldownTurn(this.actorId(), skill);
-};
 
 const _BattleManager_startBattle = BattleManager.startBattle;
 BattleManager.startBattle = function () {
@@ -364,6 +171,64 @@ const _BattleManager_endTurn = BattleManager.endTurn;
 BattleManager.endTurn = function () {
   _BattleManager_endTurn.call(this);
   skillCooldownManager.decreaseCooldownTurns();
+};
+
+/**
+ * @param {Game_BattlerBase.prototype} gameBattlerBase
+ */
+function Game_BattlerBase_SkillCooldownMixIn(gameBattlerBase) {
+  const _meetsSkillConditions = gameBattlerBase.meetsSkillConditions;
+  gameBattlerBase.meetsSkillConditions = function (skill) {
+    return _meetsSkillConditions.call(this, skill) && !this.isDuringCooldown(skill);
+  };
+
+  /**
+   * スキルクールタイムを開始する
+   * @param {MZ.Skill} skill スキルデータ
+   */
+  gameBattlerBase.setupCooldownTurn = function (skill) {
+    skillCooldownManager.setupCooldownTurn(this.skillCooldownId(), skill, this.isActor());
+  };
+
+  /**
+   * 指定したスキルのクールタイム中であるかどうか
+   * @param {MZ.Skill} skill スキルデータ
+   * @return {boolean}
+   */
+  gameBattlerBase.isDuringCooldown = function (skill) {
+    return skillCooldownManager.isDuringCooldown(this.skillCooldownId(), skill, this.isActor());
+  };
+
+  /**
+   * 指定したスキルの残りクールタイムを返す
+   * @param {MZ.Skill} skill スキルデータ
+   * @return {number}
+   */
+  gameBattlerBase.cooldownTurn = function (skill) {
+    return skillCooldownManager.cooldownTurn(this.skillCooldownId(), skill, this.isActor());
+  };
+
+  gameBattlerBase.skillCooldownId = function () {
+    return 0;
+  };
+}
+
+Game_BattlerBase_SkillCooldownMixIn(Game_BattlerBase.prototype);
+
+const _Game_Battler_useItem = Game_Battler.prototype.useItem;
+Game_Battler.prototype.useItem = function (item) {
+  _Game_Battler_useItem.call(this, item);
+  if (DataManager.isSkill(item) && $gameParty.inBattle()) {
+    this.setupCooldownTurn(item);
+  }
+};
+
+Game_Actor.prototype.skillCooldownId = function () {
+  return this.actorId();
+};
+
+Game_Enemy.prototype.skillCooldownId = function () {
+  return this.index();
 };
 
 const _Window_SKillList_drawSkillCost = Window_SkillList.prototype.drawSkillCost;
