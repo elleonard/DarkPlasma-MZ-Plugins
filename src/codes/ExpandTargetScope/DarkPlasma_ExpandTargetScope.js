c79dc66f0f52d4d4fa34fbef6152b3162617b933
plasma <librarian256@gmail.com>
2022-01-03 13:30:15 +0900

    1.0.9 全体化切り替え時の挙動修正

--- src/codes/ExpandTargetScope/DarkPlasma_ExpandTargetScope.js
+++ src/codes/ExpandTargetScope/DarkPlasma_ExpandTargetScope.js
@@ -1,68 +1,76 @@
 import { settings } from './_build/DarkPlasma_ExpandTargetScope_parameters';
 
-const _Game_Action_clear = Game_Action.prototype.clear;
-Game_Action.prototype.clear = function () {
-  _Game_Action_clear.call(this);
-  this._isExpandedScope = false;
-  this._enableForAllRate = false;
-};
-
-Game_Action.prototype.expandScope = function () {
-  this._isExpandedScope = !this.isForAllByDefault();
-};
-
-Game_Action.prototype.canExpandScope = function () {
-  return this._item.object().meta.canExpandScope;
-};
-
-Game_Action.prototype.isExpandedScope = function () {
-  return this._isExpandedScope;
-};
-
-const _Game_Action_isForOne = Game_Action.prototype.isForOne;
-Game_Action.prototype.isForOne = function () {
-  return _Game_Action_isForOne.call(this) && !this._isExpandedScope;
-};
-
-const _Game_Action_isForAll = Game_Action.prototype.isForAll;
-Game_Action.prototype.isForAll = function () {
-  return _Game_Action_isForAll.call(this) || this._isExpandedScope;
-};
-
 /**
- * 元々から全体対象であるかどうか
- * @return {boolean}
+ * @param {Game_Action.prototype} gameAction
  */
-Game_Action.prototype.isForAllByDefault = function () {
-  return _Game_Action_isForAll.call(this);
-};
+function Game_Action_ExpandTargetScopeMixIn(gameAction) {
+  const _clear = gameAction.clear;
+  gameAction.clear = function () {
+    _clear.call(this);
+    this._isExpandedScope = false;
+    this._enableForAllRate = false;
+  };
 
-const _Game_Action_needsSelection = Game_Action.prototype.needsSelection;
-Game_Action.prototype.needsSelection = function () {
-  return _Game_Action_needsSelection.call(this) || this.isForAll();
-};
+  gameAction.expandScope = function () {
+    this._isExpandedScope = !this.isForAllByDefault();
+  };
 
-const _Game_Action_makeDamageValue = Game_Action.prototype.makeDamageValue;
-Game_Action.prototype.makeDamageValue = function (target, critical) {
-  const value = _Game_Action_makeDamageValue.call(this, target, critical);
-  return Math.floor(value * this.expandedScopeDamageRate());
-};
+  gameAction.canExpandScope = function () {
+    return this._item.object().meta.canExpandScope;
+  };
 
-/**
- * 全体化している場合、全体化ダメージ倍率を返す
- * 全体化していない場合、1を返す
- * @return {number}
- */
-Game_Action.prototype.expandedScopeDamageRate = function () {
+  gameAction.isExpandedScope = function () {
+    return this._isExpandedScope;
+  };
+
+  const _isForOne = gameAction.isForOne;
+  gameAction.isForOne = function () {
+    return _isForOne.call(this) && !this._isExpandedScope;
+  };
+
+  const _isForAll = gameAction.isForAll;
+  gameAction.isForAll = function () {
+    return _isForAll.call(this) || this._isExpandedScope;
+  };
+
   /**
-   * 全体化済み かつ 敵残数2以上なら全体化倍率を有効化
-   * 単純に同条件で倍率をかけると、全体化攻撃で残り1体になった場合、最後の1体に倍率がかからない
+   * 元々から全体対象であるかどうか
+   * @return {boolean}
    */
-  if (this._isExpandedScope && $gameTroop.aliveMembers().length > 1) {
-    this._enableForAllRate = true;
-  }
-  return this._enableForAllRate ? settings.damageRateForAll / 100 : 1;
-};
+  gameAction.isForAllByDefault = function () {
+    return _isForAll.call(this);
+  };
+
+  const _needsSelection = gameAction.needsSelection;
+  gameAction.needsSelection = function () {
+    return _needsSelection.call(this) || this.isForAll();
+  };
+
+  const _makeDamageValue = gameAction.makeDamageValue;
+  gameAction.makeDamageValue = function (target, critical) {
+    const value = _makeDamageValue.call(this, target, critical);
+    return Math.floor(value * this.expandedScopeDamageRate());
+  };
+
+  /**
+   * 全体化している場合、全体化ダメージ倍率を返す
+   * 全体化していない場合、1を返す
+   * @return {number}
+   */
+  gameAction.expandedScopeDamageRate = function () {
+    /**
+     * 全体化済み かつ 敵残数2以上なら全体化倍率を有効化
+     * 単純に同条件で倍率をかけると、全体化攻撃で残り1体になった場合、最後の1体に倍率がかからない
+     * そのため、やや気持ち悪いが一度でも全体化倍率がかかった行動には必ず全体化倍率をかける
+     */
+    if (this._isExpandedScope && $gameTroop.aliveMembers().length > 1) {
+      this._enableForAllRate = true;
+    }
+    return this._enableForAllRate ? settings.damageRateForAll / 100 : 1;
+  };
+}
+
+Game_Action_ExpandTargetScopeMixIn(Game_Action.prototype);
 
 Game_Unit.prototype.selectAll = function () {
   this.aliveMembers().forEach((member) => member.select());
@@ -91,51 +99,58 @@ Game_Battler.prototype.syncSelectionEffect = function () {
   this._syncSelectionEffectRequested = false;
 };
 
-const _Scene_Battle_startActorSelection = Scene_Battle.prototype.startActorSelection;
-Scene_Battle.prototype.startActorSelection = function () {
-  _Scene_Battle_startActorSelection.call(this);
-  const action = BattleManager.inputtingAction();
-  if (action.isForAllByDefault()) {
-    this._actorWindow.setCursorAll(true);
-    this._actorWindow.setCursorFixed(true);
-  } else {
-    this._actorWindow.setCursorAll(false);
-    this._actorWindow.setCursorFixed(false);
-  }
-  this._actorWindow.refreshCursor();
-};
-
-const _Scene_Battle_startEnemySelection = Scene_Battle.prototype.startEnemySelection;
-Scene_Battle.prototype.startEnemySelection = function () {
-  _Scene_Battle_startEnemySelection.call(this);
-  const action = BattleManager.inputtingAction();
-  if (action.isForAllByDefault()) {
-    this._enemyWindow.setCursorAll(true);
-    this._enemyWindow.setCursorFixed(true);
-  } else {
-    this._enemyWindow.setCursorAll(false);
-    this._enemyWindow.setCursorFixed(false);
-  }
-  this._enemyWindow.refreshCursor();
-};
-
-const _Scene_Battle_onActorOk = Scene_Battle.prototype.onActorOk;
-Scene_Battle.prototype.onActorOk = function () {
-  if (this._actorWindow.cursorAll()) {
+/**
+ * @param {Scene_Battle.prototype} sceneBattle
+ */
+function Scene_Battle_ExpandScopeTargetMixIn(sceneBattle) {
+  const _startActorSelection = sceneBattle.startActorSelection;
+  sceneBattle.startActorSelection = function () {
+    _startActorSelection.call(this);
     const action = BattleManager.inputtingAction();
-    action.expandScope();
-  }
-  _Scene_Battle_onActorOk.call(this);
-};
+    if (action.isForAllByDefault()) {
+      this._actorWindow.setCursorAll(true);
+      this._actorWindow.setCursorFixed(true);
+    } else {
+      this._actorWindow.setCursorAll(false);
+      this._actorWindow.setCursorFixed(false);
+    }
+    this._actorWindow.refreshCursor();
+  };
 
-const _Scene_Battle_onEnemyOk = Scene_Battle.prototype.onEnemyOk;
-Scene_Battle.prototype.onEnemyOk = function () {
-  if (this._enemyWindow.cursorAll()) {
+  const _startEnemySelection = sceneBattle.startEnemySelection;
+  sceneBattle.startEnemySelection = function () {
+    _startEnemySelection.call(this);
     const action = BattleManager.inputtingAction();
-    action.expandScope();
-  }
-  _Scene_Battle_onEnemyOk.call(this);
-};
+    if (action.isForAllByDefault()) {
+      this._enemyWindow.setCursorAll(true);
+      this._enemyWindow.setCursorFixed(true);
+    } else {
+      this._enemyWindow.setCursorAll(false);
+      this._enemyWindow.setCursorFixed(false);
+    }
+    this._enemyWindow.refreshCursor();
+  };
+
+  const _onActorOk = sceneBattle.onActorOk;
+  sceneBattle.onActorOk = function () {
+    if (this._actorWindow.cursorAll()) {
+      const action = BattleManager.inputtingAction();
+      action.expandScope();
+    }
+    _onActorOk.call(this);
+  };
+
+  const _onEnemyOk = sceneBattle.onEnemyOk;
+  sceneBattle.onEnemyOk = function () {
+    if (this._enemyWindow.cursorAll()) {
+      const action = BattleManager.inputtingAction();
+      action.expandScope();
+    }
+    _onEnemyOk.call(this);
+  };
+}
+
+Scene_Battle_ExpandScopeTargetMixIn(Scene_Battle.prototype);
 
 const _Sprite_Battler_updateSelectionEffect = Sprite_Battler.prototype.updateSelectionEffect;
 Sprite_Battler.prototype.updateSelectionEffect = function () {
@@ -148,70 +163,110 @@ Sprite_Battler.prototype.updateSelectionEffect = function () {
   _Sprite_Battler_updateSelectionEffect.call(this);
 };
 
-Window_Selectable.prototype.processExpandScope = function () {
-  if (!$gameParty.inBattle() || (!this instanceof Window_BattleActor && !this instanceof Window_BattleEnemy)) {
+/**
+ * @param {Window_BattleActor.prototype|Window_BattleEnemy.prototype} windowClass
+ */
+function Window_BattleTarget_ExpandTargetScopeMixIn(windowClass) {
+  windowClass.processExpandScope = function () {
+    if (!$gameParty.inBattle()) {
+      return false;
+    }
+    const action = BattleManager.inputtingAction();
+    if (action.canExpandScope() && Input.isTriggered(settings.switchScopeButton) && !this.cursorFixed()) {
+      this.setCursorAll(!this._cursorAll);
+      return true;
+    }
     return false;
-  }
-  const action = BattleManager.inputtingAction();
-  if (action.canExpandScope() && Input.isTriggered(settings.switchScopeButton) && !this.cursorFixed()) {
-    this.setCursorAll(!this._cursorAll);
-    return true;
-  }
-  return false;
-};
+  };
 
-const _Window_BattleActor_processHandling = Window_BattleActor.prototype.processHandling;
-Window_BattleActor.prototype.processHandling = function () {
-  if (this.isOpenAndActive()) {
-    if (this.processExpandScope()) {
-      return;
+  const _processHandling = windowClass.processHandling;
+  windowClass.processHandling = function () {
+    if (this.isOpenAndActive()) {
+      if (this.processExpandScope()) {
+        return;
+      }
     }
-  }
-  return _Window_BattleActor_processHandling.call(this);
-};
+    return _processHandling.call(this);
+  };
 
-const _Window_BattleActor_setCursorAll = Window_BattleActor.prototype.setCursorAll;
-Window_BattleActor.prototype.setCursorAll = function (cursorAll) {
-  _Window_BattleActor_setCursorAll.call(this, cursorAll);
-  if (cursorAll) {
-    $gameParty.selectAll();
-  } else {
-    $gameParty.select(this.actor(this.index()));
-  }
-};
+  /**
+   * @return {Game_Unit}
+   */
+  windowClass.unit = function () {
+    return null;
+  };
 
-const _Window_BattleEnemy_processHandling = Window_BattleEnemy.prototype.processHandling;
-Window_BattleEnemy.prototype.processHandling = function () {
-  if (this.isOpenAndActive()) {
-    if (this.processExpandScope()) {
-      return;
+  /**
+   * @return {Game_Battler}
+   */
+  windowClass.currentTarget = function () {
+    return null;
+  };
+
+  const _setCursorAll = windowClass.setCursorAll;
+  windowClass.setCursorAll = function (cursorAll) {
+    _setCursorAll.call(this, cursorAll);
+    if (cursorAll) {
+      this.unit().selectAll();
+    } else {
+      this.unit().select(this.currentTarget());
     }
-  }
-  return _Window_BattleEnemy_processHandling.call(this);
-};
+    SoundManager.playCursor();
+    this.refreshCursor();
+  };
+}
 
-const _Window_BattleEnemy_setCursorAll = Window_BattleEnemy.prototype.setCursorAll;
-Window_BattleEnemy.prototype.setCursorAll = function (cursorAll) {
-  _Window_BattleEnemy_setCursorAll.call(this, cursorAll);
-  if (cursorAll) {
-    $gameTroop.selectAll();
-  } else {
-    $gameTroop.select(this.enemy());
-  }
-};
+Window_BattleTarget_ExpandTargetScopeMixIn(Window_BattleActor.prototype);
+Window_BattleTarget_ExpandTargetScopeMixIn(Window_BattleEnemy.prototype);
 
-const _Window_BattleEnemy_refreshCursorForAll = Window_BattleEnemy.prototype.refreshCursorForAll;
-Window_BattleEnemy.prototype.refreshCursorForAll = function () {
-  const maxItems = this.maxItems();
-  if (maxItems > 0) {
-    const rect = this.itemRect(0);
-    [...Array(maxItems).keys()].forEach((index) => rect.enlarge(this.itemRect(index)));
-    this.setCursorRect(rect.x, rect.y, rect.width, rect.height);
-  } else {
-    _Window_BattleEnemy_refreshCursorForAll.call(this);
-  }
-};
+/**
+ * @param {Window_BattleActor.prototype} windowClass
+ */
+function Window_BattleActor_ExpandTargetScopeMixIn(windowClass) {
+  windowClass.unit = function () {
+    return $gameParty;
+  };
 
+  windowClass.currentTarget = function () {
+    return this.actor(this.index());
+  };
+}
+
+Window_BattleActor_ExpandTargetScopeMixIn(Window_BattleActor.prototype);
+
+/**
+ * @param {Window_BattleEnemy.prototype} windowClass
+ */
+function Window_BattleEnemy_ExpandScopeTargetMixIn(windowClass) {
+  windowClass.unit = function () {
+    return $gameTroop;
+  };
+
+  windowClass.currentTarget = function () {
+    return this.enemy();
+  };
+
+  const _refreshCursorForAll = windowClass.refreshCursorForAll;
+  windowClass.refreshCursorForAll = function () {
+    const maxItems = this.maxItems();
+    if (maxItems > 0) {
+      const rect = this.itemRect(0);
+      [...Array(maxItems).keys()].forEach((index) => rect.enlarge(this.itemRect(index)));
+      this.setCursorRect(rect.x, rect.y, rect.width, rect.height);
+    } else {
+      _refreshCursorForAll.call(this);
+    }
+  };
+}
+
+Window_BattleEnemy_ExpandScopeTargetMixIn(Window_BattleEnemy.prototype);
+
+/**
+ * 全体化アニメーションのため上書き
+ * @param {Game_Battler} subject
+ * @param {Game_Action} action
+ * @param {Game_Battler[]} targets
+ */
 Window_BattleLog.prototype.startAction = function (subject, action, targets) {
   const item = action.item();
   this.push('performActionStart', subject, action);
