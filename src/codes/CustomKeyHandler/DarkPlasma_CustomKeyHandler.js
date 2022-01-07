d28f98df1b302877b4984e3d9c51dde8941ca977
plasma <librarian256@gmail.com>
2022-01-07 11:20:23 +0900

    1.1.0 ハンドラ名設定

--- src/codes/CustomKeyHandler/DarkPlasma_CustomKeyHandler.js
+++ src/codes/CustomKeyHandler/DarkPlasma_CustomKeyHandler.js
@@ -1,38 +1,53 @@
+class CustomKeyMethods {
+  /**
+   * @param {() => boolean} isTriggered
+   * @param {() => void} process
+   */
+  constructor(isTriggered, process) {
+    this._isTriggered = isTriggered;
+    this._process = process;
+  }
+
+  isTriggered() {
+    return this._isTriggered();
+  }
+
+  process(self) {
+    this._process(self);
+  }
+}
+
 /**
  * @param {string} key キー
  * @param {Window_Base.prototype} windowClass
+ * @param {string} handlerName
  */
-function Window_CustomKeyHandlerMixIn(key, windowClass) {
-  if (!windowClass.isCustomKeyTriggered) {
-    windowClass.isCustomKeyTriggered = [];
-    windowClass.processCustomKey = [];
-    windowClass.callCustomKeyHandler = [];
+function Window_CustomKeyHandlerMixIn(key, windowClass, handlerName) {
+  if (!windowClass.customKeyMethods) {
+    windowClass.customKeyMethods = [];
 
     const _processHandling = windowClass.processHandling;
     windowClass.processHandling = function () {
       _processHandling.call(this);
       if (this.isOpenAndActive()) {
-        const index = this.isCustomKeyTriggered.findIndex((isTriggered) => isTriggered.call(this));
-        if (index >= 0) {
-          return this.processCustomKey[index].call(this);
+        const customKeyMethod = this.customKeyMethods.find((method) => method.isTriggered());
+        if (customKeyMethod) {
+          return customKeyMethod.process(this);
         }
       }
     };
   }
 
-  windowClass.isCustomKeyTriggered.push(function () {
-    return Input.isTriggered(key);
-  });
-
-  windowClass.processCustomKey.push(function () {
-    this.playCursorSound();
-    this.updateInputData();
-    this.callCustomKeyHandler[windowClass.isCustomKeyTriggered.length - 1].call(this);
-  });
-
-  windowClass.callCustomKeyHandler.push(function () {
-    this.callHandler(key);
-  });
+  windowClass.customKeyMethods.push(
+    new CustomKeyMethods(
+      () => Input.isTriggered(key),
+      (self) => {
+        self.playCursorSound();
+        self.updateInputData();
+        self.callHandler(handlerName || key);
+      }
+    )
+  );
 }
 
 window.Window_CustomKeyHandlerMixIn = Window_CustomKeyHandlerMixIn;
