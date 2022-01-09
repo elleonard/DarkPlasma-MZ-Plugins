c3b05d4045aa52a08b097c0a3dce7a69ad6e6cca
plasma <librarian256@gmail.com>
2022-01-09 18:06:34 +0900

    1.0.1 同一対象への複数回攻撃で減衰しない不具合

--- src/codes/MultiAttackDamageFall/DarkPlasma_MultiAttackDamageFall.js
+++ src/codes/MultiAttackDamageFall/DarkPlasma_MultiAttackDamageFall.js
@@ -1,5 +1,28 @@
 import { settings } from './_build/DarkPlasma_MultiAttackDamageFall_parameters';
 
+class MultiAttackTarget {
+  constructor(target) {
+    this._target = target;
+    this._attacked = false;
+  }
+
+  /**
+   * 複数攻撃のダメージ計算に用いる対象であるか
+   * @param {Game_Battler} target
+   * @return {boolean}
+   */
+  isMultiAttackTarget(target) {
+    return this._target === target && !this._attacked;
+  }
+
+  /**
+   * 攻撃済みフラグを立てる
+   */
+  attack() {
+    this._attacked = true;
+  }
+}
+
 /**
  * @param {Game_Action.prototype} gameAction
  */
@@ -7,7 +30,7 @@ function Game_Action_MultiAttackDamageFallMixIn(gameAction) {
   const _clear = gameAction.clear;
   gameAction.clear = function () {
     _clear.call(this);
-    this._cachedTargets = [];
+    this._multiAttackTargets = [];
   };
 
   const _makeDamageValue = gameAction.makeDamageValue;
@@ -26,19 +49,20 @@ function Game_Action_MultiAttackDamageFallMixIn(gameAction) {
      * ダメージ計算で使うため、ターゲット情報はキャッシュしておく
      */
     const targets = _makeTargets.call(this);
-    this._cachedTargets = [...targets];
+    this._multiAttackTargets = targets.map((target) => new MultiAttackTarget(target));
     return targets;
   };
 
-  gameAction.cachedTargetIndex = function (target) {
-    return this._cachedTargets.indexOf(target);
+  gameAction.multiAttackTargetIndex = function (target) {
+    return this._multiAttackTargets.findIndex((cache) => cache.isMultiAttackTarget(target));
   };
 
   gameAction.multiAttackDamageFallRate = function (target) {
     if (!this.isMultiAttack()) {
       return 1;
     }
-    const targetIndex = this.cachedTargetIndex(target);
+    const targetIndex = this.multiAttackTargetIndex(target);
+    this._multiAttackTargets[targetIndex].attack();
     if (targetIndex > 0) {
       return Math.max((100 - settings.fallRate * targetIndex) / 100, settings.minimumRate / 100);
     }
