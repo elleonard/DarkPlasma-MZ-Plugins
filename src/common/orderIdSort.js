8003bbe7f1fa2e80bc05091b859149db6ef66c69
plasma <librarian256@gmail.com>
2022-05-14 15:00:21 +0900

    orderIdSortを修正

--- src/common/orderIdSort.js
+++ src/common/orderIdSort.js
@@ -1,3 +1,11 @@
 export function orderIdSort(a, b) {
-  return a.orderId || a.id - b.orderId || b.id;
+  if (a === null && b === null) {
+    // 両方nullなら順不同
+    return 0;
+  } else if (a === null) {
+    return 1;
+  } else if (b === null) {
+    return -1;
+  }
+  return (a.orderId || a.id) - (b.orderId || b.id);
 }
