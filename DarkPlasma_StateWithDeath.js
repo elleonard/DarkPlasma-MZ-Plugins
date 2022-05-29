69b41a0e75c866fca7c8043888a3e0106ad8f244
elleonard <librarian256@gmail.com>
2022-05-29 15:33:45 +0900

    514460dda11ba5c873031646c7164873750bd601

---
+++ DarkPlasma_StateWithDeath.js
@@ -0,0 +1,59 @@
+// DarkPlasma_StateWithDeath 1.0.0
+// Copyright (c) 2022 DarkPlasma
+// This software is released under the MIT license.
+// http://opensource.org/licenses/mit-license.php
+
+/**
+ * 2022/05/29 1.0.0 公開
+ */
+
+/*:ja
+ * @plugindesc 戦闘不能になっても消えないステート
+ * @author DarkPlasma
+ * @license MIT
+ *
+ * @target MZ
+ * @url https://github.com/elleonard/DarkPlasma-MZ-Plugins/tree/release
+ *
+ * @param statesWithDeath
+ * @text 消えないステート
+ * @type state[]
+ * @default []
+ *
+ * @help
+ * version: 1.0.0
+ * 指定したステートは戦闘不能になっても解除されません。
+ */
+
+(() => {
+  'use strict';
+
+  const pluginName = document.currentScript.src.replace(/^.*\/(.*).js$/, function () {
+    return arguments[1];
+  });
+
+  const pluginParameters = PluginManager.parameters(pluginName);
+
+  const settings = {
+    statesWithDeath: JSON.parse(pluginParameters.statesWithDeath || '[]').map((e) => {
+      return Number(e || 0);
+    }),
+  };
+
+  /**
+   * @param {Game_BattlerBase.prototype} gameBattlerBase
+   */
+  function Game_BattlerBase_StateWithDeathMixIn(gameBattlerBase) {
+    const _die = gameBattlerBase.die;
+    gameBattlerBase.die = function () {
+      const continueStates = this._states.filter((stateId) => settings.statesWithDeath.includes(stateId));
+      const continueStateTurns = {};
+      continuesStates.forEach((stateId) => (continueStateTurns[stateId] = this._stateTurns[stateId]));
+      _die.call(this);
+      this._states = continueStates;
+      this._stateTurns = continueStateTurns;
+    };
+  }
+
+  Game_BattlerBase_StateWithDeathMixIn(Game_BattlerBase.prototype);
+})();
