d4ddf150a1492d6428392e6c6e2779c1e54875c2
elleonard <librarian256@gmail.com>
2022-07-18 16:54:01 +0900

    b4f9ed7b9f582dcae431c10b0d653f446248c15c

--- DarkPlasma_EnemyBook.js
+++ DarkPlasma_EnemyBook.js
@@ -1,9 +1,10 @@
-// DarkPlasma_EnemyBook 4.3.0
+// DarkPlasma_EnemyBook 4.3.1
 // Copyright (c) 2020 DarkPlasma
 // This software is released under the MIT license.
 // http://opensource.org/licenses/mit-license.php
 
 /**
+ * 2022/07/18 4.3.1 リファクタ
  * 2022/07/16 4.3.0 ウィンドウ操作用にインターフェースを公開
  * 2022/07/09 4.2.0 レイアウト調整用にウィンドウクラスをグローバルに公開
  * 2022/05/14 4.1.2 リファクタ
@@ -213,7 +214,7 @@
  * @desc 図鑑の内容を初期化します。
  *
  * @help
- * version: 4.3.0
+ * version: 4.3.1
  * このプラグインはYoji Ojima氏によって書かれたRPGツクール公式プラグインを元に
  * DarkPlasmaが改変を加えたものです。
  *
@@ -472,7 +473,7 @@
  * @desc Clear enemy book.
  *
  * @help
- * version: 4.3.0
+ * version: 4.3.1
  * The original plugin is RMMV official plugin written by Yoji Ojima.
  * Arranged by DarkPlasma.
  *
@@ -1933,6 +1934,10 @@
     const _createWindowLayer = sceneBattle.createWindowLayer;
     sceneBattle.createWindowLayer = function () {
       _createWindowLayer.call(this);
+      this.createEnemyBookWindowLayer();
+    };
+
+    sceneBattle.createEnemyBookWindowLayer = function () {
       if (settings.enableInBattle) {
         this._enemyBookLayer = new WindowLayer();
         this._enemyBookLayer.x = (Graphics.width - Graphics.boxWidth) / 2;
