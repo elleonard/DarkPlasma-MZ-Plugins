2a873fdb6d72f606e9f875490879cc8aebbc98b1
plasma <librarian256@gmail.com>
2022-07-23 13:51:27 +0900

    1.1.0 記録装備セット数設定を追加

--- src/codes/SaveEquipSet/DarkPlasma_SaveEquipSet.js
+++ src/codes/SaveEquipSet/DarkPlasma_SaveEquipSet.js
@@ -1,5 +1,16 @@
 import { pluginName } from '../../common/pluginName';
-import { command_loadEquipSet, command_saveEquipSet } from './_build/DarkPlasma_SaveEquipSet_commands';
+import {
+  command_clearEquipSets,
+  command_deleteActorEquipSetAt,
+  command_loadActorEquipSetAt,
+  command_loadEquipSet,
+  command_saveActorEquipSetAt,
+  command_saveEquipSet,
+  parseArgs_deleteActorEquipSetAt,
+  parseArgs_loadActorEquipSetAt,
+  parseArgs_saveActorEquipSetAt,
+} from './_build/DarkPlasma_SaveEquipSet_commands';
+import { settings } from './_build/DarkPlasma_SaveEquipSet_parameters';
 
 PluginManager.registerCommand(pluginName, command_saveEquipSet, function () {
   $gameParty.allMembers().forEach((actor) => actor.saveEquipSet());
@@ -11,6 +22,34 @@ PluginManager.registerCommand(pluginName, command_loadEquipSet, function () {
    */
   $gameParty.allMembers().forEach((actor) => actor.clearEquipments());
   $gameParty.allMembers().forEach((actor) => actor.loadEquipSet());
+});
+
+PluginManager.registerCommand(pluginName, command_saveActorEquipSetAt, function (args) {
+  const parsedArgs = parseArgs_saveActorEquipSetAt(args);
+  const actor = $gameParty.allMembers().find((actor) => actor.actorId() === parsedArgs.actorId);
+  if (actor) {
+    actor.saveEquipSetAt(parsedArgs.index);
+  }
+});
+
+PluginManager.registerCommand(pluginName, command_loadActorEquipSetAt, function (args) {
+  const parsedArgs = parseArgs_loadActorEquipSetAt(args);
+  const actor = $gameParty.allMembers().find((actor) => actor.actorId() === parsedArgs.actorId);
+  if (actor) {
+    actor.loadEquipSetAt(parsedArgs.index);
+  }
+});
+
+PluginManager.registerCommand(pluginName, command_clearEquipSets, function () {
+  $gameParty.allMembers().forEach((actor) => actor.clearEquipSets());
+});
+
+PluginManager.registerCommand(pluginName, command_deleteActorEquipSetAt, function (args) {
+  const parsedArgs = parseArgs_deleteActorEquipSetAt(args);
+  const actor = $gameParty.allMembers().find((actor) => actor.actorId() === parsedArgs.actorId);
+  if (actor) {
+    actor.deleteEquipSetAt(parsedArgs.index);
+  }
 });
 
 class Game_EquipSlot {
@@ -38,13 +77,44 @@ globalThis.Game_EquipSlot = Game_EquipSlot;
  * @param {Game_Actor.prototype} gameActor
  */
 function Game_Actor_SaveEquipSetMixIn(gameActor) {
+  gameActor.equipSets = function () {
+    if (!this._equipSets) {
+      this._equipSets = [];
+    }
+    /**
+     * バージョン1.0.0からの互換
+     */
+    if (this._equipSet) {
+      if (!this._equipSets[0]) {
+        this._equipSets[0] = this._equipSet.map((slot) => slot);
+      }
+      delete this._equipSet;
+    }
+    return this._equipSets;
+  };
+
+  gameActor.equipSetAt = function (index) {
+    return this.equipSets()[index];
+  };
+
   gameActor.saveEquipSet = function () {
-    this._equipSet = this.equips().map((equip, slotId) => new Game_EquipSlot(slotId, equip));
+    this.saveEquipSetAt(0);
+  };
+
+  gameActor.saveEquipSetAt = function (index) {
+    if (settings.equipSetCount > index) {
+      this.equipSets()[index] = this.equips().map((equip, slotId) => new Game_EquipSlot(slotId, equip));
+    }
   };
 
   gameActor.loadEquipSet = function () {
-    if (this._equipSet) {
-      this._equipSet
+    this.loadEquipSetAt(0);
+  };
+
+  gameActor.loadEquipSetAt = function (index) {
+    const equipSet = this.equipSetAt(index);
+    if (settings.equipSetCount > index && equipSet) {
+      equipSet
         .filter(
           (equipSlot) =>
             $gameParty.hasItem(equipSlot.item) &&
@@ -52,6 +122,16 @@ function Game_Actor_SaveEquipSetMixIn(gameActor) {
             this.isEquipChangeOk(equipSlot.slotId)
         )
         .forEach((equipSlot) => this.changeEquip(equipSlot.slotId, equipSlot.item));
+    }
+  };
+
+  gameActor.clearEquipSets = function () {
+    this._equipSets = [];
+  };
+
+  gameActor.deleteEquipSetAt = function (index) {
+    if (this.equipSetAt(index)) {
+      this._equipSets[index] = null;
     }
   };
 }
