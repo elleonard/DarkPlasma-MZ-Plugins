082b3211f8160b7f90527137131bda364ecc6da7
elleonard <librarian256@gmail.com>
2022-07-23 19:35:40 +0900

    e6bca4facf67a07e83a6555dbecc29b35858a707

--- DarkPlasma_SaveEquipSet.js
+++ DarkPlasma_SaveEquipSet.js
@@ -1,10 +1,11 @@
-// DarkPlasma_SaveEquipSet 1.1.0
+// DarkPlasma_SaveEquipSet 1.1.1
 // Copyright (c) 2022 DarkPlasma
 // This software is released under the MIT license.
 // http://opensource.org/licenses/mit-license.php
 
 /**
- * 2022/07/23 1.1.0 記録装備セット数設定を追加
+ * 2022/07/23 1.1.1 セーブデータを正しくロードできない不具合を修正
+ *            1.1.0 記録装備セット数設定を追加
  * 2022/04/22 1.0.0 公開
  */
 
@@ -74,7 +75,7 @@
  * @default 0
  *
  * @help
- * version: 1.1.0
+ * version: 1.1.1
  * パーティメンバーの装備セットを記録し、復元するプラグインコマンドを提供します。
  *
  * 以下に該当する場合、復元時にその装備は無視され、復元されません。
@@ -170,6 +171,12 @@
     }
   });
 
+  const KIND = {
+    ITEM: 1,
+    WEAPON: 2,
+    ARMOR: 3,
+  };
+
   class Game_EquipSlot {
     /**
      * @param {number} slotId
@@ -177,7 +184,7 @@
      */
     constructor(slotId, item) {
       this._slotId = slotId;
-      this._item = item;
+      this.initIdAndKind(item);
     }
 
     get slotId() {
@@ -185,7 +192,57 @@
     }
 
     get item() {
-      return this._item;
+      /**
+       * 旧バージョンのセーブデータ救済
+       */
+      if (this._item || this._item === null) {
+        this.initIdAndKind(this._item);
+      }
+      if (this._itemId === null) {
+        return null;
+      }
+      switch (this._kind) {
+        case KIND.ITEM:
+          return $dataItems[this._itemId];
+        case KIND.WEAPON:
+          return $dataWeapons[this._itemId];
+        case KIND.ARMOR:
+          return $dataArmors[this._itemId];
+        default:
+          throw Error(`不正なアイテム種別です: ${this._kind} ${this._itemId}`);
+      }
+    }
+
+    /**
+     * @param {MZ.Item | MZ.Weapon | MZ.Armor} item
+     */
+    initIdAndKind(item) {
+      this._itemId = item ? item.id : null;
+      this._kind = item
+        ? (() => {
+            if (DataManager.isItem(item)) {
+              /**
+               * アイテムを装備する系システムにふわっと対応
+               */
+              return KIND.ITEM;
+            } else if (DataManager.isWeapon(item)) {
+              return KIND.WEAPON;
+            } else if (DataManager.isArmor(item)) {
+              return KIND.ARMOR;
+            } else {
+              /**
+               * 武器と防具のみ、1.1.0以前のセーブデータに対応
+               */
+              if (item.etypeId === 1) {
+                return KIND.WEAPON;
+              } else if (item.etypeId > 1) {
+                return KIND.ARMOR;
+              }
+            }
+            throw Error(`不正な装備です: ${item.name}`);
+          })()
+        : null;
+      delete this._item;
     }
   }
 
