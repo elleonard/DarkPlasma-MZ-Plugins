f314b088731737b2bac71eb569ffc16299af24d0
elleonard <librarian256@gmail.com>
2022-08-18 04:47:55 +0900

    e86c8708395f8db5b60d31804f89c944d5df5c41

--- DarkPlasma_MaxItemCount.js
+++ DarkPlasma_MaxItemCount.js
@@ -1,9 +1,10 @@
-// DarkPlasma_MaxItemCount 1.0.5
+// DarkPlasma_MaxItemCount 1.0.6
 // Copyright (c) 2020 DarkPlasma
 // This software is released under the MIT license.
 // http://opensource.org/licenses/mit-license.php
 
 /**
+ * 2022/08/18 1.0.6 typescript移行
  * 2021/07/05 1.0.5 MZ 1.3.2に対応
  * 2021/06/22 1.0.4 サブフォルダからの読み込みに対応
  * 2020/10/10 1.0.3 リファクタ
@@ -64,7 +65,7 @@
  * @type armor
  *
  * @help
- * version: 1.0.5
+ * version: 1.0.6
  * アイテムごとに最大所持数を設定できます。
  *
  * アイテムのメモ欄に以下のように記述してください。
@@ -90,6 +91,41 @@
     return arguments[1];
   });
 
+  function parseArgs_changeDefaultMaxItemCount(args) {
+    return {
+      count: Number(args.count || 0),
+    };
+  }
+
+  function parseArgs_changeMaxItemCount(args) {
+    return {
+      count: Number(args.count || 0),
+      id: Number(args.id || 0),
+    };
+  }
+
+  function parseArgs_changeMaxWeaponCount(args) {
+    return {
+      count: Number(args.count || 0),
+      id: Number(args.id || 0),
+    };
+  }
+
+  function parseArgs_changeMaxArmorCount(args) {
+    return {
+      count: Number(args.count || 0),
+      id: Number(args.id || 0),
+    };
+  }
+
+  const command_changeDefaultMaxItemCount = 'changeDefaultMaxItemCount';
+
+  const command_changeMaxItemCount = 'changeMaxItemCount';
+
+  const command_changeMaxWeaponCount = 'changeMaxWeaponCount';
+
+  const command_changeMaxArmorCount = 'changeMaxArmorCount';
+
   const pluginParametersOf = (pluginName) => PluginManager.parameters(pluginName);
 
   const pluginParameters = pluginParametersOf(pluginName);
@@ -98,144 +134,133 @@
     defaultMaxItemCount: Number(pluginParameters.defaultMaxItemCount || 99),
   };
 
-  const PLUGIN_COMMAND_NAME = {
-    CHANGE_DEFAULT_MAX_ITEM_COUNT: 'changeDefaultMaxItemCount',
-    CHANGE_MAX_ITEM_COUNT: 'changeMaxItemCount',
-    CHANGE_MAX_WEAPON_COUNT: 'changeMaxWeaponCount',
-    CHANGE_MAX_ARMOR_COUNT: 'changeMaxArmorCount',
-  };
-
-  PluginManager.registerCommand(pluginName, PLUGIN_COMMAND_NAME.CHANGE_DEFAULT_MAX_ITEM_COUNT, function (args) {
-    $gameParty.changeDefaultMaxItemCount(Number(args.count));
+  PluginManager.registerCommand(pluginName, command_changeDefaultMaxItemCount, function (args) {
+    const parsedArgs = parseArgs_changeDefaultMaxItemCount(args);
+    $gameParty.changeDefaultMaxItemCount(parsedArgs.count);
   });
-
-  PluginManager.registerCommand(pluginName, PLUGIN_COMMAND_NAME.CHANGE_MAX_ITEM_COUNT, function (args) {
-    $gameParty.changeMaxItemCount($dataItems[Number(args.id)], Number(args.count));
+  PluginManager.registerCommand(pluginName, command_changeMaxItemCount, function (args) {
+    const parsedArgs = parseArgs_changeMaxItemCount(args);
+    $gameParty.changeMaxItemCount($dataItems[parsedArgs.id], parsedArgs.count);
   });
-
-  PluginManager.registerCommand(pluginName, PLUGIN_COMMAND_NAME.CHANGE_MAX_WEAPON_COUNT, function (args) {
-    $gameParty.changeMaxItemCount($dataWeapons[Number(args.id)], Number(args.count));
+  PluginManager.registerCommand(pluginName, command_changeMaxWeaponCount, function (args) {
+    const parsedArgs = parseArgs_changeMaxWeaponCount(args);
+    $gameParty.changeMaxItemCount($dataWeapons[parsedArgs.id], parsedArgs.count);
   });
-
-  PluginManager.registerCommand(pluginName, PLUGIN_COMMAND_NAME.CHANGE_MAX_ARMOR_COUNT, function (args) {
-    $gameParty.changeMaxItemCount($dataArmors[Number(args.id)], Number(args.count));
+  PluginManager.registerCommand(pluginName, command_changeMaxArmorCount, function (args) {
+    const parsedArgs = parseArgs_changeMaxArmorCount(args);
+    $gameParty.changeMaxItemCount($dataArmors[parsedArgs.id], parsedArgs.count);
   });
-
-  /**
-   * 全アイテムの所持最大数をゲーム中に変更する
-   * @param {number} count 変更後の最大数
-   */
-  Game_Party.prototype.changeDefaultMaxItemCount = function (count) {
-    this._defaultMaxItemCount = count;
-    this.items()
-      .filter((item) => this.hasMaxItems(item))
-      .forEach((item) => this.setItemCountToMax(item));
-  };
-
-  /**
-   * アイテムの所持最大数をゲーム中に変更する
-   * @param {MZ.Item | MZ.Weapon | MZ.Armor} item アイテムデータ
-   * @param {number} count 変更後の最大数
-   */
-  Game_Party.prototype.changeMaxItemCount = function (item, count) {
-    if (!this._maxItemCount) {
-      this._maxItemCount = {};
-    }
-    const key = this.itemMaxCountKey(item);
-    if (!key) {
-      return;
-    }
-    this._maxItemCount[key] = count;
-    if (this.hasMaxItems(item)) {
-      this.setItemCountToMax(item);
-    }
-  };
-
-  /**
-   * 指定したアイテムの所持数を最大にする
-   * @param {MZ.Item | MZ.Weapon | MZ.Armor} item アイテムデータ
-   */
-  Game_Party.prototype.setItemCountToMax = function (item) {
-    const container = this.itemContainer(item);
-    if (container) {
-      container[item.id] = this.maxItems(item);
-      if (container[item.id] === 0) {
-        delete container[item.id];
+  function Game_Party_MaxItemCountMixIn(gameParty) {
+    /**
+     * 全アイテムの所持最大数をゲーム中に変更する
+     * @param {number} count 変更後の最大数
+     */
+    gameParty.changeDefaultMaxItemCount = function (count) {
+      this._defaultMaxItemCount = count;
+      this.items()
+        .filter((item) => this.hasMaxItems(item))
+        .forEach((item) => this.setItemCountToMax(item));
+    };
+    /**
+     * アイテムの所持最大数をゲーム中に変更する
+     * @param {MZ.Item | MZ.Weapon | MZ.Armor} item アイテムデータ
+     * @param {number} count 変更後の最大数
+     */
+    gameParty.changeMaxItemCount = function (item, count) {
+      if (!this._maxItemCount) {
+        this._maxItemCount = {};
       }
-    }
-  };
-
-  /**
-   * ゲーム中に変更されたアイテムの所持最大数を返す
-   * ゲーム中に変更されていない場合はnullを返す
-   * @param {MZ.Item | MZ.Weapon | MZ.Armor} item アイテムデータ
-   * @return {number|null}
-   */
-  Game_Party.prototype.changedMaxItemCount = function (item) {
-    if (!this._maxItemCount) {
+      const key = this.itemMaxCountKey(item);
+      if (!key) {
+        return;
+      }
+      this._maxItemCount[key] = count;
+      if (this.hasMaxItems(item)) {
+        this.setItemCountToMax(item);
+      }
+    };
+    /**
+     * 指定したアイテムの所持数を最大にする
+     * @param {MZ.Item | MZ.Weapon | MZ.Armor} item アイテムデータ
+     */
+    gameParty.setItemCountToMax = function (item) {
+      const container = this.itemContainer(item);
+      if (container) {
+        container[item.id] = this.maxItems(item);
+        if (container[item.id] === 0) {
+          delete container[item.id];
+        }
+      }
+    };
+    /**
+     * ゲーム中に変更されたアイテムの所持最大数を返す
+     * ゲーム中に変更されていない場合はnullを返す
+     * @param {MZ.Item | MZ.Weapon | MZ.Armor} item アイテムデータ
+     * @return {number|null}
+     */
+    gameParty.changedMaxItemCount = function (item) {
+      if (!this._maxItemCount) {
+        return null;
+      }
+      const key = this.itemMaxCountKey(item);
+      if (!key || this._maxItemCount[key] === undefined) {
+        return null;
+      }
+      return this._maxItemCount[key];
+    };
+    /**
+     * アイテム所持最大数のキーを返す
+     * @param {MZ.Item | MZ.Weapon | MZ.Armor} item アイテムデータ
+     * @return {string|null}
+     */
+    gameParty.itemMaxCountKey = function (item) {
+      if (DataManager.isItem(item)) {
+        return `item_${item.id}`;
+      }
+      if (DataManager.isWeapon(item)) {
+        return `weapon_${item.id}`;
+      }
+      if (DataManager.isArmor(item)) {
+        return `armor_${item.id}`;
+      }
       return null;
-    }
-    const key = this.itemMaxCountKey(item);
-    if (!key || this._maxItemCount[key] === undefined) {
-      return null;
-    }
-    return this._maxItemCount[key];
-  };
-
-  /**
-   * アイテム所持最大数のキーを返す
-   * @param {MZ.Item | MZ.Weapon | MZ.Armor} item アイテムデータ
-   * @return {string|null}
-   */
-  Game_Party.prototype.itemMaxCountKey = function (item) {
-    if (DataManager.isItem(item)) {
-      return `item_${item.id}`;
-    }
-    if (DataManager.isWeapon(item)) {
-      return `weapon_${item.id}`;
-    }
-    if (DataManager.isArmor(item)) {
-      return `armor_${item.id}`;
-    }
-    return null;
-  };
-
-  /**
-   * アイテムの所持最大数を返す
-   * @param {MZ.Item | MZ.Weapon | MZ.Armor} item アイテムデータ
-   * @return {number}
-   */
-  Game_Party.prototype.maxItems = function (item) {
-    const changedMaxCount = this.changedMaxItemCount(item);
-    if (changedMaxCount !== null) {
-      return changedMaxCount;
-    }
-    if (item.meta.maxCount !== undefined) {
-      return Number(item.meta.maxCount);
-    }
-    if (this._defaultMaxItemCount !== undefined) {
-      return this._defaultMaxItemCount;
-    }
-    return settings.defaultMaxItemCount;
-  };
-
-  /**
-   * アイテム最大所持数のうち、最も大きいものを返す
-   * @return {number}
-   */
-  Game_Party.prototype.maxOfMaxItemCount = function () {
-    return $dataItems
-      .filter((item) => !!item && item.meta.maxCount !== undefined)
-      .map((item) => item.meta.maxCount)
-      .concat(Object.values(this._maxItemCount || {}))
-      .concat([settings.defaultMaxItemCount, this._defaultMaxItemCount || 0])
-      .reduce((a, b) => Math.max(a, b), 0);
-  };
-
+    };
+    /**
+     * アイテムの所持最大数を返す
+     * @param {MZ.Item | MZ.Weapon | MZ.Armor} item アイテムデータ
+     * @return {number}
+     */
+    gameParty.maxItems = function (item) {
+      const changedMaxCount = this.changedMaxItemCount(item);
+      if (changedMaxCount !== null) {
+        return changedMaxCount;
+      }
+      if (item.meta.maxCount !== undefined) {
+        return Number(item.meta.maxCount);
+      }
+      if (this._defaultMaxItemCount !== undefined) {
+        return this._defaultMaxItemCount;
+      }
+      return settings.defaultMaxItemCount;
+    };
+    /**
+     * アイテム最大所持数のうち、最も大きいものを返す
+     * @return {number}
+     */
+    gameParty.maxOfMaxItemCount = function () {
+      return $dataItems
+        .filter((item) => !!item && item.meta.maxCount !== undefined)
+        .map((item) => Number(item.meta.maxCount))
+        .concat(Object.values(this._maxItemCount || {}))
+        .concat([settings.defaultMaxItemCount, this._defaultMaxItemCount || 0])
+        .reduce((a, b) => Math.max(a, b), 0);
+    };
+  }
+  Game_Party_MaxItemCountMixIn(Game_Party.prototype);
   Window_ItemList.prototype.drawItemNumber = function (item, x, y, width) {
     if (this.needsNumber()) {
       this.drawText(':', x, y, width - this.textWidth($gameParty.maxOfMaxItemCount().toString()), 'right');
-      this.drawText($gameParty.numItems(item), x, y, width, 'right');
+      this.drawText(`${$gameParty.numItems(item)}`, x, y, width, 'right');
     }
   };
 })();
