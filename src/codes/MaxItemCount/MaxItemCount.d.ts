10cfce0f738af82c0f1a846c61898f30d77aaab2
plasma <librarian256@gmail.com>
2022-08-18 01:23:15 +0900

    1.0.6 typescript移行

---
+++ src/codes/MaxItemCount/MaxItemCount.d.ts
@@ -0,0 +1,16 @@
+/// <reference path="../../typings/rmmz/rmmz.d.ts" />
+
+declare interface Game_Party {
+  _defaultMaxItemCount: number;
+  _maxItemCount: {
+    [key: string]: number
+  };
+
+  changeDefaultMaxItemCount(count: number): void;
+  changeMaxItemCount(item: MZ.Item|MZ.Weapon|MZ.Armor, count: number): void;
+  changedMaxItemCount(item: MZ.Item|MZ.Weapon|MZ.Armor): number|null;
+  itemMaxCountKey(item: MZ.Item|MZ.Weapon|MZ.Armor): string|null;
+  maxOfMaxItemCount(): number;
+  setItemCountToMax(item: MZ.Item|MZ.Weapon|MZ.Armor): void;
+
+}
