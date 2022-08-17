8d2caa772367e9d4ad6b580e8def925050ab34b1
plasma <librarian256@gmail.com>
2022-08-18 04:25:54 +0900

    差分検出して必要な分だけ型定義を生成する

--- scripts/buildConfigDeclaration.mjs
+++ scripts/buildConfigDeclaration.mjs
@@ -1,17 +1,29 @@
-/*const globPaths = [
-  path.resolve(__dirname, '..', '..', 'src', 'codes', '**', '_build', '*'),
-  path.resolve(__dirname, '..', '..', 'src', 'excludes', '**', '_build', '*'),
-];*/
-const globPaths = [
-  await glob('./src/**/_build/*_commands.js'),
-  await glob('./src/**/_build/*_parameters.js'),
-  await glob('./src/**/_build/*_parametersOf.js'),
-].flat();
+/**
+ * releaseブランチの最新コミットのコメントから、最後にビルドされたmasterのコミットIDを取得する
+ */
+const lastBuildCommit = await $`git log --first-parent origin/release --pretty=oneline -n 1 | awk \"{print $2}\"`;
 
+/**
+ * 差分検出
+ */
+const diffFiles = await $`git --no-pager diff ${lastBuildCommit.stdout.trim()} HEAD --name-only`;
+
+/**
+ * ひとまず、全ビルドはcodesのみ対象とする
+ */
+const buildTargets = [...new Set(diffFiles.stdout.split('\n')
+  .filter(path => path.startsWith("src/codes"))
+  .map(path => /^src\/codes\/(.+)\/.*/.exec(path)[1]))];
+
+const globPaths = await Promise.all(buildTargets.map(target => {
+  return [
+    glob(`./src/codes/${target}/_build/*_commands.js`),
+    glob(`./src/codes/${target}/_build/*_parameters.js`),
+    glob(`./src/codes/${target}/_build/*_parametersOf.js`),
+  ];
+}).flat());
 
 await Promise
   .all(globPaths.map(globPath => {
     return $`yarn tsc --declaration --allowJs --emitDeclarationOnly ${globPath}`;
   }));
-
-//await $`yarn tsc --declaration --allowJs --emitDeclarationOnly ${await glob('./src/**/_build/*.js')}`
