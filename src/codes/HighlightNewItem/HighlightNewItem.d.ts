eeb307ed1c2982f10833113694434d5c1d61bf5d
plasma <librarian256@gmail.com>
2022-08-21 17:53:16 +0900

    1.1.0 新規入手アイテムを最上部に表示する設定を追加

--- src/codes/HighlightNewItem/HighlightNewItem.d.ts
+++ src/codes/HighlightNewItem/HighlightNewItem.d.ts
@@ -15,6 +15,12 @@ declare interface Game_Party {
 }
 
 declare interface Window_ItemList {
+  _isSortedByNewItem: boolean;
+  _newItemsForSort: MZ.Item[];
+  _newWeaponsForSort: MZ.Weapon[];
+  _newArmorsForSort: MZ.Armor[];
+
   drawNewItemName(item: MZ.Item|MZ.Weapon|MZ.Armor, x: number, y: number, width: number): void;
   isNewItem(item: DataManager.Item|null): item is MZ.Item|MZ.Weapon|MZ.Armor;
+  isNewItemForSort(item: MZ.Item|MZ.Weapon|MZ.Armor): boolean;
 }
