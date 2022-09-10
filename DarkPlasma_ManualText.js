400f33a94604354a74eadbe79162d75dc9a06c89
elleonard <librarian256@gmail.com>
2022-09-10 19:31:32 +0900

    0447743bf6971b706f785cd4878df12c483d4dbf

--- DarkPlasma_ManualText.js
+++ DarkPlasma_ManualText.js
@@ -1,10 +1,11 @@
-// DarkPlasma_ManualText 1.5.1
+// DarkPlasma_ManualText 1.5.2
 // Copyright (c) 2022 DarkPlasma
 // This software is released under the MIT license.
 // http://opensource.org/licenses/mit-license.php
 
 /**
- * 2022/09/10 1.5.1 複数列表示時、表示数が奇数の場合に正しく表示されない不具合を修正
+ * 2022/09/10 1.5.2 複数列表示時、正しく表示されない不具合を修正
+ *            1.5.1 複数列表示時、表示数が奇数の場合に正しく表示されない不具合を修正
  *            1.5.0 マニュアルの複数列表示に対応
  * 2022/08/27 1.4.1 typescript移行
  * 2022/07/02 1.4.0 マニュアルの行間設定を追加
@@ -30,7 +31,7 @@
  * @default 12
  *
  * @help
- * version: 1.5.1
+ * version: 1.5.2
  * ウィンドウ右下に操作説明を表示できるようにします。
  *
  * 本プラグインは単体では機能しません。
@@ -130,12 +131,7 @@
       return this.innerWidth - (colsWidth + this.manualPadding()) * (Math.floor(index / this.manualCols()) + 1);
     };
     windowClass.manualY = function (index) {
-      return (
-        this.innerHeight -
-        this.manualLineHeight() *
-          (Math.floor((this.manualTexts().length + 1) / this.manualCols()) - (index % this.manualCols())) +
-        this.manualOffsetY()
-      );
+      return this.innerHeight - this.manualLineHeight() * ((index % this.manualRows()) + 1) + this.manualOffsetY();
     };
     windowClass.setManualOffsetY = function (offset) {
       this._manualOffsetY = offset;
@@ -157,6 +153,9 @@
     };
     windowClass.setManualCols = function (cols) {
       this._manualCols = cols;
+    };
+    windowClass.manualRows = function () {
+      return this._manualRows || 2;
     };
     windowClass.manualWidth = function () {
       return this._manualWidth || this.innerWidth / this.manualCols();
