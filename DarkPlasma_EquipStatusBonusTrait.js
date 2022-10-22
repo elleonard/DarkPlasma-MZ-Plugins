7d8552e196f668939cb52e0540bc075eda9aefd1
elleonard <librarian256@gmail.com>
2022-10-22 17:11:03 +0900

    75f284ca304cb7cef785d8e580d78f4b9362ced7

---
+++ DarkPlasma_EquipStatusBonusTrait.js
@@ -0,0 +1,237 @@
+// DarkPlasma_EquipStatusBonusTrait 1.0.0
+// Copyright (c) 2022 DarkPlasma
+// This software is released under the MIT license.
+// http://opensource.org/licenses/mit-license.php
+
+/**
+ * 2022/10/22 1.0.0 公開
+ */
+
+/*:ja
+ * @plugindesc 装備種別によるステータスボーナス特徴
+ * @author DarkPlasma
+ * @license MIT
+ *
+ * @target MZ
+ * @url https://github.com/elleonard/DarkPlasma-MZ-Plugins/tree/release
+ *
+ * @help
+ * version: 1.0.0
+ * アクター/職業/武器/防具/ステートのメモ欄に、
+ * 指定の形式でメモタグを記述することで
+ * 特定の武器・防具タイプを装備していたときに
+ * 有効になるステータスボーナス特徴を追加します。
+ *
+ * 特定武器タイプを装備していた場合に有効なステータスボーナス
+ * <weaponTypeBonus:[weaponType]:[statusType]:[value]>
+ * 特定防具タイプを装備していた場合に有効なステータスボーナス
+ * <armorTypeBonus:[armorType]:[statusType]:[value]>
+ *
+ * [statusType]:
+ *   mhp: 最大HP加算
+ *   mmp: 最大MP加算
+ *   atk: 攻撃力加算
+ *   def: 防御力加算
+ *   mat: 魔法攻撃力加算
+ *   mdf: 魔法防御力加算
+ *   agi: 敏捷性加算
+ *   luk: 運加算
+ *   hit: 命中率加算
+ *   eva: 回避率加算
+ *   cri: 会心率加算
+ *   cev: 会心回避率加算
+ *   mev: 魔法回避率加算
+ *   mrf: 魔法反射率加算
+ *   cnt: 反撃率加算
+ *   hrg: HP再生率加算
+ *   mrg: MP再生率加算
+ *   trg: TP再生率加算
+ *
+ * 記述例: 武器タイプ斧装備時に命中率+5％
+ * <weaponTypeBonus:斧:hit:5>
+ *
+ * 記述例: 防具タイプ盾装備時に防御力+10, 回避率+5％
+ * <armorTypeBonus:
+ *   盾:def:10
+ *   盾:eva:5
+ * >
+ */
+
+(() => {
+  'use strict';
+
+  const pluginName = document.currentScript.src.replace(/^.*\/(.*).js$/, function () {
+    return arguments[1];
+  });
+
+  function hasTraits(data) {
+    return 'traits' in data;
+  }
+
+  const localTraitIdStart = 1;
+  const paramPlusWithWeaponTraitId = uniqueTraitIdCache.allocate(pluginName, localTraitIdStart, '武器タイプ能力値');
+  const xparamPlusWithWeaponTraitId = uniqueTraitIdCache.allocate(
+    pluginName,
+    localTraitIdStart + 1,
+    '武器タイプ追加能力値'
+  );
+  const paramPlusWithArmorTraitId = uniqueTraitIdCache.allocate(pluginName, localTraitIdStart + 2, '防具タイプ能力値');
+  const xparamPlusWithArmorTraitId = uniqueTraitIdCache.allocate(
+    pluginName,
+    localTraitIdStart + 3,
+    '防具タイプ追加能力値'
+  );
+  const PARAM_KEYS = ['mhp', 'mmp', 'atk', 'def', 'mat', 'mdf', 'agi', 'luk'];
+  const XPARAM_KEYS = ['hit', 'eva', 'cri', 'cev', 'mev', 'mrf', 'cnt', 'hrg', 'mrg', 'trg'];
+  function dataIdFromTypeIdAndParamId(typeId, paramId) {
+    return typeId * 10 + paramId;
+  }
+  class ParamPlusWithEquipTrait {
+    constructor(typeName, statusType, value) {
+      const typeId = this.types().findIndex((type) => type === typeName.trim());
+      if (typeId < 0) {
+        throw Error(`存在しない武器・防具タイプが設定されています :${typeName}`);
+      }
+      if (!PARAM_KEYS.includes(statusType) && !XPARAM_KEYS.includes(statusType)) {
+        throw Error(`不正なステータスが設定されています :${statusType}`);
+      }
+      this._typeId = typeId;
+      this._statusType = statusType;
+      this._value = value;
+    }
+    static fromTag(tag) {
+      const traitParams = tag.split(':');
+      const clazz = this;
+      return new clazz(traitParams[0], traitParams[1], Number(traitParams[2]));
+    }
+    types() {
+      return [];
+    }
+    traitId() {
+      return 0;
+    }
+    /**
+     * タイプIDと能力値IDの両方の情報を強引に含める
+     */
+    dataId() {
+      return dataIdFromTypeIdAndParamId(this._typeId, this.paramId());
+    }
+    paramId() {
+      return this.isXParam() ? XPARAM_KEYS.indexOf(this._statusType) : PARAM_KEYS.indexOf(this._statusType);
+    }
+    trait() {
+      return {
+        code: this.traitId(),
+        dataId: this.dataId(),
+        value: this.isXParam() ? this._value / 100 : this._value,
+      };
+    }
+    isXParam() {
+      return XPARAM_KEYS.includes(this._statusType);
+    }
+  }
+  class ParamPlusWithWeaponTrait extends ParamPlusWithEquipTrait {
+    types() {
+      return ($dataSystem === null || $dataSystem === void 0 ? void 0 : $dataSystem.weaponTypes) || [];
+    }
+    traitId() {
+      return this.isXParam() ? xparamPlusWithWeaponTraitId.id : paramPlusWithWeaponTraitId.id;
+    }
+  }
+  class ParamPlusWithArmorTrait extends ParamPlusWithEquipTrait {
+    types() {
+      return ($dataSystem === null || $dataSystem === void 0 ? void 0 : $dataSystem.armorTypes) || [];
+    }
+    traitId() {
+      return this.isXParam() ? xparamPlusWithArmorTraitId.id : paramPlusWithArmorTraitId.id;
+    }
+  }
+  function DataManager_EquipStatusBonusTraitMixIn(dataManager) {
+    dataManager.loadExtraTraits = function () {
+      this._databaseFiles
+        .map((database) => globalThis[database.name])
+        .forEach((database) => {
+          if (Array.isArray(database)) {
+            database.forEach((data) => {
+              if (data && 'note' in data) {
+                this.extractExtraTraits(data);
+              }
+            });
+          }
+        });
+    };
+    dataManager.extractExtraTraits = function (data) {
+      if (hasTraits(data)) {
+        if (data.meta.weaponTypeBonus) {
+          String(data.meta.weaponTypeBonus)
+            .split('\n')
+            .filter((bonus) => bonus)
+            .map((bonus) => ParamPlusWithWeaponTrait.fromTag(bonus).trait())
+            .forEach((trait) => data.traits.push(trait));
+        }
+        if (data.meta.armorTypeBonus) {
+          String(data.meta.armorTypeBonus)
+            .split('\n')
+            .filter((bonus) => bonus)
+            .map((bonus) => ParamPlusWithArmorTrait.fromTag(bonus).trait())
+            .forEach((trait) => data.traits.push(trait));
+        }
+      }
+    };
+  }
+  DataManager_EquipStatusBonusTraitMixIn(DataManager);
+  function Scene_Boot_EquipStatusBonusTraitMixIn(sceneBoot) {
+    const _onDatabaseLoaded = sceneBoot.onDatabaseLoaded;
+    sceneBoot.onDatabaseLoaded = function () {
+      _onDatabaseLoaded.call(this);
+      this.loadExtraTraits();
+    };
+    /**
+     * 装備種別によるボーナス特徴は、装備種別一覧を含む $dataSystem がロード済みであることがロードの前提
+     * データベースのロードが完了した後に、metaを加工してtraitsを追加する
+     */
+    sceneBoot.loadExtraTraits = function () {
+      DataManager.loadExtraTraits();
+    };
+  }
+  Scene_Boot_EquipStatusBonusTraitMixIn(Scene_Boot.prototype);
+  function Game_Actor_EquipStatusBonusTraitMixIn(gameActor) {
+    const _paramPlus = gameActor.paramPlus;
+    gameActor.paramPlus = function (paramId) {
+      return _paramPlus.call(this, paramId) + this.paramPlusWithEquipTraits(paramId);
+    };
+    gameActor.paramPlusWithEquipTraits = function (paramId) {
+      return this.validParamPlusWithEquipTraits(paramId).reduce((result, trait) => result + trait.value, 0);
+    };
+    gameActor.validParamPlusWithEquipTraits = function (paramId) {
+      return this.equips()
+        .filter((equip) => equip)
+        .reduce((result, equip) => {
+          const dataId = DataManager.isWeapon(equip)
+            ? dataIdFromTypeIdAndParamId(equip.wtypeId, paramId)
+            : dataIdFromTypeIdAndParamId(equip.atypeId, paramId);
+          const traitId = DataManager.isWeapon(equip) ? paramPlusWithWeaponTraitId.id : paramPlusWithArmorTraitId.id;
+          return result.concat(this.traitsWithId(traitId, dataId));
+        }, []);
+    };
+    const _xparam = gameActor.xparam;
+    gameActor.xparam = function (xparamId) {
+      return _xparam.call(this, xparamId) + this.xparamPlusWithEquipTraits(xparamId);
+    };
+    gameActor.xparamPlusWithEquipTraits = function (xparamId) {
+      return this.validXParamPlusWithEquipTraits(xparamId).reduce((result, trait) => result + trait.value, 0);
+    };
+    gameActor.validXParamPlusWithEquipTraits = function (xparamId) {
+      return this.equips()
+        .filter((equip) => equip)
+        .reduce((result, equip) => {
+          const dataId = DataManager.isWeapon(equip)
+            ? dataIdFromTypeIdAndParamId(equip.wtypeId, xparamId)
+            : dataIdFromTypeIdAndParamId(equip.atypeId, xparamId);
+          const traitId = DataManager.isWeapon(equip) ? xparamPlusWithWeaponTraitId.id : xparamPlusWithArmorTraitId.id;
+          return result.concat(this.traitsWithId(traitId, dataId));
+        }, []);
+    };
+  }
+  Game_Actor_EquipStatusBonusTraitMixIn(Game_Actor.prototype);
+})();
