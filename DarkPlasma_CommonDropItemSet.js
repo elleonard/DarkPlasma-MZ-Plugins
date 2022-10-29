3751efb1ed83991a3a8b70d5cecbd00f5eaaae1a
elleonard <librarian256@gmail.com>
2022-10-29 12:26:15 +0900

    e003b49f72ed2d2cd180b3c246110fd28eed458f

--- DarkPlasma_CommonDropItemSet.js
+++ DarkPlasma_CommonDropItemSet.js
@@ -1,10 +1,12 @@
-// DarkPlasma_CommonDropItemSet 1.0.0
+// DarkPlasma_CommonDropItemSet 1.2.0
 // Copyright (c) 2022 DarkPlasma
 // This software is released under the MIT license.
 // http://opensource.org/licenses/mit-license.php
 
 /**
- * 2022/10/29 1.0.0 公開
+ * 2022/10/29 1.2.0 敵ごとにドロップする設定を追加
+ *            1.1.0 共通ドロップアイテム有効判定の拡張用インターフェース追加
+ *            1.0.0 公開
  */
 
 /*:ja
@@ -20,8 +22,14 @@
  * @type struct<DropItemSet>[]
  * @default []
  *
+ * @param dropOneByOneEnemy
+ * @desc ONにするとドロップ判定を戦闘ごとではなく、敵ごとに行います。
+ * @text 敵ごとにドロップするか
+ * @type boolean
+ * @default false
+ *
  * @help
- * version: 1.0.0
+ * version: 1.2.0
  * 全ての戦闘において共通でドロップするアイテム・武器・防具のセットを設定します。
  * ドロップセットはそれぞれ独立してドロップ確率判定を行います。
  * 例えば、ドロップセット1に確率10％、セット2に確率20％を設定した場合、
@@ -79,8 +87,20 @@
         };
       })(e || '{}');
     }),
+    dropOneByOneEnemy: String(pluginParameters.dropOneByOneEnemy || false) === 'true',
   };
 
+  function makeDropItems() {
+    return settings.dropItemSetList
+      .filter((dropItemSet) => dropItemSet.dropRate > Math.randomInt(100))
+      .map((dropItemSet) => {
+        const dropItems = dropItemSet.items
+          .map((id) => $dataItems[id])
+          .concat(dropItemSet.weapons.map((id) => $dataWeapons[id]))
+          .concat(dropItemSet.armors.map((id) => $dataArmors[id]));
+        return dropItems[Math.randomInt(dropItems.length)];
+      });
+  }
   function Game_Troop_CommonDropItemSetMixIn(gameTroop) {
     const _makeDropItems = gameTroop.makeDropItems;
     gameTroop.makeDropItems = function () {
@@ -90,16 +110,21 @@
       return true;
     };
     gameTroop.makeCommonDropItems = function () {
-      return settings.dropItemSetList
-        .filter((dropItemSet) => dropItemSet.dropRate > Math.randomInt(100))
-        .map((dropItemSet) => {
-          const dropItems = dropItemSet.items
-            .map((id) => $dataItems[id])
-            .concat(dropItemSet.weapons.map((id) => $dataWeapons[id]))
-            .concat(dropItemSet.armors.map((id) => $dataArmors[id]));
-          return dropItems[Math.randomInt(dropItems.length)];
-        });
+      return this.isCommonItemDropSetEnabled() ? makeDropItems() : [];
     };
   }
   Game_Troop_CommonDropItemSetMixIn(Game_Troop.prototype);
+  function Game_Enemy_CommonDropItemSetMixIn(gameEnemy) {
+    const _makeDropItems = gameEnemy.makeDropItems;
+    gameEnemy.makeDropItems = function () {
+      return _makeDropItems.call(this).concat(this.makeCommonDropItems());
+    };
+    gameEnemy.isCommonItemDropSetEnabled = function () {
+      return true;
+    };
+    gameEnemy.makeCommonDropItems = function () {
+      return this.isCommonItemDropSetEnabled() ? makeDropItems() : [];
+    };
+  }
+  Game_Enemy_CommonDropItemSetMixIn(Game_Enemy.prototype);
 })();
