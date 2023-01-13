f698d80e5a46fe3d00ebe64d726e650a58484118
elleonard <librarian256@gmail.com>
2023-01-13 13:07:58 +0900

    32798396f3475d989e886e1979e284825ec2c976

---
+++ DarkPlasma_Scene_MessageMixIn.js
@@ -0,0 +1,146 @@
+// DarkPlasma_Scene_MessageMixIn 1.0.0
+// Copyright (c) 2023 DarkPlasma
+// This software is released under the MIT license.
+// http://opensource.org/licenses/mit-license.php
+
+/**
+ * 2023/01/13 1.0.0 公開
+ */
+
+/*:ja
+ * @plugindesc 指定のシーンにメッセージウィンドウを表示させる
+ * @author DarkPlasma
+ * @license MIT
+ *
+ * @target MZ
+ * @url https://github.com/elleonard/DarkPlasma-MZ-Plugins/tree/release
+ *
+ * @param scenes
+ * @text シーン
+ * @type string[]
+ * @default []
+ *
+ * @help
+ * version: 1.0.0
+ * パラメータで指定したシーンにメッセージウィンドウを表示できるようになります。
+ *
+ * $gameMessage.add などでメッセージを追加した際に、
+ * そのシーンでもメッセージを表示します。
+ */
+
+(() => {
+  'use strict';
+
+  const pluginName = document.currentScript.src.replace(/^.*\/(.*).js$/, function () {
+    return arguments[1];
+  });
+
+  const pluginParametersOf = (pluginName) => PluginManager.parameters(pluginName);
+
+  const pluginParameters = pluginParametersOf(pluginName);
+
+  const settings = {
+    scenes: JSON.parse(pluginParameters.scenes || '[]').map((e) => {
+      return String(e || '');
+    }),
+  };
+
+  function Scene_MessageMixIn(sceneClass) {
+    const _create = sceneClass.create;
+    sceneClass.create = function () {
+      _create.call(this);
+      this.createMessageWindows();
+    };
+    sceneClass.isMessageWindowClosing = function () {
+      return Scene_Message.prototype.isMessageWindowClosing.call(this);
+    };
+    sceneClass.createMessageWindows = function () {
+      this._messageWindowLayer = new WindowLayer();
+      this._messageWindowLayer.x = (Graphics.width - Graphics.boxWidth) / 2;
+      this._messageWindowLayer.y = (Graphics.height - Graphics.boxHeight) / 2;
+      this.addChild(this._messageWindowLayer);
+      this.createMessageWindow();
+      this.createGoldWindow();
+      this.createNameBoxWindow();
+      this.createChoiceListWindow();
+      this.createNumberInputWindow();
+      this.createEventItemWindow();
+      this.associateWindows();
+    };
+    sceneClass.createMessageWindow = function () {
+      this._messageWindow = new Window_Message(this.messageWindowRect());
+      this._messageWindowLayer.addChild(this._messageWindow);
+    };
+    sceneClass.messageWindowRect = function () {
+      return Scene_Message.prototype.messageWindowRect.call(this);
+    };
+    sceneClass.createGoldWindow = function () {
+      this._goldWindow = new Window_Gold(this.goldWindowRect());
+      this._goldWindow.openness = 0;
+      this._messageWindowLayer.addChild(this._goldWindow);
+    };
+    sceneClass.goldWindowRect = function () {
+      return Scene_Message.prototype.goldWindowRect.call(this);
+    };
+    sceneClass.createNameBoxWindow = function () {
+      this._nameBoxWindow = new Window_NameBox();
+      this._messageWindowLayer.addChild(this._nameBoxWindow);
+    };
+    sceneClass.createChoiceListWindow = function () {
+      this._choiceListWindow = new Window_ChoiceList();
+      this._messageWindowLayer.addChild(this._choiceListWindow);
+    };
+    sceneClass.createNumberInputWindow = function () {
+      this._numberInputWindow = new Window_NumberInput();
+      this._messageWindowLayer.addChild(this._numberInputWindow);
+    };
+    sceneClass.createEventItemWindow = function () {
+      this._eventItemWindow = new Window_EventItem(this.eventItemWindowRect());
+      this._messageWindowLayer.addChild(this._eventItemWindow);
+    };
+    sceneClass.associateWindows = function () {
+      Scene_Message.prototype.associateWindows.call(this);
+    };
+    const _isBusy = sceneClass.isBusy;
+    sceneClass.isBusy = function () {
+      return _isBusy.call(this) || $gameMessage.isBusy();
+    };
+  }
+  function Window_Selectable_MessageMixIn(windowClass) {
+    const _isOpenAndActive = windowClass.isOpenAndActive;
+    windowClass.isOpenAndActive = function () {
+      /**
+       * メッセージ表示中は、関連ウィンドウ以外非アクティブ判定とする
+       */
+      if ($gameMessage.isBusy() && !this.isAssociatedWithMessageWindow()) {
+        return false;
+      }
+      return _isOpenAndActive.call(this);
+    };
+    windowClass.isAssociatedWithMessageWindow = function () {
+      return !!this._messageWindow;
+    };
+    const _update = windowClass.update;
+    windowClass.update = function () {
+      /**
+       * メッセージウィンドウに関係しないウィンドウのアクティブ状態を制御する
+       * メッセージ表示中にアクティブであったら非アクティブ化し、
+       * 表示が終了したらアクティブに戻す
+       */
+      if (!this.isAssociatedWithMessageWindow()) {
+        if ($gameMessage.isBusy() && this.active) {
+          this.deactivate();
+          this._deactivatedByMessage = true;
+        } else if (this._deactivatedByMessage) {
+          this.activate();
+          this._deactivatedByMessage = false;
+        }
+      }
+      _update.call(this);
+    };
+  }
+  Window_Selectable_MessageMixIn(Window_Selectable.prototype);
+  settings.scenes
+    .filter((scene) => scene in globalThis && scene !== 'Scene_Map' && scene !== 'Scene_Battle')
+    .forEach((scene) => Scene_MessageMixIn(window[scene].prototype));
+})();
