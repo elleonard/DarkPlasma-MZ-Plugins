d235adbc65c3a296d3ab76e490283a5620ce77e3
elleonard <librarian256@gmail.com>
2023-01-21 15:19:45 +0900

    011e480cc13cdd1865633cdfff86dbc8d5df20ee

--- DarkPlasma_AutoLineBreak.js
+++ DarkPlasma_AutoLineBreak.js
@@ -1,9 +1,12 @@
-// DarkPlasma_AutoLineBreak 1.2.1
+// DarkPlasma_AutoLineBreak 1.3.0
 // Copyright (c) 2020 DarkPlasma
 // This software is released under the MIT license.
 // http://opensource.org/licenses/mit-license.php
 
 /**
+ * 2023/01/21 1.3.0 typescript移行
+ *                  単語ベースの自動改行をサポート
+ *                  英語ヘルプを追加
  * 2022/04/19 1.2.1 文字列の長さ次第で二重改行される不具合を修正
  * 2022/01/07 1.2.0 行幅マージンを取得するメソッドを追加
  * 2021/12/30 1.1.0 行頭禁則文字が行末に2文字連続で来る場合に対応
@@ -15,7 +18,7 @@
  */
 
 /*:ja
- * @plugindesc ウィンドウ幅を超える日本語文章を自動で折り返す
+ * @plugindesc ウィンドウ幅を超える文章を自動で折り返す
  * @author DarkPlasma
  * @license MIT
  *
@@ -46,8 +49,14 @@
  * @type number
  * @default 4
  *
+ * @param wordBaseLineBreak
+ * @desc 単語ベースで自動改行を行うかどうか。半角スペースで区切られたひとかたまりを1単語とみなします。
+ * @text 単語ベース改行
+ * @type boolean
+ * @default false
+ *
  * @help
- * version: 1.2.1
+ * version: 1.3.0
  * ウィンドウ幅を超えるような文字列を自動で改行します。
  *
  * 以下の法則でゆるふわ禁則処理します。
@@ -58,6 +67,54 @@
  *   （行末禁則文字が連続した場合、行末に対象の文字が表示されることがあります）
  * - 行頭行末揃えを行いません。（必ずしも各行の行頭と行末が一直線に揃いません）
  * - 分離禁則を適用しません。（英単語や連数字の途中で改行されることがあります）
+ */
+
+/*:en
+ * @plugindesc Automatically line break when text width is over window's
+ * @author DarkPlasma
+ * @license MIT
+ *
+ * @target MZ
+ * @url https://github.com/elleonard/DarkPlasma-MZ-Plugins/tree/release
+ *
+ * @param prohibitLineBreakBefore
+ * @desc character that cannot be displayed at the beginning of line.
+ * @text Character can't be at the beginning
+ * @type string
+ * @default ,)]｝、〕〉》」』】〙〗〟’”｠»ゝゞーァィゥェォッャュョヮヵヶぁぃぅぇぉっゃゅょゎゕゖㇰㇱㇲㇳㇴㇵㇶㇷㇸㇹㇷ゚ㇺㇻㇼㇽㇾㇿ々〻‐゠–〜～?!‼⁇⁈⁉・:;/。.
+ *
+ * @param prohibitLineBreakAfter
+ * @desc character that cannot be displayed at the end of line.
+ * @text Character can't be at the end
+ * @type string
+ * @default ([｛〔〈《「『【〘〖〝‘“｟«
+ *
+ * @param ignoreAutoLineBreakWindows
+ * @desc Windows should not be auto line breaking.
+ * @text No auto line break windows
+ * @type string[]
+ * @default []
+ *
+ * @param lineWidthMargin
+ * @desc Margin for characters that cannot be at the beginning or end of line.
+ * @text Line width margin
+ * @type number
+ * @default 4
+ *
+ * @param wordBaseLineBreak
+ * @desc When this is ON, it is enabled word base line breaking. Word is separated by half-width space.
+ * @text Word base line break
+ * @type boolean
+ * @default false
+ *
+ * @help
+ * version: 1.3.0
+ * This is plugin for automatically line break when text width is over window's.
+ *
+ * Especially, it supports line breaking rule for Japanese (multi byte characters) partially.
+ * (see Japanese help in this file.)
+ *
+ * For English, I recommend enabling word base line break settings.
  */
 
 (() => {
@@ -81,228 +138,214 @@
       return String(e || '');
     }),
     lineWidthMargin: Number(pluginParameters.lineWidthMargin || 4),
+    wordBaseLineBreak: String(pluginParameters.wordBaseLineBreak || false) === 'true',
   };
 
-  /**
-   * 自動折返しが無効なウィンドウであるかどうか
-   * @return {boolean}
-   */
-  Window_Base.prototype.isIgnoreAutoLineBreakWindow = function () {
-    return settings.ignoreAutoLineBreakWindows.includes(this.constructor.name);
-  };
-
-  /**
-   * 自動折返しが有効かどうか
-   * @return {boolean}
-   */
-  Window_Base.prototype.isAutoLineBreakEnabled = function () {
-    if (this.isIgnoreAutoLineBreakWindow()) {
-      return false;
-    }
-    return true;
-  };
-
-  const _Window_Base_processCharacter = Window_Base.prototype.processCharacter;
-  Window_Base.prototype.processCharacter = function (textState) {
-    if (this.shouldLineBreakHere(textState)) {
-      this.flushTextState(textState);
-      if (textState.text[textState.index] !== '\n') {
-        this.processNewLine(textState);
+  function Window_AutoLineBreakMixIn(windowClass) {
+    /**
+     * 自動折返しが無効なウィンドウであるかどうか
+     */
+    windowClass.isIgnoreAutoLineBreakWindow = function () {
+      return settings.ignoreAutoLineBreakWindows.includes(this.constructor.name);
+    };
+    /**
+     * 自動折返しが有効かどうか
+     */
+    windowClass.isAutoLineBreakEnabled = function () {
+      if (this.isIgnoreAutoLineBreakWindow()) {
+        return false;
       }
-      /**
-       * 改ページが必要になったら次の文字は処理しない
-       */
-      if (this.needsNewPage && this.needsNewPage(textState)) {
-        return;
+      return true;
+    };
+    const _processCharacter = windowClass.processCharacter;
+    windowClass.processCharacter = function (textState) {
+      if (this.shouldLineBreakHere(textState)) {
+        this.flushTextState(textState);
+        if (textState.text[textState.index] !== '\n') {
+          this.processNewLine(textState);
+        }
+        /**
+         * 改ページが必要になったら次の文字は処理しない
+         */
+        if (this.needsNewPage && this.needsNewPage(textState)) {
+          return;
+        }
       }
-    }
-    if (textState.text[textState.index].charCodeAt(0) >= 0x20) {
-      textState.lineBuffer += textState.text[textState.index];
-    }
-    _Window_Base_processCharacter.call(this, textState);
-  };
-
-  const _Window_Base_processNewLine = Window_Base.prototype.processNewLine;
-  Window_Base.prototype.processNewLine = function (textState) {
-    _Window_Base_processNewLine.call(this, textState);
-    textState.lineBuffer = this.createTextBuffer(textState.rtl);
-  };
-
-  const _Window_Base_createTextState = Window_Base.prototype.createTextState;
-  Window_Base.prototype.createTextState = function (text, x, y, width) {
-    const textState = _Window_Base_createTextState.call(this, text, x, y, width);
-    textState.lineBuffer = textState.buffer;
-    return textState;
-  };
-
-  const _Window_Base_flushTextState = Window_Base.prototype.flushTextState;
-  Window_Base.prototype.flushTextState = function (textState) {
-    _Window_Base_flushTextState.call(this, textState);
-    textState.lineBuffer = textState.buffer;
-  };
-
-  /**
-   * 自動改行すべき状態であるかどうか
-   * @param {Window_Base.TextState} textState
-   * @return {boolean}
-   */
-  Window_Base.prototype.shouldLineBreakHere = function (textState) {
-    if (!textState || textState.index === 0 || !textState.text[textState.index] || !this.isAutoLineBreakEnabled()) {
+      if (textState.text[textState.index].charCodeAt(0) >= 0x20) {
+        textState.lineBuffer += textState.text[textState.index];
+      }
+      _processCharacter.call(this, textState);
+    };
+    const _processNewLine = windowClass.processNewLine;
+    windowClass.processNewLine = function (textState) {
+      _processNewLine.call(this, textState);
+      textState.lineBuffer = this.createTextBuffer(textState.rtl);
+    };
+    const _createTextState = windowClass.createTextState;
+    windowClass.createTextState = function (text, x, y, width) {
+      const textState = _createTextState.call(this, text, x, y, width);
+      textState.lineBuffer = textState.buffer;
+      return textState;
+    };
+    const _flushTextState = windowClass.flushTextState;
+    windowClass.flushTextState = function (textState) {
+      _flushTextState.call(this, textState);
+      textState.lineBuffer = textState.buffer;
+    };
+    /**
+     * 自動改行すべき状態であるかどうか
+     */
+    windowClass.shouldLineBreakHere = function (textState) {
+      if (!textState || textState.index === 0 || !textState.text[textState.index] || !this.isAutoLineBreakEnabled()) {
+        return false;
+      }
+      return this.shouldCharacterBaseLineBreakHere(textState) || this.shouldWordBaseLineBreakHere(textState);
+    };
+    windowClass.shouldCharacterBaseLineBreakHere = function (textState) {
+      let nextCharacter = textState.text[textState.index];
+      if (this.isSurrogatePair(nextCharacter)) {
+        return false;
+      }
+      let next2Character = this.nextNCharacter(textState, 2);
+      let next3Character = this.nextNCharacter(textState, 3);
+      const size = this.textWidth(`${textState.lineBuffer}${nextCharacter}`);
+      if (size + textState.x > this.lineWidth()) {
+        return !this.isProhibitLineBreakBefore(nextCharacter);
+      } else if (
+        next2Character &&
+        next3Character &&
+        size + textState.x + this.textWidth(`${next2Character}${next3Character}`) > this.lineWidth()
+      ) {
+        // 行頭禁則文字が行末に2つ並んでおり、かつ枠をはみ出す場合
+        // 例えば、 しゅー のように、2つまでであれば並ぶ余地が十分に考えられる
+        // 3つ以上は流石に先読みコストがかかりすぎるので対応しない
+        return this.isProhibitLineBreakBefore(next2Character) && this.isProhibitLineBreakBefore(next3Character);
+      }
+      // 行末禁則チェック
+      if (
+        next2Character &&
+        this.textWidth(`${textState.lineBuffer}${nextCharacter}${next2Character}`) + textState.x > this.lineWidth()
+      ) {
+        return this.isProhibitLineBreakAfter(nextCharacter);
+      }
       return false;
-    }
-    let nextCharacter = textState.text[textState.index];
-    if (this.isSurrogatePair(nextCharacter)) {
+    };
+    windowClass.shouldWordBaseLineBreakHere = function (textState) {
+      if (!settings.wordBaseLineBreak) {
+        return false;
+      }
+      const isInitialOfWord = textState.text[textState.index - 1] === ' ' && textState.text[textState.index] !== ' ';
+      const nextSpaceIndex = textState.text.indexOf(' ', textState.index + 1);
+      if (!isInitialOfWord || nextSpaceIndex < 0) {
+        return false;
+      }
+      const currentWord = textState.text.substring(textState.index, nextSpaceIndex);
+      const size = this.textWidth(`${textState.lineBuffer}${currentWord}`);
+      return size + textState.x > this.lineWidth();
+    };
+    /**
+     * N文字先の文字
+     */
+    windowClass.nextNCharacter = function (textState, n) {
+      let targetIndex = textState.index + n - 1;
+      if (!textState.text[targetIndex]) {
+        return null;
+      }
+      return textState.text.substring(targetIndex, targetIndex + 1);
+    };
+    windowClass.isSurrogatePair = function (character) {
+      return character.charCodeAt(0) >= 0xdc00 && character.charCodeAt(0) <= 0xdfff;
+    };
+    /**
+     * 行末禁則文字かどうか
+     */
+    windowClass.isProhibitLineBreakBefore = function (character) {
+      return settings.prohibitLineBreakBefore.includes(character);
+    };
+    /**
+     * 行頭禁則文字かどうか
+     */
+    windowClass.isProhibitLineBreakAfter = function (character) {
+      return settings.prohibitLineBreakAfter.includes(character);
+    };
+    /**
+     * 折返し幅
+     */
+    windowClass.lineWidth = function () {
+      return this.contentsWidth() - settings.lineWidthMargin;
+    };
+    windowClass.lineWidthMargin = function () {
+      return settings.lineWidthMargin;
+    };
+  }
+  Window_AutoLineBreakMixIn(Window_Base.prototype);
+  function Window_DisableAutoLineBreakMixIn(windowClass) {
+    windowClass.isAutoLineBreakEnabled = function () {
       return false;
-    }
-    let next2Character = this.nextNCharacter(textState, 2);
-    let next3Character = this.nextNCharacter(textState, 3);
-    const size = this.textWidth(`${textState.lineBuffer}${nextCharacter}`);
-    if (size + textState.x > this.lineWidth()) {
-      return !this.isProhibitLineBreakBefore(nextCharacter);
-    } else if (
-      next2Character &&
-      next3Character &&
-      size + textState.x + this.textWidth(`${next2Character}${next3Character}`) > this.lineWidth()
-    ) {
-      // 行頭禁則文字が行末に2つ並んでおり、かつ枠をはみ出す場合
-      // 例えば、 しゅー のように、2つまでであれば並ぶ余地が十分に考えられる
-      // 3つ以上は流石に先読みコストがかかりすぎるので対応しない
-      return this.isProhibitLineBreakBefore(next2Character) && this.isProhibitLineBreakBefore(next3Character);
-    }
-    // 行末禁則チェック
-    if (
-      next2Character &&
-      this.textWidth(`${textState.lineBuffer}${nextCharacter}${next2Character}`) + textState.x > this.lineWidth()
-    ) {
-      return this.isProhibitLineBreakAfter(nextCharacter);
-    }
-    return false;
-  };
-
-  /**
-   * N文字先の文字
-   * @param {MZ.TextState} textState
-   * @param {number} n
-   * @return {string|null}
-   */
-  Window_Base.prototype.nextNCharacter = function (textState, n) {
-    let targetIndex = textState.index + n - 1;
-    if (!textState.text[targetIndex]) {
-      return null;
-    }
-    return textState.text.substring(targetIndex, targetIndex + 1);
-  };
-
-  Window_Base.prototype.isSurrogatePair = function (character) {
-    return character.charCodeAt(0) >= 0xdc00 && character.charCodeAt(0) <= 0xdfff;
-  };
-
-  /**
-   * 行末禁則文字かどうか
-   * @param {string} character
-   * @return {boolean}
-   */
-  Window_Base.prototype.isProhibitLineBreakBefore = function (character) {
-    return settings.prohibitLineBreakBefore.includes(character);
-  };
-
-  /**
-   * 行頭禁則文字かどうか
-   * @param {string} character
-   * @return {boolean}
-   */
-  Window_Base.prototype.isProhibitLineBreakAfter = function (character) {
-    return settings.prohibitLineBreakAfter.includes(character);
-  };
-
-  /**
-   * 折返し幅
-   * @return {number}
-   */
-  Window_Base.prototype.lineWidth = function () {
-    return this.contentsWidth() - settings.lineWidthMargin;
-  };
-
-  Window_Base.prototype.lineWidthMargin = function () {
-    return settings.lineWidthMargin;
-  };
-
+    };
+  }
   /**
    * Window_ChoiceList は選択肢幅によってウィンドウサイズが変わる
    * そのため、自動折返しの対象外とする
-   * @return {boolean}
    */
-  Window_ChoiceList.prototype.isAutoLineBreakEnabled = function () {
-    return false;
-  };
-
+  Window_DisableAutoLineBreakMixIn(Window_ChoiceList.prototype);
   /**
    * Window_NameBox は名前の幅によってウィンドウサイズが変わる
    * そのため、自動折返しの対象外とする
-   * @return {boolean}
    */
-  Window_NameBox.prototype.isAutoLineBreakEnabled = function () {
-    return false;
-  };
-
-  const _Window_BattleLog_initialize = Window_BattleLog.prototype.initialize;
-  Window_BattleLog.prototype.initialize = function () {
-    /**
-     * 各テキストの改行の数
-     * @type {number[]}
-     */
-    this._newLines = [];
-    _Window_BattleLog_initialize.apply(this, arguments);
-  };
-
-  const _Window_BattleLog_clear = Window_BattleLog.prototype.clear;
-  Window_BattleLog.prototype.clear = function () {
-    _Window_BattleLog_clear.call(this);
-    this._newLines = [];
-  };
-
-  const _Window_BattleLog_refresh = Window_BattleLog.prototype.refresh;
-  Window_BattleLog.prototype.refresh = function () {
-    this._lines.forEach((text, index) => {
-      this._newLines[index] = 0;
-      this.textSizeEx(text);
-    });
-    _Window_BattleLog_refresh.call(this);
-  };
-
-  const _Window_BattleLog_addText = Window_BattleLog.prototype.addText;
-  Window_BattleLog.prototype.addText = function (text) {
-    this._newLines.push(0);
-    _Window_BattleLog_addText.call(this, text);
-  };
-
-  const _Window_BattleLog_numLines = Window_BattleLog.prototype.numLines;
-  Window_BattleLog.prototype.numLines = function () {
-    return _Window_BattleLog_numLines.call(this) + this._newLines.reduce((prev, current) => prev + current, 0);
-  };
-
-  const _Window_BattleLog_drawLineText = Window_BattleLog.prototype.drawLineText;
-  Window_BattleLog.prototype.drawLineText = function (index) {
-    /**
-     * 描画中のindex
-     * @type {number}
-     */
-    this._currentIndex = index;
-    _Window_BattleLog_drawLineText.call(this, index);
-  };
-
-  const _Window_BattleLog_processNewLine = Window_BattleLog.prototype.processNewLine;
-  Window_BattleLog.prototype.processNewLine = function (textState) {
-    _Window_BattleLog_processNewLine.call(this, textState);
-    if (!textState.drawing) {
-      this._newLines[this._currentIndex]++;
-    }
-  };
-
-  const _Window_BattleLog_lineRect = Window_BattleLog.prototype.lineRect;
-  Window_BattleLog.prototype.lineRect = function (index) {
-    const rect = _Window_BattleLog_lineRect.call(this, index);
-    rect.y += this._newLines.slice(0, index).reduce((prev, current) => prev + current, 0) * this.lineHeight();
-    return rect;
-  };
+  Window_DisableAutoLineBreakMixIn(Window_NameBox.prototype);
+  function Window_BattleLog_AutoLineBreakMixIn(windowClass) {
+    const _initialize = windowClass.initialize;
+    windowClass.initialize = function (rect) {
+      /**
+       * 各テキストの改行の数
+       */
+      this._newLines = [];
+      _initialize.call(this, rect);
+    };
+    const _clear = windowClass.clear;
+    windowClass.clear = function () {
+      _clear.call(this);
+      this._newLines = [];
+    };
+    const _refresh = windowClass.refresh;
+    windowClass.refresh = function () {
+      this._lines.forEach((text, index) => {
+        this._newLines[index] = 0;
+        this.textSizeEx(text);
+      });
+      _refresh.call(this);
+    };
+    const _addText = windowClass.addText;
+    windowClass.addText = function (text) {
+      this._newLines.push(0);
+      _addText.call(this, text);
+    };
+    const _numLines = windowClass.numLines;
+    windowClass.numLines = function () {
+      return _numLines.call(this) + this._newLines.reduce((prev, current) => prev + current, 0);
+    };
+    const _drawLineText = windowClass.drawLineText;
+    windowClass.drawLineText = function (index) {
+      /**
+       * 描画中のindex
+       */
+      this._currentIndex = index;
+      _drawLineText.call(this, index);
+    };
+    const _processNewLine = windowClass.processNewLine;
+    windowClass.processNewLine = function (textState) {
+      _processNewLine.call(this, textState);
+      if (!textState.drawing) {
+        this._newLines[this._currentIndex]++;
+      }
+    };
+    const _lineRect = windowClass.lineRect;
+    windowClass.lineRect = function (index) {
+      const rect = _lineRect.call(this, index);
+      rect.y += this._newLines.slice(0, index).reduce((prev, current) => prev + current, 0) * this.lineHeight();
+      return rect;
+    };
+  }
+  Window_BattleLog_AutoLineBreakMixIn(Window_BattleLog.prototype);
 })();
