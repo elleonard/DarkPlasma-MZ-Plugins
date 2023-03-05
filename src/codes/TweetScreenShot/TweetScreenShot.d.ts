cbee4f2efe365085db0e59899f3bdb65085038c9
plasma <librarian256@gmail.com>
2023-03-05 19:39:46 +0900

    不要ファイルの削除

--- src/codes/TweetScreenShot/TweetScreenShot.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference path="../../typings/rmmz.d.ts" />
-
-declare interface SceneManager {
-  tweetScreenshot(): void;
-  tweetImage(base64Image: string): void;
-  notifyTweetScreenshotError(error: Error): void;
-}
