173a85c01ab3d22b2ef949126ede17b18491d0d2
plasma <librarian256@gmail.com>
2023-03-13 02:28:11 +0900

    型定義の修正

--- src/codes/FusionItem/FusionItem.d.ts
+++ src/codes/FusionItem/FusionItem.d.ts
@@ -45,12 +45,75 @@ declare interface Game_Party {
   numEquippedItem(item: MZ.Item|MZ.Weapon|MZ.Armor): number;
 }
 
-declare interface Scene_FusionItem extends Scene_Shop {
+declare interface Scene_FusionItem extends Scene_ShopLike<FusionItemGoods, Window_FusionShopBuy> {
 
 }
+
+declare class Scene_ShopLike<TGoods, TBuyWindow> extends Scene_MenuBase {
+  _goods: TGoods[];
+  public _item: MZ.Item | MZ.Weapon | MZ.Armor | null;
+  public _helpWindow: Window_Help;
+  public _goldWindow: Window_Gold;
+  public _commandWindow: Window_ShopCommand;
+  public _dummyWindow: Window_Base;
+  public _numberWindow: Window_ShopNumber;
+  public _statusWindow: Window_ShopStatus;
+  public _buyWindow: TBuyWindow;
+  public _categoryWindow: Window_ItemCategory;
+  public _sellWindow: Window_ShopSell;
+
+  prepare(goods: TGoods[]): void;
+
+  create(): void;
+  createGoldWindow(): void;
+  goldWindowRect(): Rectangle;
+  commandWindowRect(): Rectangle;
+  createDummyWindow(): void;
+  dummyWindowRect(): Rectangle;
+  createNumberWindow(): void;
+  numberWindowRect(): Rectangle;
+  statusWindowRect(): Rectangle;
+  buyWindowRect(): Rectangle;
+  createCategoryWindow(): void;
+  categoryWindowRect(): Rectangle;
+  createSellWindow(): void;
+  sellWindowRect(): Rectangle;
+  statusWidth(): number;
+
+  activateBuyWindow(): void;
+  activateSellWindow(): void;
+
+  commandBuy(): void;
+  commandSell(): void;
+
+  onCategoryOk(): void;
+  onCategoryCancel(): void;
+  onBuyOk(): void;
+  onSellOk(): void;
+  onSellCancel(): void;
+  onNumberOk(): void;
+  onNumberCancel(): void;
+
+  doBuy(number: number): void;
+  doSell(number: number): void;
+  endNumberInput(): void;
+
+  maxBuy(): number;
+  maxSell(): number;
+  money(): number;
+  currencyUnit(): string;
+  sellingPrice(): number;
+}
+
+type Constructor<T = Scene_MenuBase> = new (...args: any[]) => T;
+function Scene_ShopLikeMixIn<TScene extends Constructor, TGoods, TBuyWindow>(sceneClass: TScene): Scene_ShopLike<TGoods, TBuyWindow>;
 
 declare interface Window_FusionShopStatus extends Window_ShopStatus {
   _materials: FusionItemMaterial[];
 
   materialLineHeight(): number;
+}
+
+declare interface Window_FusionShopBuy extends Window_ShopBuy {
+
 }
