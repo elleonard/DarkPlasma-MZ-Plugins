0ba0338e6c8f2021f62e23f8ddaabb1e1bfddf81
plasma <librarian256@gmail.com>
2023-03-18 11:55:12 +0900

    1.0.0 システムのタイプや弱体にアイコンを設定する

---
+++ src/codes/SystemTypeIcon/DarkPlasma_SystemTypeIcon.ts
@@ -0,0 +1,19 @@
+/// <reference path="./SystemTypeIcon.d.ts" />
+
+import { settings } from "./_build/DarkPlasma_SystemTypeIcon_parameters";
+
+function Game_System_SystemTypeIconMixIn(gameSystem: Game_System) {
+  gameSystem.elementIconIndex = function (elementId) {
+    return settings.elementIcons[elementId] || 0;
+  };
+
+  gameSystem.largeDebuffStatusIconIndex = function (paramName) {
+    return settings.debuffStatusIcons[paramName]?.large || 0;
+  };
+
+  gameSystem.smallDebuffStatusIconIndex = function (paramName) {
+    return settings.debuffStatusIcons[paramName]?.small || 0;
+  };
+}
+
+Game_System_SystemTypeIconMixIn(Game_System.prototype);
