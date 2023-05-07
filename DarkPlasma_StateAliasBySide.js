35b905aa9d17af4e30993f58f7eb313c10a394b5
elleonard <6374339+elleonard@users.noreply.github.com>
2023-05-07 12:28:32 +0900

    a45269e2f00dfc4ab094c7e29a5ad85a819ea3d7

--- DarkPlasma_StateAliasBySide.js
+++ DarkPlasma_StateAliasBySide.js
@@ -1,13 +1,15 @@
-// DarkPlasma_StateAliasBySide 1.0.0
+// DarkPlasma_StateAliasBySide 1.0.1
 // Copyright (c) 2022 DarkPlasma
 // This software is released under the MIT license.
 // http://opensource.org/licenses/mit-license.php
 
 /**
+ * 2023/05/07 1.0.1 typescript移行
+ *                  エイリアス元ステートの無効フラグを貫通する不具合を修正
  * 2022/03/11 1.0.0 公開
  */
 
-/*:ja
+/*:
  * @plugindesc 敵味方に応じてステートを変更する
  * @author DarkPlasma
  * @license MIT
@@ -16,20 +18,22 @@
  * @url https://github.com/elleonard/DarkPlasma-MZ-Plugins/tree/release
  *
  * @help
- * version: 1.0.0
+ * version: 1.0.1
  * 敵・味方に応じて別のステートを付与することができます。
  *
  * <stateAliasEnemy:X>
  * ステートのメモ欄にこう記述した場合、
  * 敵にそのステートを付与する場合、
  * 代わりにステートID:Xのステートが付与されます。
- * ステート有効度や無効フラグはXではなく元のステートのものが使用されます。
+ * ステート有効度はXではなく元のステートのものが使用されます。
+ * ステート無効フラグは両方のステートのものが使用されます。
  *
  * <stateAliasActor:X>
  * ステートのメモ欄にこう記述した場合、
  * 味方にそのステートを付与する場合、
  * 代わりにステートID:Xのステートが付与されます。
- * ステート有効度や無効フラグはXではなく元のステートのものが使用されます。
+ * ステート有効度はXではなく元のステートのものが使用されます。
+ * ステート無効フラグは両方のステートのものが使用されます。
  *
  * 例:
  * <stateAliasEnemy:14>
@@ -37,12 +41,8 @@
  * ステートID4ではなく14が付与されます。
  * 味方用の毒と敵用の毒を分けたい場合に有効です。
  *
- * この場合、ステート有効度及び無効フラグはID4のものが使用されます。
- *
- * ステートID:14 の毒に対して、以下のように記述します。
- * <stateAliasActor:4>
- * ステートID14を味方に付与する場合、
- * ステートID14ではなく4が付与されます。
+ * この場合、ステート有効度はID4のものが使用されます。
+ * ステート無効フラグはID4, ID14どちらかがついていれば無効となります。
  */
 
 (() => {
@@ -56,18 +56,15 @@
     gameBattler.addState = function (stateId) {
       _addState.call(this, this.aliasedStateIdBySide(stateId));
     };
-
-    /**
-     * @param {number} stateId
-     * @return {number}
-     */
     gameBattler.aliasedStateIdBySide = function (stateId) {
+      if (!this.isStateAddable(stateId)) {
+        return stateId;
+      }
       if (this.isActor()) {
         return Number($dataStates[stateId].meta.stateAliasActor || stateId);
       }
       return Number($dataStates[stateId].meta.stateAliasEnemy || stateId);
     };
   }
-
   Game_Battler_StateAliasBySideMixIn(Game_Battler.prototype);
 })();
