df332dbf8e42a04995c7ce9088bb11f027561f29
elleonard <6374339+elleonard@users.noreply.github.com>
2023-06-14 02:07:09 +0900

    0d4690db1a5b92897c52b1c57bbd5a7e611aaefe

--- DarkPlasma_MasterVolume.js
+++ DarkPlasma_MasterVolume.js
@@ -1,14 +1,16 @@
-// DarkPlasma_MasterVolume 1.0.1
+// DarkPlasma_MasterVolume 1.0.2
 // Copyright (c) 2022 DarkPlasma
 // This software is released under the MIT license.
 // http://opensource.org/licenses/mit-license.php
 
 /**
+ * 2023/06/14 1.0.2 typescript移行
+ *                  初期値の設定が効かない不具合を修正
  * 2022/08/07 1.0.1 マスターボリューム0で保存するとデフォルトに戻される不具合を修正
  *            1.0.0 公開
  */
 
-/*:ja
+/*:
  * @plugindesc マスターボリュームを設定する
  * @author DarkPlasma
  * @license MIT
@@ -33,7 +35,7 @@
  * @default 20
  *
  * @help
- * version: 1.0.1
+ * version: 1.0.2
  * オプション画面にマスターボリュームを追加します。
  * BGM/BGS/ME/SEすべての音量を一括調整できます。
  *
@@ -52,14 +54,20 @@
   const pluginParameters = pluginParametersOf(pluginName);
 
   const settings = {
-    optionName: String(pluginParameters.optionName || '全体 音量'),
+    optionName: String(pluginParameters.optionName || `全体 音量`),
     defaultVolume: Number(pluginParameters.defaultVolume || 100),
     offset: Number(pluginParameters.offset || 20),
   };
 
-  /**
-   * @param {typeof ConfigManager} configManager
-   */
+  function WebAudio_MasterVolumeMixIn(webAudio) {
+    const _initialize = webAudio.initialize;
+    webAudio.initialize = function () {
+      const result = _initialize.call(this);
+      this.setMasterVolume(settings.defaultVolume);
+      return result;
+    };
+  }
+  WebAudio_MasterVolumeMixIn(WebAudio);
   function ConfigManager_MasterVolumeMixIn(configManager) {
     Object.defineProperty(configManager, 'masterVolume', {
       get: function () {
@@ -70,14 +78,12 @@
       },
       configurable: true,
     });
-
     const _makeData = configManager.makeData;
     configManager.makeData = function () {
       const config = _makeData.call(this);
       config.masterVolume = this.masterVolume;
       return config;
     };
-
     const _applyData = configManager.applyData;
     configManager.applyData = function (config) {
       _applyData.call(this, config);
@@ -86,15 +92,8 @@
         : settings.defaultVolume;
     };
   }
-
   ConfigManager_MasterVolumeMixIn(ConfigManager);
-
-  /**
-   * @param {typeof AudioManager} audioManager
-   */
   function AudioManager_MasterVolumeMixIn(audioManager) {
-    audioManager._masterVolume = 100;
-
     Object.defineProperty(audioManager, 'masterVolume', {
       get: function () {
         return this._masterVolume;
@@ -103,39 +102,31 @@
         this._masterVolume = value;
         WebAudio.setMasterVolume(this._masterVolume / 100);
         Video.setVolume(this._masterVolume / 100);
+        if (this._currentBgm) {
+          this.updateBgmParameters(this._currentBgm);
+        }
       },
       configurable: true,
     });
+    audioManager.masterVolume = settings.defaultVolume;
   }
-
   AudioManager_MasterVolumeMixIn(AudioManager);
-
-  /**
-   * @param {Scene_Options.prototype} sceneOptions
-   */
   function Scene_Options_MasterVolumeMixIn(sceneOptions) {
     const _maxCommands = sceneOptions.maxCommands;
     sceneOptions.maxCommands = function () {
       return _maxCommands.call(this) + 1;
     };
   }
-
   Scene_Options_MasterVolumeMixIn(Scene_Options.prototype);
-
-  /**
-   * @param {Window_Options.prototype} windowClass
-   */
   function Window_Options_MasterVolumeMixIn(windowClass) {
     const _addVolumeOptions = windowClass.addVolumeOptions;
     windowClass.addVolumeOptions = function () {
       this.addCommand(settings.optionName, 'masterVolume');
       _addVolumeOptions.call(this);
     };
-
     windowClass.volumeOffset = function () {
       return settings.offset;
     };
   }
-
   Window_Options_MasterVolumeMixIn(Window_Options.prototype);
 })();
