1edec9efaae923bcd55d98aa1d944b4841a4bc3c
elleonard <6374339+elleonard@users.noreply.github.com>
2023-09-28 11:38:08 +0900

    fb98b472fa60a067f0e586455a0784692f282bfe

--- DarkPlasma_DashSpeed.js
+++ DarkPlasma_DashSpeed.js
@@ -1,16 +1,17 @@
-// DarkPlasma_DashSpeed 1.0.3
+// DarkPlasma_DashSpeed 1.0.4
 // Copyright (c) 2020 DarkPlasma
 // This software is released under the MIT license.
 // http://opensource.org/licenses/mit-license.php
 
 /**
+ * 2023/09/28 1.0.4 typescript移行
  * 2021/07/05 1.0.3 MZ 1.3.2に対応
  * 2021/06/22 1.0.2 サブフォルダからの読み込みに対応
  * 2020/10/10 1.0.1 リファクタ
  * 2020/10/02 1.0.0 公開
  */
 
-/*:ja
+/*:
  * @plugindesc ダッシュ速度を変更する
  * @author DarkPlasma
  * @license MIT
@@ -34,7 +35,7 @@
  * @default 1
  *
  * @help
- * version: 1.0.3
+ * version: 1.0.4
  * ダッシュ速度を変更します。
  */
 
@@ -45,6 +46,8 @@
     return arguments[1];
   });
 
+  const command_changeDashSpeed = 'changeDashSpeed';
+
   const pluginParametersOf = (pluginName) => PluginManager.parameters(pluginName);
 
   const pluginParameters = pluginParametersOf(pluginName);
@@ -53,29 +56,24 @@
     defaultDashSpeed: Number(pluginParameters.defaultDashSpeed || 1),
   };
 
-  const PLUGIN_COMMAND_NAME = {
-    CHANGE_DASH_SPEED: 'changeDashSpeed',
-  };
-
-  const _Game_Player_initMember = Game_Player.prototype.initMembers;
-  Game_Player.prototype.initMembers = function () {
-    _Game_Player_initMember.call(this);
-    this._dashSpeed = settings.defaultDashSpeed;
-  };
-
-  Game_Player.prototype.realMoveSpeed = function () {
-    return this._moveSpeed + (this.isDashing() ? this._dashSpeed : 0);
-  };
-
-  Game_Player.prototype.dashSpeed = function () {
-    return this._dashSpeed === undefined ? settings.defaultDashSpeed : this._dashSpeed;
-  };
-
-  Game_Player.prototype.setDashSpeed = function (dashSpeed) {
-    this._dashSpeed = dashSpeed;
-  };
-
-  PluginManager.registerCommand(pluginName, PLUGIN_COMMAND_NAME.CHANGE_DASH_SPEED, function (args) {
+  PluginManager.registerCommand(pluginName, command_changeDashSpeed, function (args) {
     $gamePlayer.setDashSpeed(Number(args.dashSpeed));
   });
+  function Game_Player_DashSpeedMixIn(gamePlayer) {
+    const _initMember = gamePlayer.initMembers;
+    gamePlayer.initMembers = function () {
+      _initMember.call(this);
+      this._dashSpeed = settings.defaultDashSpeed;
+    };
+    gamePlayer.realMoveSpeed = function () {
+      return this._moveSpeed + (this.isDashing() ? this._dashSpeed : 0);
+    };
+    gamePlayer.dashSpeed = function () {
+      return this._dashSpeed === undefined ? settings.defaultDashSpeed : this._dashSpeed;
+    };
+    gamePlayer.setDashSpeed = function (dashSpeed) {
+      this._dashSpeed = dashSpeed;
+    };
+  }
+  Game_Player_DashSpeedMixIn(Game_Player.prototype);
 })();
