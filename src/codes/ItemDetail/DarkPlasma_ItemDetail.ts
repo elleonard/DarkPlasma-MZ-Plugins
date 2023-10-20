e58dc43eb039fe2cc7a826a5d845acd824454623
plasma <6374339+elleonard@users.noreply.github.com>
2023-10-20 11:50:38 +0900

    1.0.0 アイテムの詳細説明文を表示する

---
+++ src/codes/ItemDetail/DarkPlasma_ItemDetail.ts
@@ -0,0 +1,155 @@
+/// <reference path="./ItemDetail.d.ts" />
+
+import { settings } from "./_build/DarkPlasma_ItemDetail_parameters";
+
+function Scene_Item_DetailMixIn(sceneItem: Scene_Item) {
+  const _create = sceneItem.create;
+  sceneItem.create = function () {
+    _create.call(this);
+    this.createDetailWindow();
+  };
+
+  const _createItemWindow = sceneItem.createItemWindow;
+  sceneItem.createItemWindow = function () {
+    _createItemWindow.call(this);
+    this._itemWindow.setHandler('detail', () => this.toggleDetailWindow());
+  };
+
+  sceneItem.createDetailWindow = function () {
+    this._detailWindowLayer = new WindowLayer();
+    this._detailWindowLayer.x = (Graphics.width - Graphics.boxWidth);
+    this._detailWindowLayer.y = (Graphics.height - Graphics.boxHeight);
+    this.addChild(this._detailWindowLayer);
+    this._detailWindow = new Window_ItemDetail(this.detailWindowRect());
+    this._detailWindowLayer.addChild(this._detailWindow);
+    this._itemWindow.setDetailWindow(this._detailWindow);
+  };
+
+  sceneItem.detailWindowRect = function () {
+    return this.itemWindowRect();
+  };
+
+  sceneItem.toggleDetailWindow = function () {
+    this._itemWindow.activate();
+    this._detailWindow.scrollTo(0, 0);
+    if (!this._detailWindow.visible) {
+      this._detailWindow.show();
+    } else {
+      this._detailWindow.hide();
+    }
+  };
+}
+
+Scene_Item_DetailMixIn(Scene_Item.prototype);
+
+Window_CustomKeyHandlerMixIn(settings.openDetailKey, Window_ItemList.prototype, 'detail');
+
+function Window_ItemList_DetailMixIn(windowClass: Window_ItemList) {
+  windowClass.setDetailWindow = function (detailWindow) {
+    this._detailWindow = detailWindow;
+  };
+
+  const _setHelpWindowItem = windowClass.setHelpWindowItem;
+  windowClass.setHelpWindowItem = function (item) {
+    _setHelpWindowItem.call(this, item);
+    this._detailWindow?.setItem(item as MZ.Item);
+  };
+
+  const _isCursorMovable = windowClass.isCursorMovable;
+  windowClass.isCursorMovable = function () {
+    return _isCursorMovable.call(this)
+      && (!this._detailWindow || !this._detailWindow.visible);
+  };
+
+  const _isOkEnabled = windowClass.isOkEnabled;
+  windowClass.isOkEnabled = function () {
+    return _isOkEnabled.call(this)
+      && (!this._detailWindow || !this._detailWindow.visible);
+  };
+
+  const _processCancel = windowClass.processCancel;
+  windowClass.processCancel = function () {
+    this._detailWindow?.hide();
+    _processCancel.call(this);
+  };
+}
+
+Window_ItemList_DetailMixIn(Window_ItemList.prototype);
+
+class Window_ItemDetail extends Window_Scrollable {
+  _text: string;
+
+  initialize(rect: Rectangle) {
+    super.initialize(rect);
+    this._text = '';
+    this.hide();
+  }
+
+  setItem(item: MZ.Item|null) {
+    this.setText(String(item?.meta.detail || ''));
+  }
+
+  setText(text: string) {
+    if (this._text !== text) {
+      this._text = text;
+      this.refresh();
+    }
+  }
+
+  drawDetail(detail: string) {
+    this.drawTextEx(detail, 0, this.baseLineY());
+  }
+
+  baseLineY() {
+    return -(this.scrollBaseY()/this.scrollBlockHeight()) * this.lineHeight();
+  }
+
+  public scrollBlockHeight(): number {
+    return this.lineHeight();
+  }
+
+  public overallHeight(): number {
+    return this.textSizeEx(this._text).height + settings.heightAdjustment;
+  }
+
+  public paint(): void {
+    this.contents.clear();
+    this.drawDetail(this._text);
+  }
+
+  refresh() {
+    this.paint();
+  }
+
+  public update(): void {
+    super.update();
+    this.processCursorMove();
+  }
+
+  processCursorMove() {
+    if (this.isCursorMovable()) {
+      if (Input.isRepeated('down')) {
+        this.cursorDown();
+      }
+      if (Input.isRepeated('up')) {
+        this.cursorUp();
+      }
+    }
+  }
+
+  isCursorMovable() {
+    return this.visible;
+  }
+
+  cursorDown() {
+    if (this.scrollY() <= this.maxScrollY()) {
+      this.smoothScrollDown(1);
+    }
+  }
+
+  cursorUp() {
+    if (this.scrollY() > 0) {
+      this.smoothScrollUp(1);
+    }
+  }
+}
---
+++ src/codes/ItemDetail/ItemDetail.d.ts
@@ -0,0 +1,24 @@
+/// <reference path="../../typings/rmmz.d.ts" />
+/// <reference path="../CustomKeyHandler/CustomKeyHandler.d.ts" />
+/// <reference path="../CustomKeyHandler/CustomKeyHandlerExport.d.ts" />
+
+declare interface Scene_Item {
+  _detailWindowLayer: WindowLayer;
+  _detailWindow: Window_ItemDetail;
+
+  createDetailWindow(): void;
+  detailWindowRect(): Rectangle;
+  toggleDetailWindow(): void;
+}
+
+declare interface Window_ItemList {
+  _detailWindow?: Window_ItemDetail;
+
+  setDetailWindow(detailWindow: Window_ItemDetail): void;
+}
+
+declare interface Window_ItemDetail extends Window_Scrollable {
+  _text: string;
+
+  setItem(item: MZ.Item|null): void;
+}
