2d3ee8d606073ee2e4d060968bffa1ef09691d8f
plasma <6374339+elleonard@users.noreply.github.com>
2023-11-18 14:15:44 +0900

    1.1.0 戦闘不能後も継続するステート一覧を取得するインターフェース追加

--- src/codes/StateWithDeath/DarkPlasma_StateWithDeath.ts
+++ src/codes/StateWithDeath/DarkPlasma_StateWithDeath.ts
@@ -3,15 +3,19 @@ import { settings } from './_build/DarkPlasma_StateWithDeath_parameters';
 /**
  * @param {Game_BattlerBase.prototype} gameBattlerBase
  */
-function Game_BattlerBase_StateWithDeathMixIn(gameBattlerBase) {
+function Game_BattlerBase_StateWithDeathMixIn(gameBattlerBase: Game_BattlerBase) {
   const _die = gameBattlerBase.die;
   gameBattlerBase.die = function () {
-    const continueStates = this._states.filter((stateId) => settings.statesWithDeath.includes(stateId));
-    const continueStateTurns = {};
+    const continueStates = this.continueStateIdsAfterDeath();
+    const continueStateTurns: {[stateId: number]: number} = {};
     continueStates.forEach((stateId) => (continueStateTurns[stateId] = this._stateTurns[stateId]));
     _die.call(this);
     this._states = continueStates;
     this._stateTurns = continueStateTurns;
+  };
+
+  gameBattlerBase.continueStateIdsAfterDeath = function () {
+    return this._states.filter((stateId) => settings.statesWithDeath.includes(stateId));
   };
 }
 
