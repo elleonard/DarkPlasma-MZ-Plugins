b7f7372451d9734ddb0813b5051d9866bf8dbbcb
elleonard <6374339+elleonard@users.noreply.github.com>
2024-02-08 12:10:24 +0900

    c36ce0f3eb5a635070afdd1679b9f23bcc487d7e

--- DarkPlasma_FallImages.js
+++ DarkPlasma_FallImages.js
@@ -1,9 +1,11 @@
-// DarkPlasma_FallImages 1.0.4
+// DarkPlasma_FallImages 1.0.5
 // Copyright (c) 2020 DarkPlasma
 // This software is released under the MIT license.
 // http://opensource.org/licenses/mit-license.php
 
 /**
+ * 2024/02/08 1.0.5 TypeScript移行
+ *                  画像が降っていない状態で画像を消すコマンドを実行するとエラーになる不具合を修正
  * 2021/07/05 1.0.4 MZ 1.3.2に対応
  * 2021/06/22 1.0.3 サブフォルダからの読み込みに対応
  * 2020/12/16 1.0.2 ゲーム終了時に正しく状態を初期化しない不具合を修正
@@ -11,7 +13,7 @@
  * 2020/10/24 1.0.0 公開
  */
 
-/*:ja
+/*:
  * @plugindesc 画面内に画像を降らせる
  * @author DarkPlasma
  * @license MIT
@@ -42,7 +44,7 @@
  * @desc 振らせている画像をフェードアウトさせ、止ませます。
  *
  * @help
- * version: 1.0.4
+ * version: 1.0.5
  * 何らかの画像を降らせる画面演出を提供します。
  *
  * プラグインパラメータにIDと画像ファイルを設定し、
@@ -125,6 +127,12 @@
     return arguments[1];
   });
 
+  const command_startFall = 'startFall';
+
+  const command_stopFall = 'stopFall';
+
+  const command_fadeOutFall = 'fadeOutFall';
+
   const pluginParametersOf = (pluginName) => PluginManager.parameters(pluginName);
 
   const pluginParameters = pluginParametersOf(pluginName);
@@ -135,7 +143,7 @@
         const parsed = JSON.parse(parameter);
         return {
           id: Number(parsed.id || 1),
-          file: String(parsed.file || ''),
+          file: String(parsed.file || ``),
           rows: Number(parsed.rows || 5),
           cols: Number(parsed.cols || 18),
           count: Number(parsed.count || 40),
@@ -150,26 +158,16 @@
     }),
   };
 
-  const PLUGIN_COMMAND_NAME = {
-    START_FALL: 'startFall',
-    STOP_FALL: 'stopFall',
-    FADEOUT_FALL: 'fadeOutFall',
-  };
-
   const START_Y_OFFSET = -100;
-
-  PluginManager.registerCommand(pluginName, PLUGIN_COMMAND_NAME.START_FALL, function (args) {
-    fallImageStatus.requestStart(Number(args.id));
+  PluginManager.registerCommand(pluginName, command_startFall, function (args) {
+    fallImageStatus?.requestStart(Number(args.id));
   });
-
-  PluginManager.registerCommand(pluginName, PLUGIN_COMMAND_NAME.STOP_FALL, function () {
-    fallImageStatus.requestStop();
+  PluginManager.registerCommand(pluginName, command_stopFall, function () {
+    fallImageStatus?.requestStop();
   });
-
-  PluginManager.registerCommand(pluginName, PLUGIN_COMMAND_NAME.FADEOUT_FALL, function () {
-    fallImageStatus.requestFadeOut();
+  PluginManager.registerCommand(pluginName, command_fadeOutFall, function () {
+    fallImageStatus?.requestFadeOut();
   });
-
   class FallImageStatus {
     /**
      * @param {boolean} startRequested 開始リクエストされているか
@@ -185,17 +183,9 @@
       this._fadeOutRequested = fadeOutRequested;
       this._isFalling = isFalling;
     }
-
-    /**
-     * @return {FallImageStatus}
-     */
     static newInstance() {
       return new FallImageStatus(false, 0, false, false, false);
     }
-
-    /**
-     * @return {object}
-     */
     toSave() {
       return {
         startRequested: this.startRequested,
@@ -205,98 +195,81 @@
         isFalling: this.isFalling,
       };
     }
-
-    /**
-     * @param {object} saveObject セーブデータから取得したオブジェクト
-     * @return {FallImageStatus}
-     */
     static fromSave(saveObject) {
       return new FallImageStatus(
         saveObject.startRequested,
         saveObject.requestedImageId,
         saveObject.stopRequested,
         saveObject.fadeOutRequested,
-        saveObject.isFalling
+        saveObject.isFalling,
       );
     }
-
     get startRequested() {
       return this._startRequested;
     }
-
     get requestedImageId() {
       return this._requestedImageId;
     }
-
     get stopRequested() {
       return this._stopRequested;
     }
-
     get fadeOutRequested() {
       return this._fadeOutRequested;
     }
-
     get isFalling() {
       return this._isFalling;
     }
-
     requestedImageSetting() {
       return settings.images.find((image) => image.id === this._requestedImageId);
     }
-
     requestStart(fallSettingId) {
       this._startRequested = true;
       this._requestedImageId = fallSettingId;
       this._isFalling = true;
     }
-
     requestStop() {
       this._stopRequested = true;
       this._isFalling = false;
     }
-
     requestFadeOut() {
       this._fadeOutRequested = true;
       this._isFalling = false;
     }
-
     clearStartRequest() {
       this._startRequested = false;
     }
-
     clearStopRequest() {
       this._stopRequested = false;
     }
-
     clearFadeOutRequest() {
       this._fadeOutRequested = false;
     }
   }
-
   let fallImageStatus = null;
-
-  const _Game_System_initialize = Game_System.prototype.initialize;
-  Game_System.prototype.initialize = function () {
-    _Game_System_initialize.call(this);
-    fallImageStatus = FallImageStatus.newInstance();
-  };
-
-  const _Game_System_onBeforeSave = Game_System.prototype.onBeforeSave;
-  Game_System.prototype.onBeforeSave = function () {
-    _Game_System_onBeforeSave.call(this);
-    this._fallImageStatus = fallImageStatus.toSave();
-  };
-
-  const _Game_System_onAfterLoad = Game_System.prototype.onAfterLoad;
-  Game_System.prototype.onAfterLoad = function () {
-    _Game_System_onAfterLoad.call(this);
-    if (this._fallImageStatus) {
-      fallImageStatus = FallImageStatus.fromSave(this._fallImageStatus);
-    } else {
+  function Game_System_FallImageMixIn(gameSystem) {
+    const _initialize = gameSystem.initialize;
+    gameSystem.initialize = function () {
+      _initialize.call(this);
       fallImageStatus = FallImageStatus.newInstance();
-    }
-  };
-
+    };
+    const _onBeforeSave = gameSystem.onBeforeSave;
+    gameSystem.onBeforeSave = function () {
+      _onBeforeSave.call(this);
+      if (fallImageStatus) {
+        this._fallImageStatus = fallImageStatus.toSave();
+      }
+    };
+    const _onAfterLoad = gameSystem.onAfterLoad;
+    gameSystem.onAfterLoad = function () {
+      _onAfterLoad.call(this);
+      if (this._fallImageStatus) {
+        fallImageStatus = FallImageStatus.fromSave(this._fallImageStatus);
+      } else {
+        fallImageStatus = FallImageStatus.newInstance();
+      }
+    };
+  }
+  Game_System_FallImageMixIn(Game_System.prototype);
   class Sprite_Falling extends Sprite {
     initialize(fallSettingId) {
       super.initialize();
@@ -306,7 +279,12 @@
       }
       this.bitmap = ImageManager.loadBitmap('', this._fallSetting.file);
     }
-
+    fallSetting() {
+      if (!this._fallSetting) {
+        throw new Error(`設定値が存在しません`);
+      }
+      return this._fallSetting;
+    }
     setup() {
       this.opacity = 255;
       this.x = Math.floor(Math.random() * Graphics.boxWidth);
@@ -314,12 +292,14 @@
       this._lifeTime = this.initialLifeTime();
       const scale = this.calcScale();
       this.scale.set(scale, scale);
-      this._row = Math.randomInt(this._fallSetting.rows);
-      this._animationFrame = Math.randomInt(this._fallSetting.cols * this._fallSetting.animationSpeed);
+      this._row = Math.randomInt(this.fallSetting().rows);
+      this._animationFrame = Math.randomInt(this.fallSetting().cols * this.fallSetting().animationSpeed);
       this.updateFrame();
     }
-
     update() {
+      if (!this._fallSetting) {
+        throw new Error(`設定値が存在しません`);
+      }
       this._lifeTime--;
       this._animationFrame = (this._animationFrame + 1) % (this._fallSetting.cols * this._fallSetting.animationSpeed);
       this.x += this.moveSpeedX();
@@ -327,102 +307,90 @@
       this.updateFrame();
       this.updateLifeTime();
     }
-
     wavering() {
-      return Math.randomInt(10 - this._fallSetting.waveringFrequency + 1) === 0;
+      return Math.randomInt(10 - this.fallSetting().waveringFrequency + 1) === 0;
     }
-
     moveSpeedX() {
-      return (Math.randomInt(this._fallSetting.moveSpeedX) + 1) * (this.wavering() ? -1 : 1);
+      return (Math.randomInt(this.fallSetting().moveSpeedX) + 1) * (this.wavering() ? -1 : 1);
     }
-
     moveSpeedY() {
-      return Math.randomInt(this._fallSetting.moveSpeedY) + 1;
+      return Math.randomInt(this.fallSetting().moveSpeedY) + 1;
     }
-
     updateLifeTime() {
       this._lifeTime--;
       if (this._lifeTime <= 50) {
         this.opacity -= 255 / 50;
       }
-      if (this._lifeTime <= 0 && fallImageStatus.isFalling) {
+      if (this._lifeTime <= 0 && fallImageStatus?.isFalling) {
         this.setup();
       }
     }
-
     updateFrame() {
       const width = this.frameWidth();
       const height = this.frameHeight();
       this.setFrame(
-        Math.floor(this._animationFrame / this._fallSetting.animationSpeed) * width,
+        Math.floor(this._animationFrame / this.fallSetting().animationSpeed) * width,
         this._row * height,
         width,
-        height
+        height,
       );
     }
-
     frameHeight() {
-      return Math.floor(this.bitmap.height / this._fallSetting.rows);
+      return Math.floor((this.bitmap?.height || 0) / this.fallSetting().rows);
     }
-
     frameWidth() {
-      return Math.floor(this.bitmap.width / this._fallSetting.cols);
+      return Math.floor((this.bitmap?.width || 0) / this.fallSetting().cols);
     }
-
     initialLifeTime() {
-      return this._fallSetting.minimumLifeTime + Math.floor(Math.random() * this._fallSetting.lifeTimeRange);
+      return this.fallSetting().minimumLifeTime + Math.floor(Math.random() * this.fallSetting().lifeTimeRange);
     }
-
     calcScale() {
       return Math.randomInt(2) === 0 ? 0.5 : 1.5;
     }
   }
-
-  const _Spriteset_Map_initialize = Spriteset_Map.prototype.initialize;
-  Spriteset_Map.prototype.initialize = function () {
-    this._fallImageSprites = [];
-    _Spriteset_Map_initialize.call(this);
-  };
-
-  const _Spriteset_Map_createLowerLayer = Spriteset_Map.prototype.createLowerLayer;
-  Spriteset_Map.prototype.createLowerLayer = function () {
-    _Spriteset_Map_createLowerLayer.call(this);
-    this.createFallImage();
-  };
-
-  Spriteset_Map.prototype.needCreateFallImage = function () {
-    return fallImageStatus.startRequested || (fallImageStatus.isFalling && this._fallImageSprites.length === 0);
-  };
-
-  Spriteset_Map.prototype.createFallImage = function () {
-    const image = fallImageStatus.requestedImageSetting();
-    if (!image || !this.needCreateFallImage()) {
-      return;
-    }
-    fallImageStatus.clearStartRequest();
-    this._fallImageSprites = [...Array(image.count).keys()].map((_) => new Sprite_Falling(image.id));
-    this._fallImageSprites.forEach((sprite) => {
-      this.addChild(sprite);
-      sprite.setup();
-    });
-  };
-
-  const _Spriteset_Map_update = Spriteset_Map.prototype.update;
-  Spriteset_Map.prototype.update = function () {
-    _Spriteset_Map_update.call(this);
-    this.updateFallImage();
-  };
-
-  Spriteset_Map.prototype.updateFallImage = function () {
-    if (fallImageStatus.startRequested) {
+  function Spriteset_Map_FallImagesMixIn(spritesetMap) {
+    const _initialize = spritesetMap.initialize;
+    spritesetMap.initialize = function () {
+      this._fallImageSprites = [];
+      _initialize.call(this);
+    };
+    const _createLowerLayer = spritesetMap.createLowerLayer;
+    spritesetMap.createLowerLayer = function () {
+      _createLowerLayer.call(this);
       this.createFallImage();
-    } else if (fallImageStatus.stopRequested) {
-      this.destroyFallImages();
-    }
-  };
-
-  Spriteset_Map.prototype.destroyFallImages = function () {
-    this._fallImageSprites.forEach((sprite) => sprite.destroy());
-    fallImageStatus.clearStopRequest();
-  };
+    };
+    spritesetMap.needCreateFallImage = function () {
+      return !!fallImageStatus?.startRequested || (!!fallImageStatus?.isFalling && this._fallImageSprites.length === 0);
+    };
+    spritesetMap.createFallImage = function () {
+      const image = fallImageStatus?.requestedImageSetting();
+      if (!image || !this.needCreateFallImage()) {
+        return;
+      }
+      fallImageStatus?.clearStartRequest();
+      this._fallImageSprites = [...Array(image.count).keys()].map((_) => new Sprite_Falling(image.id));
+      this._fallImageSprites.forEach((sprite) => {
+        this.addChild(sprite);
+        sprite.setup();
+      });
+    };
+    const _update = spritesetMap.update;
+    spritesetMap.update = function () {
+      _update.call(this);
+      this.updateFallImage();
+    };
+    spritesetMap.updateFallImage = function () {
+      if (fallImageStatus?.startRequested) {
+        this.createFallImage();
+      } else if (fallImageStatus?.stopRequested) {
+        this.destroyFallImages();
+      }
+    };
+    spritesetMap.destroyFallImages = function () {
+      this._fallImageSprites.forEach((sprite) => sprite.destroy());
+      this._fallImageSprites = [];
+      fallImageStatus?.clearStopRequest();
+    };
+  }
+  Spriteset_Map_FallImagesMixIn(Spriteset_Map.prototype);
 })();
