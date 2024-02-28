6689ae6401b0396635959a3fbe8ae75b5dc8bd84
elleonard <6374339+elleonard@users.noreply.github.com>
2024-02-28 13:06:29 +0900

    fc0808bc7cbc6a88298c3679c60be35e17d97dfd

--- DarkPlasma_NoseForTreasure.js
+++ DarkPlasma_NoseForTreasure.js
@@ -1,15 +1,16 @@
-// DarkPlasma_NoseForTreasure 1.1.0
+// DarkPlasma_NoseForTreasure 1.1.1
 // Copyright (c) 2021 DarkPlasma
 // This software is released under the MIT license.
 // http://opensource.org/licenses/mit-license.php
 
 /**
+ * 2024/02/28 1.1.1 TypeScript移行
  * 2021/10/23 1.1.0 特定イベントの上にバルーンを表示する機能を追加
  * 2021/10/22 1.0.1 プラグインコマンドの日本語名を追加
  * 2021/10/21 1.0.0 公開
  */
 
-/*:ja
+/*:
  * @plugindesc マップ上にある特定のイベントの数を取得する
  * @author DarkPlasma
  * @license MIT
@@ -71,7 +72,7 @@
  * @default 0
  *
  * @help
- * version: 1.1.0
+ * version: 1.1.1
  * イベントのメモ欄にイベントの種類を表すタグを記入した上で
  * プラグインコマンドを実行すると、
  * 指定した変数にその種類のイベントの数を取得します。
@@ -103,37 +104,46 @@
     return arguments[1];
   });
 
+  function parseArgs_noseForTreasure(args) {
+    return {
+      tag: String(args.tag || ``),
+      variableId: Number(args.variableId || 0),
+      selfSwitches: JSON.parse(args.selfSwitches || '[{"name":"A","state":"false"}]').map((e) => {
+        return ((parameter) => {
+          const parsed = JSON.parse(parameter);
+          return {
+            name: String(parsed.name || `A`),
+            state: String(parsed.state || false) === 'true',
+          };
+        })(e || '{}');
+      }),
+      balloon: Number(args.balloon || 0),
+    };
+  }
+
   const PLUGIN_COMMANDS = {
     NOSE_FOR_TREASURE: 'noseForTreasure',
   };
-
   PluginManager.registerCommand(pluginName, PLUGIN_COMMANDS.NOSE_FOR_TREASURE, function (args) {
-    const tag = String(args.tag);
-    const variableId = Number(args.variableId);
-    const selfSwitches = JSON.parse(args.selfSwitches).map((e) => {
-      const parsed = JSON.parse(e);
-      return {
-        name: String(parsed.name),
-        state: String(parsed.state) === 'true',
-      };
-    });
+    const parsedArgs = parseArgs_noseForTreasure(args);
     const targetEvents = $gameMap
       .events()
       .filter(
         (gameEvent) =>
-          gameEvent.event().meta[tag] &&
-          selfSwitches.every(
-            (selfSwitch) => $gameSelfSwitches.value(gameEvent.selfSwitchKey(selfSwitch.name)) === selfSwitch.state
-          )
+          gameEvent.event().meta[parsedArgs.tag] &&
+          parsedArgs.selfSwitches.every(
+            (selfSwitch) => $gameSelfSwitches.value(gameEvent.selfSwitchKey(selfSwitch.name)) === selfSwitch.state,
+          ),
       );
-    $gameVariables.setValue(variableId, targetEvents.length);
-    const balloon = Number(args.balloon);
-    if (balloon) {
-      targetEvents.forEach((event) => $gameTemp.requestBalloon(event, balloon));
+    $gameVariables.setValue(parsedArgs.variableId, targetEvents.length);
+    if (parsedArgs.balloon) {
+      targetEvents.forEach((event) => $gameTemp.requestBalloon(event, parsedArgs.balloon));
     }
   });
-
-  Game_Event.prototype.selfSwitchKey = function (selfSwitchCh) {
-    return [this._mapId, this._eventId, selfSwitchCh];
-  };
+  function Game_Event_NoseForTreasureMixIn(gameEvent) {
+    gameEvent.selfSwitchKey = function (selfSwitchCh) {
+      return [this._mapId, this._eventId, selfSwitchCh];
+    };
+  }
+  Game_Event_NoseForTreasureMixIn(Game_Event.prototype);
 })();
