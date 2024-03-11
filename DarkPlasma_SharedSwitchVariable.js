4c58604183908c9afab1f9911339cfc153ec8d07
elleonard <6374339+elleonard@users.noreply.github.com>
2024-03-12 02:51:16 +0900

    d0fb30f6af1f8a80fd150b43c8871253f27baa89

--- DarkPlasma_SharedSwitchVariable.js
+++ DarkPlasma_SharedSwitchVariable.js
@@ -1,15 +1,17 @@
-// DarkPlasma_SharedSwitchVariable 1.0.2
+// DarkPlasma_SharedSwitchVariable 1.0.3
 // Copyright (c) 2021 DarkPlasma
 // This software is released under the MIT license.
 // http://opensource.org/licenses/mit-license.php
 
 /**
+ * 2024/03/12 1.0.3 TypeScript移行
+ *                  TemplateEvent.jsがあるとゲームが起動しない不具合を修正
  * 2021/07/05 1.0.2 MZ 1.3.2に対応
  * 2021/06/22 1.0.1 サブフォルダからの読み込みに対応
  * 2021/05/29 1.0.0 公開
  */
 
-/*:ja
+/*:
  * @plugindesc 全てのセーブデータで共有するスイッチ・変数を指定する
  * @author DarkPlasma
  * @license MIT
@@ -34,7 +36,7 @@
  * @desc 共有セーブデータにスイッチ・変数を保存します。
  *
  * @help
- * version: 1.0.2
+ * version: 1.0.3
  * 全てのセーブデータで共有するスイッチ・変数を指定します。
  * 指定したスイッチ・変数の値は共有セーブデータ(save/shared.rmmzsave)に保存します。
  *
@@ -101,97 +103,89 @@
   PluginManager.registerCommand(pluginName, 'saveSharedInfo', function () {
     DataManager.saveSharedInfo();
   });
-
-  const _DataManager_extractSaveContents = DataManager.extractSaveContents;
-  DataManager.extractSaveContents = function (contents) {
-    _DataManager_extractSaveContents.call(this, contents);
-    this.loadSharedInfo();
-  };
-
-  const _DataManager_isMapLoaded = DataManager.isMapLoaded;
-  DataManager.isMapLoaded = function () {
-    /**
-     * 共有データロード完了後にマップ処理を行う
-     */
-    return _DataManager_isMapLoaded.call(this) && this.isSharedInfoLoaded();
-  };
-
-  DataManager.isSharedInfoLoaded = function () {
-    return this._isSharedInfoLoaded;
-  };
-
-  DataManager.loadSharedInfo = function () {
-    this._isSharedInfoLoaded = false;
-    StorageManager.loadObject('shared')
-      .then((sharedInfo) => {
-        const sharedSwitches = sharedInfo.switches || [];
-        sharedSwitches.forEach((sharedSwitch) => {
-          $gameSwitches.setValue(sharedSwitch.id, sharedSwitch.value);
-        });
-        const sharedVariables = sharedInfo.variables || [];
-        sharedVariables.forEach((sharedVariable) => {
-          $gameVariables.setValue(sharedVariable.id, sharedVariable.value);
-        });
-        this._isSharedInfoLoaded = true;
-        return 0;
-      })
-      .catch(() => {
-        /**
-         * セーブデータが存在しなかった場合にもロード完了扱いにする
-         */
-        this._isSharedInfoLoaded = true;
-      });
-  };
-
-  DataManager.makeSharedInfo = function () {
-    return {
-      switches: this.sharedSaveSwitches(),
-      variables: this.sharedSaveVariables(),
+  function DataManager_SharedSwitchVariableMixIn(dataManager) {
+    const _extractSaveContents = dataManager.extractSaveContents;
+    dataManager.extractSaveContents = function (contents) {
+      _extractSaveContents.call(this, contents);
+      this.loadSharedInfo();
     };
-  };
-
-  const _DataManager_saveGame = DataManager.saveGame;
-  DataManager.saveGame = function (savefileId) {
-    this.saveSharedInfo();
-    return _DataManager_saveGame.call(this, savefileId);
-  };
-
-  DataManager.saveSharedInfo = function () {
-    StorageManager.saveObject('shared', this.makeSharedInfo());
-  };
-
-  const _DataManager_setupNewGame = DataManager.setupNewGame;
-  DataManager.setupNewGame = function () {
-    _DataManager_setupNewGame.call(this);
-    this.loadSharedInfo();
-  };
-
-  DataManager.sharedSaveSwitches = function () {
-    return settings.switchRangeList
-      .filter((switchRange) => switchRange.from <= switchRange.to)
-      .map((switchRange) => range(switchRange.to - switchRange.from + 1, switchRange.from))
-      .flat()
-      .map((switchId) => {
-        return {
-          id: switchId,
-          value: $gameSwitches.value(switchId),
-        };
-      });
-  };
-
-  DataManager.sharedSaveVariables = function () {
-    return settings.variableRangeList
-      .filter((variableRange) => variableRange.from <= variableRange.to)
-      .map((variableRange) => range(variableRange.to - variableRange.from + 1, variableRange.from))
-      .flat()
-      .map((variableId) => {
-        return {
-          id: variableId,
-          value: $gameVariables.value(variableId),
-        };
-      });
-  };
-
+    dataManager.isSharedInfoLoaded = function () {
+      return this._isSharedInfoLoaded;
+    };
+    dataManager.loadSharedInfo = function () {
+      this._isSharedInfoLoaded = false;
+      StorageManager.loadObject('shared')
+        .then((sharedInfo) => {
+          const sharedSwitches = sharedInfo.switches || [];
+          sharedSwitches.forEach((sharedSwitch) => {
+            $gameSwitches.setValue(sharedSwitch.id, sharedSwitch.value);
+          });
+          const sharedVariables = sharedInfo.variables || [];
+          sharedVariables.forEach((sharedVariable) => {
+            $gameVariables.setValue(sharedVariable.id, sharedVariable.value);
+          });
+          this._isSharedInfoLoaded = true;
+          return 0;
+        })
+        .catch(() => {
+          /**
+           * セーブデータが存在しなかった場合にもロード完了扱いにする
+           */
+          this._isSharedInfoLoaded = true;
+        });
+    };
+    dataManager.makeSharedInfo = function () {
+      return {
+        switches: this.sharedSaveSwitches(),
+        variables: this.sharedSaveVariables(),
+      };
+    };
+    const _saveGame = dataManager.saveGame;
+    dataManager.saveGame = function (savefileId) {
+      this.saveSharedInfo();
+      return _saveGame.call(this, savefileId);
+    };
+    dataManager.saveSharedInfo = function () {
+      StorageManager.saveObject('shared', this.makeSharedInfo());
+    };
+    const _setupNewGame = dataManager.setupNewGame;
+    dataManager.setupNewGame = function () {
+      _setupNewGame.call(this);
+      this.loadSharedInfo();
+    };
+    dataManager.sharedSaveSwitches = function () {
+      return settings.switchRangeList
+        .filter((switchRange) => switchRange.from <= switchRange.to)
+        .map((switchRange) => range(switchRange.to - switchRange.from + 1, switchRange.from))
+        .flat()
+        .map((switchId) => {
+          return {
+            id: switchId,
+            value: $gameSwitches.value(switchId),
+          };
+        });
+    };
+    dataManager.sharedSaveVariables = function () {
+      return settings.variableRangeList
+        .filter((variableRange) => variableRange.from <= variableRange.to)
+        .map((variableRange) => range(variableRange.to - variableRange.from + 1, variableRange.from))
+        .flat()
+        .map((variableId) => {
+          return {
+            id: variableId,
+            value: $gameVariables.value(variableId),
+          };
+        });
+    };
+  }
+  DataManager_SharedSwitchVariableMixIn(DataManager);
+  function Scene_Map_SharedSwitchVariableMixIn(sceneMap) {
+    const _isReady = sceneMap.isReady;
+    sceneMap.isReady = function () {
+      return _isReady.call(this) && DataManager.isSharedInfoLoaded();
+    };
+  }
+  Scene_Map_SharedSwitchVariableMixIn(Scene_Map.prototype);
   /**
    * 指定した数値から開始する連番の配列を返す
    * @param {number} length 数値
