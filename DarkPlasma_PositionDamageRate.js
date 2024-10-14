ea71f2d722c1200a9478537a5cab21ba307b2bc3
elleonard <6374339+elleonard@users.noreply.github.com>
2024-10-15 06:35:30 +0900

    ea6f85b84ca9d253897dcc3d9bc11ee6fd6d8de0

--- DarkPlasma_PositionDamageRate.js
+++ DarkPlasma_PositionDamageRate.js
@@ -1,16 +1,18 @@
-// DarkPlasma_PositionDamageRate 1.0.3
+// DarkPlasma_PositionDamageRate 1.0.4
 // Copyright (c) 2020 DarkPlasma
 // This software is released under the MIT license.
 // http://opensource.org/licenses/mit-license.php
 
 /**
+ * 2024/10/15 1.0.4 TypeScript移行
+ *                  装備画面で物理ダメージ率、魔法ダメージ率の差分を出そうとする場合に正常な値が表示できない不具合の修正
  * 2021/07/05 1.0.3 MZ 1.3.2に対応
  * 2021/06/22 1.0.2 サブフォルダからの読み込みに対応
  * 2020/10/24 1.0.1 まともに動かない不具合を修正
  * 2020/10/23 1.0.0 公開
  */
 
-/*:ja
+/*:
  * @plugindesc 前衛アクターの立ち位置で被ダメ倍率を変える
  * @author DarkPlasma
  * @license MIT
@@ -31,7 +33,7 @@
  * @default ["100"]
  *
  * @help
- * version: 1.0.3
+ * version: 1.0.4
  * 前衛アクターの立ち位置（先頭から何番目か）で
  * 受けるダメージの倍率を変更します。
  */
@@ -60,34 +62,48 @@
     PHYSICAL_DAMAGE_RATE: 6,
     MAGICAL_DAMAGE_RATE: 7,
   };
-
-  const _Game_Actor_sparam = Game_Actor.prototype.sparam;
-  Game_Actor.prototype.sparam = function (sparamId) {
-    const value = _Game_Actor_sparam.call(this, sparamId);
-    if (sparamId === SPARAM_ID.PHYSICAL_DAMAGE_RATE) {
-      return value * this.physicalDamageRateByPosition();
-    }
-    if (sparamId === SPARAM_ID.MAGICAL_DAMAGE_RATE) {
-      return value * this.magicalDamageRateByPosition();
-    }
-    return value;
-  };
-
-  Game_Actor.prototype.physicalDamageRateByPosition = function () {
-    const index = this.index();
-    return (
-      (settings.physicalDamageRates.length > index
-        ? settings.physicalDamageRates[index]
-        : settings.physicalDamageRates[settings.physicalDamageRates.length - 1]) / 100
-    );
-  };
-
-  Game_Actor.prototype.magicalDamageRateByPosition = function () {
-    const index = this.index();
-    return (
-      (settings.magicalDamageRates.length > index
-        ? settings.magicalDamageRates[index]
-        : settings.magicalDamageRates[settings.magicalDamageRates.length - 1]) / 100
-    );
-  };
+  function Game_Actor_PositionDamageRateMixIn(gameActor) {
+    const _sparam = gameActor.sparam;
+    gameActor.sparam = function (sparamId) {
+      const value = _sparam.call(this, sparamId);
+      if (sparamId === SPARAM_ID.PHYSICAL_DAMAGE_RATE) {
+        return value * this.physicalDamageRateByPosition();
+      }
+      if (sparamId === SPARAM_ID.MAGICAL_DAMAGE_RATE) {
+        return value * this.magicalDamageRateByPosition();
+      }
+      return value;
+    };
+    gameActor.physicalDamageRateByPosition = function () {
+      const index = this.originalIndex();
+      return (
+        (settings.physicalDamageRates.length > index
+          ? settings.physicalDamageRates[index]
+          : settings.physicalDamageRates[settings.physicalDamageRates.length - 1]) / 100
+      );
+    };
+    gameActor.magicalDamageRateByPosition = function () {
+      const index = this.originalIndex();
+      return (
+        (settings.magicalDamageRates.length > index
+          ? settings.magicalDamageRates[index]
+          : settings.magicalDamageRates[settings.magicalDamageRates.length - 1]) / 100
+      );
+    };
+    /**
+     * 装備画面のtempActorから参照しようとすると index だけでは正しい値が取れない
+     */
+    gameActor.originalIndex = function () {
+      const index = this.index();
+      if (index >= 0) {
+        return index;
+      }
+      const originalIndex = $gameParty
+        .members()
+        .find((actor) => actor.actorId() === this.actorId())
+        ?.index();
+      return originalIndex === undefined ? -1 : originalIndex;
+    };
+  }
+  Game_Actor_PositionDamageRateMixIn(Game_Actor.prototype);
 })();
