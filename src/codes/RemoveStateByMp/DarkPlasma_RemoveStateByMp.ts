7249f1cb6dcdba6bd2e1c88709d72f87475b9cbb
plasma <6374339+elleonard@users.noreply.github.com>
2024-11-24 07:34:46 +0900

    1.0.1 スキル使用によるMP消費時にステートが解除されない不具合を修正

--- src/codes/RemoveStateByMp/DarkPlasma_RemoveStateByMp.ts
+++ src/codes/RemoveStateByMp/DarkPlasma_RemoveStateByMp.ts
@@ -7,9 +7,18 @@ function Game_Battler_RemoveStateByMpMixIn(gameBattler: Game_Battler) {
       .forEach(state => this.removeState(state.id));
   };
 
-  const _gainMp = gameBattler.gainMp;
-  gameBattler.gainMp = function (value) {
-    _gainMp.call(this, value);
+  /**
+   * gainMp, setMp経由の場合はrefreshを呼ぶ
+   */
+  const _refresh = gameBattler.refresh;
+  gameBattler.refresh = function () {
+    _refresh.call(this);
+    this.removeStatesByMp();
+  };
+
+  const _paySkillCost = gameBattler.paySkillCost;
+  gameBattler.paySkillCost = function (skill) {
+    _paySkillCost.call(this, skill);
     this.removeStatesByMp();
   };
 }
