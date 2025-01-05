5507b0de06197760339f89826f02667f65a3cd23
elleonard <6374339+elleonard@users.noreply.github.com>
2025-01-05 23:29:03 +0900

    0979b1b52ab516a8eed6258ebf699cf2adbc9c9d

--- DarkPlasma_ElementDamageBonusTrait.js
+++ DarkPlasma_ElementDamageBonusTrait.js
@@ -1,9 +1,10 @@
-// DarkPlasma_ElementDamageBonusTrait 1.0.1
+// DarkPlasma_ElementDamageBonusTrait 1.1.0
 // Copyright (c) 2025 DarkPlasma
 // This software is released under the MIT license.
 // http://opensource.org/licenses/mit-license.php
 
 /**
+ * 2025/01/05 1.1.0 異なる属性のダメージ倍率を加算するか乗算するかの設定を追加
  * 2025/01/05 1.0.1 単一属性の行動の属性を正常に反映できない不具合を修正
  * 2025/01/05 1.0.0 公開
  */
@@ -19,24 +20,38 @@
  * @base DarkPlasma_AllocateUniqueTraitId
  * @orderAfter DarkPlasma_MultiElementAction
  *
+ * @param addition
+ * @desc ONにすると、異なる属性のダメージ倍率同士を加算します。OFFの場合は乗算します。
+ * @text 加算するか
+ * @type boolean
+ * @default true
+ *
  * @help
- * version: 1.0.1
+ * version: 1.1.0
  * アクター、職業、装備、敵キャラ、ステートのメモ欄に
  * elementDamageBonusメモタグを設定することで
  * 指定した属性のダメージ倍率を増減する特徴を設定できます。
  *
  * 設定例:
- * 火属性を含む行動のダメージ倍率+50％
+ * 火属性の与える属性ダメージ倍率+50％
  * <elementDamageBonus:
  *   火:50
  * >
  *
- * 氷属性を含む行動のダメージ倍率+20％
- * 雷属性を含む行動のダメージ倍率+30％
+ * 氷属性の与える属性ダメージ倍率+20％
+ * 雷属性の与える属性ダメージ倍率+30％
  * <elementDamangeBonus:
  *   氷:20
  *   雷:30
  * >
+ *
+ * その属性の特徴の値をすべて加算した値を元に、与える属性ダメージ倍率を決定します。
+ * 与える属性ダメージ倍率を敵の有効度に乗算して実際の属性ダメージを計算します。
+ * 与える属性ダメージ倍率のデフォルト値は100％です。
+ *
+ * DarkPlasma_MultiElementActionで実現される複合属性については
+ * すべての属性について与える属性ダメージ倍率を計算し、それぞれを
+ * プラグインの設定に従って加算または乗算します。
  *
  *
  * 本プラグインの利用には下記プラグインを必要とします。
@@ -55,6 +70,14 @@
   function hasTraits(data) {
     return 'traits' in data;
   }
+
+  const pluginParametersOf = (pluginName) => PluginManager.parameters(pluginName);
+
+  const pluginParameters = pluginParametersOf(pluginName);
+
+  const settings = {
+    addition: String(pluginParameters.addition || true) === 'true',
+  };
 
   const elementDamageBonusTrait = uniqueTraitIdCache.allocate(pluginName, 1, '属性ダメージボーナス');
   function DataManager_ElementDamageBonusTraitMixIn(dataManager) {
@@ -106,19 +129,25 @@
     };
   }
   Scene_Boot_ElementDamageBonusTraitMixIn(Scene_Boot.prototype);
+  function Game_BattlerBase_ElementDamageBonusTraitMixIn(gameBattlerBase) {
+    gameBattlerBase.elementAttackDamageBonus = function (elementId) {
+      return this.traitsSum(elementDamageBonusTrait.id, elementId);
+    };
+    gameBattlerBase.elementAttackDamageRate = function (elementId) {
+      return 1 + this.elementAttackDamageBonus(elementId) / 100;
+    };
+  }
+  Game_BattlerBase_ElementDamageBonusTraitMixIn(Game_BattlerBase.prototype);
   function Game_Action_ElementDamageBonusTraitMixIn(gameAction) {
     const _calcElementRate = gameAction.calcElementRate;
     gameAction.calcElementRate = function (target) {
       const result = _calcElementRate.call(this, target);
-      return result + this.elementDamageBonus(this.actionAttackElements());
+      return result * this.elementDamageBonus(this.actionAttackElements());
     };
     gameAction.elementDamageBonus = function (elements) {
-      return (
-        elements.reduce(
-          (result, elementId) => this.subject().traitsSum(elementDamageBonusTrait.id, elementId) + result,
-          0,
-        ) / 100
-      );
+      return settings.addition
+        ? elements.reduce((result, elementId) => this.subject().elementAttackDamageBonus(elementId) + result, 100) / 100
+        : elements.reduce((result, elementId) => this.subject().elementAttackDamageRate(elementId) * result, 1);
     };
     if (!gameAction.actionAttackElements) {
       gameAction.actionAttackElements = function () {
