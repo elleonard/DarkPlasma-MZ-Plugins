8261b9616c2fa2b6bd9215c5e1e13b3dca970a96
elleonard <6374339+elleonard@users.noreply.github.com>
2025-03-22 20:57:44 +0900

    c0976fd024e794a18d51fa2e0316f364919bff9c

--- DarkPlasma_ChoiceExtension.js
+++ DarkPlasma_ChoiceExtension.js
@@ -1,9 +1,10 @@
-// DarkPlasma_ChoiceExtension 1.2.0
+// DarkPlasma_ChoiceExtension 1.2.1
 // Copyright (c) 2020 DarkPlasma
 // This software is released under the MIT license.
 // http://opensource.org/licenses/mit-license.php
 
 /**
+ * 2025/03/22 1.2.1 キャンセル選択肢の設定が分岐・禁止でなく非表示の選択肢がある場合にキャンセルすると、正常に選択結果を実行しない不具合を修正
  * 2024/10/28 1.2.0 セーブデータに含むクラス名の命名を見直し
  *                  typescript移行
  * 2021/07/05 1.1.3 MZ 1.3.2に対応
@@ -27,12 +28,8 @@
  * @default 6
  *
  * @help
- * version: 1.2.0
+ * version: 1.2.1
  * 選択肢を拡張します。
- *
- * 注意:
- *   本プラグインはMPP_ChoiceEXの機能を一部MZ対応していますが、
- *   積極的に全機能を移植するつもりはありません。
  *
  * 選択肢の文章中に
  * if(条件式)
@@ -53,6 +50,8 @@
  * それらに設定された選択肢をまとめて表示します。
  *
  * デフォルトやキャンセル時の挙動は最初の選択肢コマンドのものが適用されます。
+ * キャンセルの選択肢が非表示になっている場合にキャンセルすると
+ * 何もせずに選択肢の処理を終了します。
  *
  * プログラムインターフェース:
  * $gameMessage.originalChoices() : object[]
@@ -118,225 +117,246 @@
       return this._branchIndex;
     }
   }
-  const _Game_Message_clear = Game_Message.prototype.clear;
-  Game_Message.prototype.clear = function () {
-    _Game_Message_clear.call(this);
-    this._choiceEnabled = [];
-    this._originalChoices = [];
-  };
-  Game_Message.prototype.setChoiceEnabled = function (choiceEnabled) {
-    this._choiceEnabled = choiceEnabled;
-  };
-  Game_Message.prototype.choiceEnabled = function () {
-    return this._choiceEnabled;
-  };
-  Game_Message.prototype.setOriginalChoices = function (originalChoices) {
-    this._originalChoices = originalChoices;
-  };
-  /**
-   * 元々の選択肢設定を、下記形式のオブジェクトの配列として返す
-   * {
-   *   displayName: string,
-   *   displayed: boolean,
-   *   enabled: boolean
-   * }
-   * @return {object[]}
-   */
-  Game_Message.prototype.originalChoices = function () {
-    return this._originalChoices;
-  };
-  /**
-   * 元々の選択肢設定の中で、表示すべきものの元々のインデックス一覧を返す
-   */
-  Game_Message.prototype.originalIndexOfDiplayedChoices = function () {
-    const result = [];
-    this.originalChoices().forEach((choice, index) => {
-      if (choice.displayed) {
-        result.push(index);
-      }
-    });
-    return result;
-  };
-  const _Game_Interpreter_setup = Game_Interpreter.prototype.setup;
-  Game_Interpreter.prototype.setup = function (list, eventId) {
-    _Game_Interpreter_setup.call(this, list, eventId);
-    this._mergedChoices = [];
-    this._choiceBranches = [];
-  };
-  Game_Interpreter.prototype.currentCommandIsMergedChoice = function () {
-    const mergedChoice = this._mergedChoices.find((mergedChoice) => mergedChoice.index === this._index);
-    return mergedChoice && mergedChoice.merged();
-  };
-  Game_Interpreter.prototype.setupChoices = function (params) {
-    if (this.currentCommandIsMergedChoice()) {
-      return;
-    }
-    const choices = this.mergeSequencialChoices();
-    const displayedChoices = choices.filter((choice) => choice.displayed);
-    const cancelType = params[1] < choices.length ? params[1] : -2;
-    const defaultType = params.length > 2 ? params[2] || 0 : 0;
-    const positionType = params.length > 3 ? params[3] : 2;
-    const background = params.length > 4 ? params[4] || 0 : 0;
-    $gameMessage.setChoices(
-      displayedChoices.map((choice) => choice.displayName),
-      defaultType,
-      cancelType,
-    );
-    $gameMessage.setChoiceEnabled(displayedChoices.map((choice) => choice.enabled));
-    $gameMessage.setChoiceBackground(background);
-    $gameMessage.setChoicePositionType(positionType === undefined ? 2 : positionType);
-    $gameMessage.setChoiceCallback((n) => {
-      this._branch[this._indent] = n;
-    });
-    $gameMessage.setOriginalChoices(choices);
-  };
-  /**
-   * 連続した選択肢の内容をマージする
-   */
-  Game_Interpreter.prototype.mergeSequencialChoices = function () {
-    let choices = [];
-    let branchIndex = 0;
-    for (
-      let commandIndex = this._index, sequencialChoiceIndex = 0;
-      commandIndex !== -1;
-      commandIndex = this.followingChoiceCommandIndex(commandIndex), sequencialChoiceIndex++
-    ) {
-      this._mergedChoices.push(new Game_MergedSequencialChoicesIndex(commandIndex, sequencialChoiceIndex));
-      this._list[commandIndex].parameters[0].forEach((choice, index) => {
-        let choiceDisplayName = choice.replace(/\s*if\((.*?)\)/, '').replace(/\s*en\((.*?)\)/, '');
-        let isDisplayed = /\s*if\((.*?)\)/.test(choice) ? this.evalChoiceIf(choice) : true;
-        let isEnabled = /s*en\((.*?)\)/.test(choice) ? this.evalChoiceEnabled(choice) : true;
-        if (isDisplayed) {
-          this._choiceBranches.push(
-            new Game_ChoiceBranchIndex(this.findChoiceBranchCommandIndex(commandIndex, index), branchIndex),
-          );
-          branchIndex++;
+  function Game_Message_ChoiceExtensionMixIn(gameMessage) {
+    const _clear = gameMessage.clear;
+    gameMessage.clear = function () {
+      _clear.call(this);
+      this._choiceEnabled = [];
+      this._originalChoices = [];
+    };
+    gameMessage.setChoiceEnabled = function (choiceEnabled) {
+      this._choiceEnabled = choiceEnabled;
+    };
+    gameMessage.choiceEnabled = function () {
+      return this._choiceEnabled;
+    };
+    gameMessage.setOriginalChoices = function (originalChoices) {
+      this._originalChoices = originalChoices;
+    };
+    /**
+     * 元々の選択肢設定を、下記形式のオブジェクトの配列として返す
+     * {
+     *   displayName: string,
+     *   displayed: boolean,
+     *   enabled: boolean
+     * }
+     * @return {object[]}
+     */
+    gameMessage.originalChoices = function () {
+      return this._originalChoices;
+    };
+    /**
+     * 元々の選択肢設定の中で、表示すべきものの元々のインデックス一覧を返す
+     */
+    gameMessage.originalIndexOfDiplayedChoices = function () {
+      const result = [];
+      this.originalChoices().forEach((choice, index) => {
+        if (choice.displayed) {
+          result.push(index);
         }
-        choices.push({
-          displayName: choiceDisplayName,
-          displayed: isDisplayed,
-          enabled: isEnabled,
-        });
       });
-    }
-    return choices;
-  };
-  /**
-   * 選択肢が有効であるかどうか返す
-   * @param {string} choice 選択肢テキスト
-   * @return {boolean}
-   */
-  Game_Interpreter.prototype.evalChoiceEnabled = function (choice) {
-    const match = /\s*en\((.*?)\)/.exec(choice);
-    if (match) {
-      return this.evalChoiceCondition(match[1]);
-    }
-    return true;
-  };
-  /**
-   * 選択肢を表示すべきかどうか返す
-   * @param {string} choice 選択肢テキスト
-   * @return {boolean}
-   */
-  Game_Interpreter.prototype.evalChoiceIf = function (choice) {
-    const match = /\s*if\((.*?)\)/.exec(choice);
-    if (match) {
-      return this.evalChoiceCondition(match[1]);
-    }
-    return true;
-  };
-  Game_Interpreter.prototype.evalChoiceCondition = function (condition) {
-    try {
-      const parsedCondition = condition
-        .replace(/s\[(\d+)\]/g, (_, index) => String($gameSwitches.value(index)))
-        .replace(/v\[(\d+)\]/g, (_, index) => String($gameVariables.value(index)));
-      return !!eval(parsedCondition);
-    } catch (e) {
-      return false;
-    }
-  };
-  /**
-   * 検索元インデックス（選択肢開始コマンドを想定）と分岐インデックスから、
-   * 対象の分岐コマンド位置を返す
-   * @param {number} fromIndex 検索元インデックス
-   * @param {number} branchIndex 分岐インデックス
-   * @return {number}
-   */
-  Game_Interpreter.prototype.findChoiceBranchCommandIndex = function (fromIndex, branchIndex) {
-    const fromCommand = this._list[fromIndex];
-    return (
-      this._list
-        .slice(fromIndex)
-        .findIndex(
-          (command) =>
-            command.code === EVENT_COMMAND.CHOICE_BRANCH &&
-            command.indent === fromCommand.indent &&
-            command.parameters[0] === branchIndex,
-        ) + fromIndex
-    );
-  };
-  /**
-   * 指定したインデックスから見て次の選択肢終了コマンド位置を返す
-   * @param {number} fromIndex 検索元インデックス
-   * @return {number}
-   */
-  Game_Interpreter.prototype.findChoiceEndIndex = function (fromIndex) {
-    const fromCommand = this._list[fromIndex];
-    return (
-      this._list
-        .slice(fromIndex)
-        .findIndex((command) => fromCommand.indent === command.indent && command.code === EVENT_COMMAND.CHOICE_END) +
-      fromIndex
-    );
-  };
-  /**
-   * 指定したインデックスの選択肢分岐コマンドの親となる選択肢開始コマンド位置を返す
-   * @param {number} fromIndex 検索元インデックス
-   * @return {number}
-   */
-  Game_Interpreter.prototype.findChoiceStartIndex = function (fromIndex) {
-    const fromCommand = this._list[fromIndex];
-    return (
-      fromIndex -
-      this._list
-        .slice(0, fromIndex + 1)
-        .reverse()
-        .findIndex((command) => fromCommand.indent === command.indent && command.code === EVENT_COMMAND.CHOICE_START)
-    );
-  };
-  /**
-   * 指定したインデックスから連続した選択肢コマンド位置を返す
-   * 連続していなければ-1を返す
-   * @param {number} fromIndex 検索元インデックス
-   * @return {number}
-   */
-  Game_Interpreter.prototype.followingChoiceCommandIndex = function (fromIndex) {
-    const choiceEndCommandIndex = this.findChoiceEndIndex(fromIndex);
-    const targetCommand = this._list[choiceEndCommandIndex + 1];
-    return targetCommand.code === EVENT_COMMAND.CHOICE_START ? choiceEndCommandIndex + 1 : -1;
-  };
-  Game_Interpreter.prototype.command402 = function (params) {
-    const choiceBranchIndex = this._choiceBranches.find((choiceBranch) => choiceBranch.commandIndex === this._index);
-    if (!choiceBranchIndex || this._branch[this._indent] !== choiceBranchIndex.branchIndex) {
-      this.skipBranch();
-    }
-    return true;
-  };
-  const _Window_ChoiceList_drawItem = Window_ChoiceList.prototype.drawItem;
-  Window_ChoiceList.prototype.drawItem = function (index) {
-    this.changePaintOpacity(this.isCommandEnabled(index));
-    _Window_ChoiceList_drawItem.call(this, index);
-  };
-  Window_ChoiceList.prototype.makeCommandList = function () {
-    const choices = $gameMessage.choices();
-    const choiceEnabled = $gameMessage.choiceEnabled();
-    choices.forEach((choice, index) => {
-      this.addCommand(choice, 'choice', choiceEnabled[index]);
-    });
-  };
-  Window_ChoiceList.prototype.maxLines = function () {
-    return settings.maxPageRows;
-  };
+      return result;
+    };
+  }
+  Game_Message_ChoiceExtensionMixIn(Game_Message.prototype);
+  function Game_Interpreter_ChoiceExtensionMixIn(gameInterpreter) {
+    const _setup = gameInterpreter.setup;
+    gameInterpreter.setup = function (list, eventId) {
+      _setup.call(this, list, eventId);
+      this._mergedChoices = [];
+      this._choiceBranches = [];
+    };
+    gameInterpreter.currentCommandIsMergedChoice = function () {
+      const mergedChoice = this._mergedChoices.find((mergedChoice) => mergedChoice.index === this._index);
+      return !!mergedChoice && mergedChoice.merged();
+    };
+    gameInterpreter.setupChoices = function (params) {
+      if (this.currentCommandIsMergedChoice()) {
+        return;
+      }
+      const choices = this.mergeSequencialChoices();
+      const displayedChoices = choices.filter((choice) => choice.displayed);
+      const cancelType =
+        params[1] < choices.length
+          ? ((originalCancelIndex) => {
+              /**
+               * キャンセル時の選択肢が表示されているなら、そこに合わせる
+               */
+              const originalCancelChoice = choices[originalCancelIndex];
+              if (originalCancelChoice.displayed) {
+                return displayedChoices.indexOf(originalCancelChoice);
+              }
+              /**
+               * 表示されていない場合は、何もしない (全選択結果をスキップ)
+               */
+              return -2;
+            })(params[1])
+          : -2;
+      const defaultType = params.length > 2 ? params[2] || 0 : 0;
+      const positionType = params.length > 3 ? params[3] : 2;
+      const background = params.length > 4 ? params[4] || 0 : 0;
+      $gameMessage.setChoices(
+        displayedChoices.map((choice) => choice.displayName),
+        defaultType,
+        cancelType,
+      );
+      $gameMessage.setChoiceEnabled(displayedChoices.map((choice) => choice.enabled));
+      $gameMessage.setChoiceBackground(background);
+      $gameMessage.setChoicePositionType(positionType === undefined ? 2 : positionType);
+      $gameMessage.setChoiceCallback((n) => {
+        this._branch[this._indent] = n;
+      });
+      $gameMessage.setOriginalChoices(choices);
+    };
+    /**
+     * 連続した選択肢の内容をマージする
+     */
+    gameInterpreter.mergeSequencialChoices = function () {
+      let choices = [];
+      let branchIndex = 0;
+      for (
+        let commandIndex = this._index, sequencialChoiceIndex = 0;
+        commandIndex !== -1;
+        commandIndex = this.followingChoiceCommandIndex(commandIndex), sequencialChoiceIndex++
+      ) {
+        this._mergedChoices.push(new Game_MergedSequencialChoicesIndex(commandIndex, sequencialChoiceIndex));
+        this._list[commandIndex].parameters[0].forEach((choice, index) => {
+          let choiceDisplayName = choice.replace(/\s*if\((.*?)\)/, '').replace(/\s*en\((.*?)\)/, '');
+          let isDisplayed = /\s*if\((.*?)\)/.test(choice) ? this.evalChoiceIf(choice) : true;
+          let isEnabled = /s*en\((.*?)\)/.test(choice) ? this.evalChoiceEnabled(choice) : true;
+          if (isDisplayed) {
+            this._choiceBranches.push(
+              new Game_ChoiceBranchIndex(this.findChoiceBranchCommandIndex(commandIndex, index), branchIndex),
+            );
+            branchIndex++;
+          }
+          choices.push({
+            displayName: choiceDisplayName,
+            displayed: isDisplayed,
+            enabled: isEnabled,
+          });
+        });
+      }
+      return choices;
+    };
+    /**
+     * 選択肢が有効であるかどうか返す
+     * @param {string} choice 選択肢テキスト
+     * @return {boolean}
+     */
+    gameInterpreter.evalChoiceEnabled = function (choice) {
+      const match = /\s*en\((.*?)\)/.exec(choice);
+      if (match) {
+        return this.evalChoiceCondition(match[1]);
+      }
+      return true;
+    };
+    /**
+     * 選択肢を表示すべきかどうか返す
+     * @param {string} choice 選択肢テキスト
+     * @return {boolean}
+     */
+    gameInterpreter.evalChoiceIf = function (choice) {
+      const match = /\s*if\((.*?)\)/.exec(choice);
+      if (match) {
+        return this.evalChoiceCondition(match[1]);
+      }
+      return true;
+    };
+    gameInterpreter.evalChoiceCondition = function (condition) {
+      try {
+        const parsedCondition = condition
+          .replace(/s\[(\d+)\]/g, (_, index) => String($gameSwitches.value(index)))
+          .replace(/v\[(\d+)\]/g, (_, index) => String($gameVariables.value(index)));
+        return !!eval(parsedCondition);
+      } catch (e) {
+        return false;
+      }
+    };
+    /**
+     * 検索元インデックス（選択肢開始コマンドを想定）と分岐インデックスから、
+     * 対象の分岐コマンド位置を返す
+     */
+    gameInterpreter.findChoiceBranchCommandIndex = function (fromIndex, branchIndex) {
+      const fromCommand = this._list[fromIndex];
+      return (
+        this._list
+          .slice(fromIndex)
+          .findIndex(
+            (command) =>
+              command.code === EVENT_COMMAND.CHOICE_BRANCH &&
+              command.indent === fromCommand.indent &&
+              command.parameters[0] === branchIndex,
+          ) + fromIndex
+      );
+    };
+    /**
+     * 指定したインデックスから見て次の選択肢終了コマンド位置を返す
+     * @param {number} fromIndex 検索元インデックス
+     * @return {number}
+     */
+    gameInterpreter.findChoiceEndIndex = function (fromIndex) {
+      const fromCommand = this._list[fromIndex];
+      return (
+        this._list
+          .slice(fromIndex)
+          .findIndex((command) => fromCommand.indent === command.indent && command.code === EVENT_COMMAND.CHOICE_END) +
+        fromIndex
+      );
+    };
+    /**
+     * 指定したインデックスの選択肢分岐コマンドの親となる選択肢開始コマンド位置を返す
+     * @param {number} fromIndex 検索元インデックス
+     * @return {number}
+     */
+    gameInterpreter.findChoiceStartIndex = function (fromIndex) {
+      const fromCommand = this._list[fromIndex];
+      return (
+        fromIndex -
+        this._list
+          .slice(0, fromIndex + 1)
+          .reverse()
+          .findIndex((command) => fromCommand.indent === command.indent && command.code === EVENT_COMMAND.CHOICE_START)
+      );
+    };
+    /**
+     * 指定したインデックスから連続した選択肢コマンド位置を返す
+     * 連続していなければ-1を返す
+     * @param {number} fromIndex 検索元インデックス
+     * @return {number}
+     */
+    gameInterpreter.followingChoiceCommandIndex = function (fromIndex) {
+      const choiceEndCommandIndex = this.findChoiceEndIndex(fromIndex);
+      const targetCommand = this._list[choiceEndCommandIndex + 1];
+      return targetCommand.code === EVENT_COMMAND.CHOICE_START ? choiceEndCommandIndex + 1 : -1;
+    };
+    gameInterpreter.command402 = function (params) {
+      const choiceBranchIndex = this._choiceBranches.find((choiceBranch) => choiceBranch.commandIndex === this._index);
+      if (!choiceBranchIndex || this._branch[this._indent] !== choiceBranchIndex.branchIndex) {
+        this.skipBranch();
+      }
+      return true;
+    };
+  }
+  Game_Interpreter_ChoiceExtensionMixIn(Game_Interpreter.prototype);
+  function Window_ChoiceList_ChoiceExtensionMixin(windowChoiceList) {
+    const _drawItem = windowChoiceList.drawItem;
+    windowChoiceList.drawItem = function (index) {
+      this.changePaintOpacity(this.isCommandEnabled(index));
+      _drawItem.call(this, index);
+    };
+    windowChoiceList.makeCommandList = function () {
+      const choices = $gameMessage.choices();
+      const choiceEnabled = $gameMessage.choiceEnabled();
+      choices.forEach((choice, index) => {
+        this.addCommand(choice, 'choice', choiceEnabled[index]);
+      });
+    };
+    windowChoiceList.maxLines = function () {
+      return settings.maxPageRows;
+    };
+  }
+  Window_ChoiceList_ChoiceExtensionMixin(Window_ChoiceList.prototype);
   globalThis.ChoiceBranchIndex = Game_ChoiceBranchIndex;
   globalThis.MergedSequencialChoicesIndex = Game_MergedSequencialChoicesIndex;
   globalThis.Game_ChoiceBranchIndex = Game_ChoiceBranchIndex;
