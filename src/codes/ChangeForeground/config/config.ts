4336d0c343177296e646c50c35f8cfd16dd6b34b
plasma <6374339+elleonard@users.noreply.github.com>
2025-09-15 14:14:25 +0900

    1.0.1 英語翻訳を追加 (Add English translation)

--- src/codes/ChangeForeground/config/config.ts
+++ src/codes/ChangeForeground/config/config.ts
@@ -5,6 +5,11 @@ import { dedent } from '@qnighy/dedent';
 
 const histories: PluginHistorySchema[] = [
   {
+    date: "2025/09/15",
+    version: "1.0.1",
+    description: "英語翻訳を追加 (Add English translation)",
+  },
+  {
     date: "2025/08/12",
     version: "1.0.0",
     description: "最初のバージョン",
@@ -12,43 +17,83 @@ const histories: PluginHistorySchema[] = [
 ];
 
 const commandChangeForeground = createCommand("ChangeForeground", {
-  text: "近景画像を切り替える",
-  description: "近景画像を切り替えます。",
+  text: {
+    ja: "近景画像を切り替える",
+    en: "Change foreground",
+  },
+  description: {
+    ja: "近景画像を切り替えます。",
+    en: "Change the foreground image.",
+  },
   args: [
     createFileParam("file", {
-      text: "ファイル",
-      description: "視差ゼロの画像を指定する場合はファイル名を文字列で直接入力してください。",
+      text: {
+        ja: "ファイル",
+        en: "file",
+      },
+      description: {
+        ja: "視差ゼロの画像を指定する場合はファイル名を文字列で直接入力してください。",
+        en: "If the name starts '!', you must enter the file name as string.",
+      },
       dir: "img/paralaxes",
     }),
     createBooleanParam("loopX", {
-      text: "X方向ループ",
+      text: {
+        ja: "X方向ループ",
+        en: "loop X",
+      },
       default: false,
     }),
     createBooleanParam("loopY", {
-      text: "Y方向ループ",
+      text: {
+        ja: "Y方向ループ",
+        en: "loop Y",
+      },
       default: false,
     }),
     createNumberParam("scrollSpeedX", {
-      text: "X方向スクロール速度",
-      description: "X軸方向のスクロール速度を設定します。",
+      text: {
+        ja: "X方向スクロール速度",
+        en: "X scroll speed",
+      },
+      description: {
+        ja: "X軸方向のスクロール速度を設定します。",
+        en: "If loop X is false, this value is ignored.",
+      },
     }),
     createNumberParam("scrollSpeedY", {
-      text: "Y方向スクロール速度",
-      description: "Y軸方向のスクロール速度を設定します。",
+      text: {
+        ja: "Y方向スクロール速度",
+        en: "Y scroll speed",
+      },
+      description: {
+        ja: "Y軸方向のスクロール速度を設定します。",
+        en: "If loop Y is false, this value is ignored.",
+      },
     }),
   ],
 });
 
 const commandClearForeground = createCommand("ClearForeground", {
-  text: "近景画像を消去する",
-  description: "設定された近景画像を消去します。",
+  text: {
+    ja: "近景画像を消去する",
+    en: "Clear foreground",
+  },
+  description: {
+    ja: "設定された近景画像を消去します。",
+    en: "Clear foreground.",
+  },
 });
 
 export const config = new ConfigDefinitionBuilder(
   "ChangeForeground",
   2025,
-  "近景画像を切り替える"
+  {
+    ja: "近景画像を切り替える",
+    en: "Change the foreground image.",
+  },
 )
+  .withLocate("en")
   .withHistories(histories)
   .withLicense("MIT")
   .withCommands([
@@ -59,6 +104,10 @@ export const config = new ConfigDefinitionBuilder(
     name: "Foreground",
     version: "1.1.0",
   })
-  .withHelp(dedent`準公式プラグインForeground.jsで設定された近景画像を
-    プラグインコマンドで切り替えます。`)
+  .withHelp({
+    ja: dedent`準公式プラグインForeground.jsで設定された近景画像を
+    プラグインコマンドで切り替えます。`,
+    en: dedent `This plugin provide the plugin command
+    that change the foreground image set by Foreground.js.`,
+  })
   .build();
