b595796d2339a5363449261bf3f68b674a2ea6b1
elleonard <6374339+elleonard@users.noreply.github.com>
2025-09-15 11:49:17 +0900

    03d441f93189960f6c8a8422e5d5da9b4299fd5b

--- DarkPlasma_CriticalDamageRateTrait.js
+++ DarkPlasma_CriticalDamageRateTrait.js
@@ -1,9 +1,10 @@
-// DarkPlasma_CriticalDamageRateTrait 1.0.0
+// DarkPlasma_CriticalDamageRateTrait 1.1.0
 // Copyright (c) 2024 DarkPlasma
 // This software is released under the MIT license.
 // http://opensource.org/licenses/mit-license.php
 
 /**
+ * 2025/09/15 1.1.0 装備絞り込み用の特徴名設定を追加
  * 2024/02/20 1.0.0 公開
  */
 
@@ -17,6 +18,7 @@
  *
  * @base DarkPlasma_AllocateUniqueTraitId
  * @orderAfter DarkPlasma_AllocateUniqueTraitId
+ * @orderAfter DarkPlasma_FilterEquip
  *
  * @param defaultCriticalDamageRate
  * @desc 会心ダメージ率の初期値を設定します。
@@ -24,8 +26,14 @@
  * @type number
  * @default 300
  *
+ * @param namesForFilter
+ * @desc 装備絞り込みの際に使用する特徴名を設定します。
+ * @text フィルタ用特徴名
+ * @type struct<NamesForFilter>
+ * @default {"traitName":"追加能力値","effectName":"会心ダメージ率"}
+ *
  * @help
- * version: 1.0.0
+ * version: 1.1.0
  * 会心ダメージ率の特徴を設定できます。
  *
  * アクター、職業、スキル、武器、防具、敵キャラ、ステートのメモ欄に
@@ -37,8 +45,19 @@
  * DarkPlasma_AllocateUniqueTraitId version:1.0.1
  * 下記プラグインと共に利用する場合、それよりも下に追加してください。
  * DarkPlasma_AllocateUniqueTraitId
+ * DarkPlasma_FilterEquip
  */
-
+/*~struct~NamesForFilter:
+ * @param traitName
+ * @text 特徴名
+ * @type string
+ * @default 追加能力値
+ *
+ * @param effectName
+ * @text 効果名
+ * @type string
+ * @default 会心ダメージ率
+ */
 (() => {
   'use strict';
 
@@ -56,9 +75,18 @@
 
   const settings = {
     defaultCriticalDamageRate: Number(pluginParameters.defaultCriticalDamageRate || 300),
+    namesForFilter: pluginParameters.namesForFilter
+      ? ((parameter) => {
+          const parsed = JSON.parse(parameter);
+          return {
+            traitName: String(parsed.traitName || `追加能力値`),
+            effectName: String(parsed.effectName || `会心ダメージ率`),
+          };
+        })(pluginParameters.namesForFilter)
+      : { traitName: '追加能力値', effectName: '会心ダメージ率' },
   };
 
-  const criticalDamageRateTrait = uniqueTraitIdCache.allocate(pluginName, 0, '会心ダメージ率');
+  const criticalDamageRateTrait = uniqueTraitIdCache.allocate(pluginName, 0, settings.namesForFilter.traitName);
   function DataManager_CriticalDamageRateTraitMixIn(dataManager) {
     const _extractMetadata = dataManager.extractMetadata;
     dataManager.extractMetadata = function (data) {
@@ -85,4 +113,15 @@
     };
   }
   Game_Action_CriticalDamageRateTraitMixIn(Game_Action.prototype);
+  function Scene_Equip_AllocateUniqueTraitDataIdMixIn(sceneEquip) {
+    if ('equipFilterBuilder' in sceneEquip && settings.namesForFilter.effectName) {
+      const _equipFilterBuilder = sceneEquip.equipFilterBuilder;
+      sceneEquip.equipFilterBuilder = function (equips) {
+        return _equipFilterBuilder.call(this, equips).withTraitToEffectNameRule((traitId, dataId) => {
+          return traitId === criticalDamageRateTrait.id ? settings.namesForFilter.effectName : null;
+        });
+      };
+    }
+  }
+  Scene_Equip_AllocateUniqueTraitDataIdMixIn(Scene_Equip.prototype);
 })();
