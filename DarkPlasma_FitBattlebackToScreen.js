ae6ec21217ebe6fd0dea3a42214c17568f12f864
elleonard <6374339+elleonard@users.noreply.github.com>
2026-04-02 07:40:02 +0900

    a94a5e47b604f216f9fb55eb813f166d21a43ba7

---
+++ DarkPlasma_FitBattlebackToScreen.js
@@ -0,0 +1,38 @@
+// DarkPlasma_FitBattlebackToScreen 1.0.0
+// Copyright (c) 2026 DarkPlasma
+// This software is released under the MIT license.
+// http://opensource.org/licenses/mit-license.php
+
+/**
+ * 2026/04/02 1.0.0 最初のバージョン
+ */
+
+/*:
+ * @plugindesc 戦闘背景を画面の大きさに合わせる
+ * @author DarkPlasma
+ * @license MIT
+ *
+ * @target MZ
+ * @url https://github.com/elleonard/DarkPlasma-MZ-Plugins/tree/release
+ *
+ * @help
+ * version: 1.0.0
+ * 戦闘背景を画面の大きさに合わせて拡大・縮小します。
+ */
+
+(() => {
+  'use strict';
+
+  function Sprite_Battleback_FitToScreenMixIn(spriteBattleback) {
+    spriteBattleback.adjustPosition = function () {
+      this.width = Graphics.width;
+      this.height = Graphics.height;
+      const ratioX = this.width / this.bitmap.width;
+      const ratioY = this.height / this.bitmap.height;
+      const scale = Math.max(ratioX, ratioY, 1.0);
+      this.scale.x = scale;
+      this.scale.y = scale;
+    };
+  }
+  Sprite_Battleback_FitToScreenMixIn(Sprite_Battleback.prototype);
+})();
