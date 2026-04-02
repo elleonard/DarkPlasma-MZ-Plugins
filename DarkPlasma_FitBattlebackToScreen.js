6ea39027d4a8076a428b085edff7227cc92ce200
elleonard <6374339+elleonard@users.noreply.github.com>
2026-04-02 10:23:27 +0900

    c6f82a0704b7574eeb717751ef153c18c12c5190

--- DarkPlasma_FitBattlebackToScreen.js
+++ DarkPlasma_FitBattlebackToScreen.js
@@ -1,10 +1,11 @@
-// DarkPlasma_FitBattlebackToScreen 1.0.0
+// DarkPlasma_FitBattlebackToScreen 1.1.0
 // Copyright (c) 2026 DarkPlasma
 // This software is released under the MIT license.
 // http://opensource.org/licenses/mit-license.php
 
 /**
- * 2026/04/02 1.0.0 最初のバージョン
+ * 2026/04/02 1.1.0 縮小に対応
+ *            1.0.0 最初のバージョン
  */
 
 /*:
@@ -16,8 +17,9 @@
  * @url https://github.com/elleonard/DarkPlasma-MZ-Plugins/tree/release
  *
  * @help
- * version: 1.0.0
- * 戦闘背景を画面の大きさに合わせて拡大します。
+ * version: 1.1.0
+ * 戦闘背景を画面の大きさに合わせて拡大・縮小します。
+ * 縦横比は維持します。
  */
 
 (() => {
@@ -25,11 +27,11 @@
 
   function Sprite_Battleback_FitToScreenMixIn(spriteBattleback) {
     spriteBattleback.adjustPosition = function () {
-      this.width = Graphics.width;
-      this.height = Graphics.height;
-      const ratioX = this.width / this.bitmap.width;
-      const ratioY = this.height / this.bitmap.height;
-      const scale = Math.max(ratioX, ratioY, 1.0);
+      this.width = Math.max(Graphics.width, this.bitmap.width);
+      this.height = Math.max(Graphics.height, this.bitmap.height);
+      const ratioX = Graphics.width / this.bitmap.width;
+      const ratioY = Graphics.height / this.bitmap.height;
+      const scale = Math.max(ratioX, ratioY);
       this.scale.x = scale;
       this.scale.y = scale;
     };
