c6f82a0704b7574eeb717751ef153c18c12c5190
plasma <6374339+elleonard@users.noreply.github.com>
2026-04-02 10:22:58 +0900

    1.1.0 縮小に対応

--- src/codes/FitBattlebackToScreen/config/config.ts
+++ src/codes/FitBattlebackToScreen/config/config.ts
@@ -1,11 +1,14 @@
 import { ConfigDefinitionBuilder } from '../../../../modules/config/configDefinitionBuilder.js';
-import { PluginHistorySchema } from '../../../../modules/config/configSchema.js';
-import {} from '../../../../modules/config/createParameter.js';
+import { PluginHistorySchema, PluginParameterSchema } from '../../../../modules/config/configSchema.js';
 import { dedent } from '@qnighy/dedent';
 
 const histories: PluginHistorySchema[] = [
   {
     date: "2026/04/02",
+    version: "1.1.0",
+    description: "縮小に対応",
+  },
+  {
     version: "1.0.0",
     description: "最初のバージョン",
   }
@@ -18,5 +21,6 @@ export const config = new ConfigDefinitionBuilder(
 )
   .withHistories(histories)
   .withLicense("MIT")
-  .withHelp(dedent`戦闘背景を画面の大きさに合わせて拡大します。`)
+  .withHelp(dedent`戦闘背景を画面の大きさに合わせて拡大・縮小します。
+    縦横比は維持します。`)
   .build();
--- src/codes/FitBattlebackToScreen/plugin/DarkPlasma_FitBattlebackToScreen.ts
+++ src/codes/FitBattlebackToScreen/plugin/DarkPlasma_FitBattlebackToScreen.ts
@@ -2,11 +2,12 @@
 
 function Sprite_Battleback_FitToScreenMixIn(spriteBattleback: Sprite_Battleback) {
   spriteBattleback.adjustPosition = function () {
-    this.width = Graphics.width;
-    this.height = Graphics.height;
-    const ratioX = this.width / this.bitmap!.width;
-    const ratioY = this.height / this.bitmap!.height;
-    const scale = Math.max(ratioX, ratioY, 1.0);
+    this.width = Math.max(Graphics.width, this.bitmap!.width);
+    this.height = Math.max(Graphics.height, this.bitmap!.height);
+    
+    const ratioX = Graphics.width / this.bitmap!.width;
+    const ratioY = Graphics.height / this.bitmap!.height;
+    const scale = Math.max(ratioX, ratioY);
     this.scale.x = scale;
     this.scale.y = scale;
   };
