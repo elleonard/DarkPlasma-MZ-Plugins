cef2484fef5b44e657f05ee851553c2818fc9f2e
elleonard <6374339+elleonard@users.noreply.github.com>
2026-04-20 00:53:24 +0900

    5ce130f4908b1f8986de257ddbb6ade044d43339

--- DarkPlasma_StateIgnoreRecoverAll.js
+++ DarkPlasma_StateIgnoreRecoverAll.js
@@ -1,9 +1,10 @@
-// DarkPlasma_StateIgnoreRecoverAll 1.0.0
+// DarkPlasma_StateIgnoreRecoverAll 1.0.1
 // Copyright (c) 2026 DarkPlasma
 // This software is released under the MIT license.
 // http://opensource.org/licenses/mit-license.php
 
 /**
+ * 2026/04/20 1.0.1 全回復すると指定ステートの歩数情報が消えてしまう不具合を修正
  * 2026/01/18 1.0.0 最初のバージョン
  */
 
@@ -22,7 +23,7 @@
  * @default []
  *
  * @help
- * version: 1.0.0
+ * version: 1.0.1
  * イベントコマンド「全回復」で解除されないステートを実現します。
  */
 
@@ -45,19 +46,70 @@
       : [],
   };
 
+  function Game_Temp_StateIgnoreRecoverAllMixIn(gameTemp) {
+    gameTemp.evacuateStatesAndMeta = function (statesAndMeta) {
+      this._evacuatedStatesAndMeta = statesAndMeta;
+    };
+    gameTemp.evacuatedStatesAndMeta = function () {
+      return this._evacuatedStatesAndMeta;
+    };
+    gameTemp.clearEvacuatedStatesAndMeta = function () {
+      this._evacuatedStatesAndMeta = undefined;
+    };
+  }
+  Game_Temp_StateIgnoreRecoverAllMixIn(Game_Temp.prototype);
   function Game_BattlerBase_StateIgnoreRecoverAllMixIn(gameBattlerBase) {
     const _recoverAll = gameBattlerBase.recoverAll;
     gameBattlerBase.recoverAll = function () {
-      const continueStates = this.stateIdsIgnoreRecoverAll();
-      const continueStateTurns = {};
-      continueStates.forEach((stateId) => (continueStateTurns[stateId] = this._stateTurns[stateId]));
+      $gameTemp.evacuateStatesAndMeta(this.statesAndMetaForIgnoreRecoverAll());
       _recoverAll.call(this);
-      this._states = continueStates;
-      this._stateTurns = continueStateTurns;
+      const evacuated = $gameTemp.evacuatedStatesAndMeta();
+      this.restoreStatesAndMeta(evacuated);
+      $gameTemp.clearEvacuatedStatesAndMeta();
+    };
+    gameBattlerBase.statesAndMetaForIgnoreRecoverAll = function () {
+      const stateIds = this.stateIdsIgnoreRecoverAll();
+      const stateTurns = {};
+      stateIds.forEach((stateId) => {
+        stateTurns[stateId] = this._stateTurns[stateId];
+      });
+      return {
+        ids: stateIds,
+        turns: stateTurns,
+      };
     };
     gameBattlerBase.stateIdsIgnoreRecoverAll = function () {
       return this._states.filter((stateId) => settings.states.includes(stateId));
     };
+    gameBattlerBase.restoreStatesAndMeta = function (statesAndMeta) {
+      this._states = statesAndMeta.ids;
+      this._stateTurns = statesAndMeta.turns;
+    };
   }
   Game_BattlerBase_StateIgnoreRecoverAllMixIn(Game_BattlerBase.prototype);
+  function Game_Actor_StateIgnoreRecoverAllMixIn(gameActor) {
+    const _statesAndMetaForIgnoreRecoverAll = gameActor.statesAndMetaForIgnoreRecoverAll;
+    gameActor.statesAndMetaForIgnoreRecoverAll = function () {
+      return {
+        ..._statesAndMetaForIgnoreRecoverAll.call(this),
+        steps: this.stateStepsForIgnoreRecoverAll(),
+      };
+    };
+    gameActor.stateStepsForIgnoreRecoverAll = function () {
+      const stateIds = this.stateIdsIgnoreRecoverAll();
+      const stateSteps = {};
+      stateIds.forEach((stateId) => {
+        stateSteps[stateId] = this._stateSteps[stateId];
+      });
+      return stateSteps;
+    };
+    const _restoreStatesAndMeta = gameActor.restoreStatesAndMeta;
+    gameActor.restoreStatesAndMeta = function (statesAndMeta) {
+      _restoreStatesAndMeta.call(this, statesAndMeta);
+      if (statesAndMeta.steps) {
+        this._stateSteps = statesAndMeta.steps;
+      }
+    };
+  }
+  Game_Actor_StateIgnoreRecoverAllMixIn(Game_Actor.prototype);
 })();
