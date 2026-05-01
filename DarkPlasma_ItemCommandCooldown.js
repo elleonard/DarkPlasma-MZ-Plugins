5852a54ed5d555dbe7e5a9e027db93f8c0f264b1
elleonard <6374339+elleonard@users.noreply.github.com>
2026-05-01 16:13:16 +0900

    2da9d4245e368550de13fd40aa5ab40e5362b696

--- DarkPlasma_ItemCommandCooldown.js
+++ DarkPlasma_ItemCommandCooldown.js
@@ -1,15 +1,17 @@
-// DarkPlasma_ItemCommandCooldown 1.0.1
-// Copyright (c) 2021 DarkPlasma
+// DarkPlasma_ItemCommandCooldown 1.0.2
+// Copyright (c) 2026 DarkPlasma
 // This software is released under the MIT license.
 // http://opensource.org/licenses/mit-license.php
 
 /**
+ * 2026/05/01 1.0.2 TypeScript移行
+ *                  色設定をカラーパレット対応
  * 2021/11/04 1.0.1 色設定が効かない不具合を修正
  *                  クールタイムが表示より1ターン短い不具合を修正
  * 2021/11/01 1.0.0 公開
  */
 
-/*:ja
+/*:
  * @plugindesc アイテムコマンドにクールタイムを設定する
  * @author DarkPlasma
  * @license MIT
@@ -27,10 +29,10 @@
  * @desc コマンドの後ろにクールタイムターン数を表示する設定をします。
  * @text 表示設定
  * @type struct<DisplaySetting>
- * @default {"enabled":"true", "format":"CT:{turn}", "color":"2"}
+ * @default {"enabled":"true","format":"CT:{turn}","color":"2"}
  *
  * @help
- * version: 1.0.1
+ * version: 1.0.2
  * アイテムコマンドにクールタイムを設定します。
  * アイテムコマンドを使用した後、
  * 一定ターン数アイテムコマンドを使用不能にできます。
@@ -60,7 +62,7 @@
  * @param color
  * @desc 表示色を設定します。
  * @text 色
- * @type number
+ * @type color
  * @default 2
  */
 (() => {
@@ -76,37 +78,40 @@
 
   const settings = {
     defaultCooldownTurn: Number(pluginParameters.defaultCooldownTurn || 3),
-    display: ((parameter) => {
-      const parsed = JSON.parse(parameter);
-      return {
-        enabled: String(parsed.enabled || true) === 'true',
-        format: String(parsed.format || 'CT:{turn}'),
-        color: Number(parsed.color || 2),
-      };
-    })(pluginParameters.display || '{"enabled":"true", "format":"CT:{turn}", "color":"2"}'),
+    display: pluginParameters.display
+      ? ((parameter) => {
+          const parsed = JSON.parse(parameter);
+          return {
+            enabled: String(parsed.enabled || true) === 'true',
+            format: String(parsed.format || `CT:{turn}`),
+            color: parsed.color?.startsWith('#') ? String(parsed.color) : Number(parsed.color || 2),
+          };
+        })(pluginParameters.display)
+      : { enabled: true, format: 'CT:{turn}', color: 2 },
   };
 
+  function ColorManager_ItemCommandCooldownMixIn(colorManager) {
+    colorManager.itemCooldownColor = function () {
+      return typeof settings.display.color === 'string'
+        ? settings.display.color
+        : this.textColor(settings.display.color);
+    };
+  }
+  ColorManager_ItemCommandCooldownMixIn(ColorManager);
   class ItemCommandCooldownTurns {
     initialize() {
       this._turns = {};
       $gameParty.allMembers().forEach((actor) => (this._turns[actor.actorId()] = 0));
     }
-
     setup(actor) {
       this._turns[actor.actorId()] = actor.initialItemCommandCooldownTurn();
     }
-
-    /**
-     * @param {number} actorId
-     * @return {number}
-     */
     cooldownTurn(actorId) {
       if (!this._turns[actorId]) {
         this._turns[actorId] = 0;
       }
       return this._turns[actorId];
     }
-
     decrease() {
       $gameParty.allMembers().forEach((actor) => {
         this._turns[actor.actorId()] = this.cooldownTurn(actor.actorId()) - 1;
@@ -116,31 +121,20 @@
       });
     }
   }
-
   const itemCommandCooldownTurns = new ItemCommandCooldownTurns();
-
-  /**
-   * @param {BattleManager} battleManager
-   */
   function BattleManager_ItemCommandCooldownMixIn(battleManager) {
     const _startBattle = battleManager.startBattle;
     battleManager.startBattle = function () {
       _startBattle.call(this);
       itemCommandCooldownTurns.initialize();
     };
-
     const _endTurn = battleManager.endTurn;
     battleManager.endTurn = function () {
       _endTurn.call(this);
       itemCommandCooldownTurns.decrease();
     };
   }
-
   BattleManager_ItemCommandCooldownMixIn(BattleManager);
-
-  /**
-   * @param {Game_Battler.prototype} gameBattler
-   */
   function Game_Battler_ItemCommandCooldownMixIn(gameBattler) {
     const _useItem = gameBattler.useItem;
     gameBattler.useItem = function (item) {
@@ -149,67 +143,58 @@
         this.setupItemCooldownTurn();
       }
     };
-
     gameBattler.setupItemCooldownTurn = function () {};
   }
-
   Game_Battler_ItemCommandCooldownMixIn(Game_Battler.prototype);
-
-  /**
-   * @param {Game_Actor.prototype} gameActor
-   */
   function Game_Actor_ItemCommandCooldownMixIn(gameActor) {
     gameActor.setupItemCooldownTurn = function () {
       itemCommandCooldownTurns.setup(this);
     };
-
     gameActor.itemCommandCooldownTurn = function () {
       return itemCommandCooldownTurns.cooldownTurn(this.actorId());
     };
-
     gameActor.initialItemCommandCooldownTurn = function () {
       /**
        * ターン終了時に減算されるため、+1しておく
        */
       return settings.defaultCooldownTurn + this.itemCommandCooldownTurnPlus() + 1;
     };
-
+    /**
+     * TODO: AllocateUniqueTraitIdを使う
+     */
     gameActor.itemCommandCooldownTurnPlus = function () {
       return this.traitObjects()
         .filter((object) => !!object.meta.itemCommandCooldownTurnPlus)
         .reduce((result, object) => result + Number(object.meta.itemCommandCooldownTurnPlus), 0);
     };
-
     gameActor.canItemCommand = function () {
       return !this.isInItemCommandCooldown();
     };
-
     gameActor.isInItemCommandCooldown = function () {
       return itemCommandCooldownTurns.cooldownTurn(this.actorId()) > 0;
     };
   }
-
   Game_Actor_ItemCommandCooldownMixIn(Game_Actor.prototype);
-
-  /**
-   * @param {Window_ActorCommand.prototype} windowClass
-   */
   function Window_ActorCommand_ItemCommandCooldownMixIn(windowClass) {
     const _addItemCommand = windowClass.addItemCommand;
     windowClass.addItemCommand = function () {
       _addItemCommand.call(this);
       const itemCommand = this._list.find((command) => command.symbol === 'item');
       if (itemCommand) {
-        itemCommand.enabled = this._actor.canItemCommand();
+        itemCommand.enabled = this._actor?.canItemCommand() || false;
       }
     };
-
     const _drawItem = windowClass.drawItem;
     windowClass.drawItem = function (index) {
-      if (settings.display.enabled && this._actor.isInItemCommandCooldown() && this.commandSymbol(index) === 'item') {
+      if (
+        settings.display.enabled &&
+        this._actor &&
+        this._actor.isInItemCommandCooldown() &&
+        this.commandSymbol(index) === 'item'
+      ) {
         const rect = this.itemLineRect(index);
         const align = this.itemTextAlign();
-        const cooldownText = settings.display.format.replace(/\{turn\}/gi, this._actor.itemCommandCooldownTurn());
+        const cooldownText = settings.display.format.replace(/\{turn\}/gi, `${this._actor.itemCommandCooldownTurn()}`);
         /**
          * 中央寄せでいい感じにクールタイムの色だけ変えるため、詰め用文字列を作る
          */
@@ -222,26 +207,20 @@
           rect.x,
           rect.y,
           rect.width,
-          align
+          align,
         );
-        this.changeTextColor(ColorManager.textColor(settings.display.color));
+        this.changeTextColor(ColorManager.itemCooldownColor());
         this.drawText(
           `${this.paddingText(commandWidth, this.commandName(index).length)} ${cooldownText}`,
           rect.x,
           rect.y,
           rect.width,
-          align
+          align,
         );
       } else {
         _drawItem.call(this, index);
       }
     };
-
-    /**
-     * @param {number} width
-     * @param {number} minLength
-     * @return {string}
-     */
     windowClass.paddingText = function (width, minLength) {
       let result = ''.padStart(minLength, ' ');
       while (this.textWidth(result) < width) {
@@ -250,6 +229,5 @@
       return result;
     };
   }
-
   Window_ActorCommand_ItemCommandCooldownMixIn(Window_ActorCommand.prototype);
 })();
