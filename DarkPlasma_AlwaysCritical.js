e11f90c3032ba946ebbc7f6c34c6ee2ce0ee81c6
elleonard <6374339+elleonard@users.noreply.github.com>
2026-05-06 02:55:41 +0900

    11937f0cc55e119bf642b3a75e67b519065140c9

--- DarkPlasma_AlwaysCritical.js
+++ DarkPlasma_AlwaysCritical.js
@@ -1,16 +1,17 @@
-// DarkPlasma_AlwaysCritical 1.0.2
-// Copyright (c) 2020 DarkPlasma
+// DarkPlasma_AlwaysCritical 1.0.3
+// Copyright (c) 2026 DarkPlasma
 // This software is released under the MIT license.
 // http://opensource.org/licenses/mit-license.php
 
 /**
+ * 2026/05/06 1.0.3 TypeScript移行
  * 2021/07/05 1.0.2 MZ 1.3.2に対応
  * 2021/06/22 1.0.1 サブフォルダからの読み込みに対応
  * 2020/09/10 1.0.0 公開
  */
 
-/*:ja
- * @plugindesc 常時クリティカルが出るスキル
+/*:
+ * @plugindesc 常時クリティカルが出る行動
  * @author DarkPlasma
  * @license MIT
  *
@@ -18,19 +19,22 @@
  * @url https://github.com/elleonard/DarkPlasma-MZ-Plugins/tree/release
  *
  * @help
- * version: 1.0.2
- * スキルのメモ欄に<alwaysCritical>と書くと、
+ * version: 1.0.3
+ * スキル/アイテムのメモ欄に<alwaysCritical>と書くと、
  * そのスキルが常にクリティカルヒットします。
  */
 
 (() => {
   'use strict';
 
-  const _Game_Action_itemCri = Game_Action.prototype.itemCri;
-  Game_Action.prototype.itemCri = function (target) {
-    if (this.item().meta.alwaysCritical) {
-      return 1;
-    }
-    return _Game_Action_itemCri.call(this, target);
-  };
+  function Game_Action_AlwaysCriticalMixIn(gameAction) {
+    const _itemCri = gameAction.itemCri;
+    gameAction.itemCri = function (target) {
+      if (this.item()?.meta.alwaysCritical) {
+        return 1;
+      }
+      return _itemCri.call(this, target);
+    };
+  }
+  Game_Action_AlwaysCriticalMixIn(Game_Action.prototype);
 })();
