ca72749f3c9826acda93801d2c2a07dc35eb873d
plasma <6374339+elleonard@users.noreply.github.com>
2026-05-08 09:48:15 +0900

    前提プラグインへのパス修正

--- src/codes/RegenerateByValueTraitCustomSample/RegenerateByValueTraitCustomSample.d.ts
+++ src/codes/RegenerateByValueTraitCustomSample/RegenerateByValueTraitCustomSample.d.ts
@@ -1,2 +1,2 @@
 /// <reference path="../../typings/rmmz.d.ts" />
-/// <reference path="../RegenerateByValueTrait/RegenerateByValueTrait.d.ts" />
+/// <reference path="../RegenerateByValueTrait/plugin/RegenerateByValueTrait.d.ts" />
