b87ae52012e2497990ec9ebf8bca077909cce6ee
elleonard <6374339+elleonard@users.noreply.github.com>
2026-05-09 14:40:26 +0900

    fcf5ffd35ee4b42bb90d4f3c7ca58c9105c630bb

--- DarkPlasma_RandomSkill.js
+++ DarkPlasma_RandomSkill.js
@@ -1,9 +1,10 @@
-// DarkPlasma_RandomSkill 1.0.0
-// Copyright (c) 2023 DarkPlasma
+// DarkPlasma_RandomSkill 1.1.0
+// Copyright (c) 2026 DarkPlasma
 // This software is released under the MIT license.
 // http://opensource.org/licenses/mit-license.php
 
 /**
+ * 2026/05/09 1.1.0 スキル対象を引き継ぐ設定に対応
  * 2023/11/25 1.0.0 公開
  */
 
@@ -19,7 +20,7 @@
  * @orderAfter DarkPlasma_AllocateUniqueEffectCode
  *
  * @help
- * version: 1.0.0
+ * version: 1.1.0
  * 使用するとランダムで指定したスキルのうち
  * どれか一つを発動する使用効果を実現します。
  *
@@ -31,11 +32,19 @@
  *   skillId:6, weight:1
  * >
  *
- * この使用効果を持つスキル・アイテムの対象は
- * 使用者に設定してください。
+ * 対象は発動するスキルの対象範囲からランダムに決定します。
+ * この使用効果を持つスキルと同じ対象にしたい場合は、
+ * 対象範囲を揃えた上で以下のように記述してください。
+ *
+ * <randomSkill:
+ *   skillId:5, weight:2, target: same
+ *   skillId:6, weight:1
+ * >
+ * target: sameを記述したスキルのみ、同じ対象に発動します。
+ * (対象範囲が異なる場合の挙動は定義しません)
  *
  * 本プラグインの利用には下記プラグインを必要とします。
- * DarkPlasma_AllocateUniqueEffectCode version:1.0.1
+ * DarkPlasma_AllocateUniqueEffectCode version:1.0.2
  * 下記プラグインと共に利用する場合、それよりも下に追加してください。
  * DarkPlasma_AllocateUniqueEffectCode
  */
@@ -52,8 +61,8 @@
   function registerRandomSkillEffect(skills) {
     const dataId = $dataRandomSkillEffects.length;
     $dataRandomSkillEffects.push({
-      dataId: dataId,
-      skills: skills,
+      dataId,
+      skills,
     });
     return dataId;
   }
@@ -67,16 +76,30 @@
             .split('\n')
             .filter((line) => /skillId:[ ]?[0-9]+/.test(line))
             .map((line) => {
-              const columns = line.split(',').map((column) => {
-                return {
-                  type: column.trim().startsWith('skillId') ? 'skillId' : 'weight',
-                  value: Number(column.split(':')[1].trim() || '1'),
-                };
-              });
-              return {
-                skillId: columns.find((c) => c.type === 'skillId')?.value || 1,
-                weight: columns.find((c) => c.type === 'weight')?.value || 1,
+              const result = {
+                skillId: 1,
+                weight: 1,
+                targetType: 'random',
               };
+              line.split(',').forEach((column) => {
+                const type = (() => {
+                  const c = column.trim();
+                  if (c.startsWith('skillId')) {
+                    return 'skillId';
+                  } else if (c.startsWith('weight')) {
+                    return 'weight';
+                  } else if (c.startsWith('target')) {
+                    return 'target';
+                  }
+                  throw Error(`不正なトークン ${c}`);
+                })();
+                if (type === 'target') {
+                  result.targetType = column.split(':')[1].trim() === 'same' ? 'same' : 'random';
+                } else {
+                  result[type] = Number(column.split(':')[1].trim() || '1');
+                }
+              });
+              return result;
             }),
         );
         data.effects.push({
@@ -112,7 +135,10 @@
           w -= skill.weight;
           return w < 0;
         });
-        this.subject().forceAction(skill.skillId, -1);
+        this.subject().forceAction(
+          skill.skillId,
+          skill.targetType === 'same' && (target.isActor() || target.isEnemy()) ? target.index() : -1,
+        );
         this.makeSuccess(target);
       } else {
         _applyItemEffect.call(this, target, effect);
